310fce29148bfe170487eb107b5a1cb3ed3da297
srahn <stefan.rahn@gdi-service.de>
2014-07-09 14:24:20 +0200

    input check functions

---
+++ funktionen/input_check_functions.php
@@ -0,0 +1,54 @@
+<script type="text/javascript">
+
+	checknumbers = function(input, type, length, decimal_length){
+		if(type == 'numeric' || type == 'float4' || type == 'float8'){
+			var val = input.value.replace(/[a-zA-Z]/g, '');
+			val = val.replace(/,/g, '.');
+			parts = val.split('.');
+			ohne_leerz = parts[0].replace(/ /g, '').length;
+			mit_leerz = parts[0].length;
+			length = parseInt(length) - parseInt(decimal_length);
+			if(length != '' &&  ohne_leerz > length){
+				alert('Für dieses Feld sind maximal '+length+' Vorkommastellen erlaubt.');
+				parts[0] = parts[0].substring(0, length - ohne_leerz + mit_leerz);
+			}
+			val = parts[0];
+			if(parts[1] != undefined){
+				if(decimal_length != '' && parts[1].length > parseInt(decimal_length)){
+					alert(unescape('F%FCr dieses Feld sind maximal '+decimal_length+' Nachkommastellen erlaubt.'));
+					parts[1] = parts[1].substring(0, decimal_length);
+				}
+				val = val+'.'+parts[1];
+			}
+			if(input.value != val){
+				input.value = val;
+			}
+		}
+		if(type == 'int2' || type == 'int4' || type == 'int8'){
+			if(input.value.search(/[^-\d]/g) != -1 || input.value.search(/.-/g) != -1){
+				alert('Es sind nur ganzzahlige Angaben erlaubt!');
+				var val = input.value.replace(/[^-\d]/g, '');
+				val = val.replace(/-/g, '');
+				input.value = val;
+			}
+		}
+	}
+
+	checkDate = function(string){
+    var split = string.split(".");
+    var day = parseInt(split[0], 10);
+    var month = parseInt(split[1], 10);
+    var year = parseInt(split[2], 10);
+    var check = new Date(year, month-1, day);
+    var day2 = check.getDate();
+    var year2 = check.getFullYear();
+    var month2 = check.getMonth()+1;
+    if(year2 == year && month == month2 && day == day2){
+    	return true;
+    }
+    else{
+    	return false;
+    }
+	}
+	
+</script>
