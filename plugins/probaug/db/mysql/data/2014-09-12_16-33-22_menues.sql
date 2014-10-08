5eb287b101ceebbb652f33caa4c3cc3bc058ff47
srahn <stefan.rahn@gdi-service.de>
2014-10-08 15:33:33 +0200

    seeds

--- plugins/probaug/db/mysql/data/2014-09-12_16-33-22_menues.sql
+++ plugins/probaug/db/mysql/data/2014-09-12_16-33-22_menues.sql
@@ -1,7 +1,7 @@
-INSERT INTO `u_menues` (`id`, `name`, `name_low-german`, `name_english`, `name_polish`, `name_vietnamese`, `links`, `obermenue`, `menueebene`, `target`, `order`) VALUES
+INSERT INTO `u_menues` (`name`, `name_low-german`, `name_english`, `name_polish`, `name_vietnamese`, `links`, `obermenue`, `menueebene`, `target`, `order`) VALUES
 ('Bauauskunft', 'Bauauskunft', 'Bauauskunft', NULL, 'ThÃ´ng tin xÃ¢y dá»±ng', 'index.php?go=changemenue', 0, 1, NULL, 140);
 
 SET @obermenue_id = LAST_INSERT_ID();
 
-INSERT INTO `u_menues` (`id`, `name`, `name_low-german`, `name_english`, `name_polish`, `name_vietnamese`, `links`, `obermenue`, `menueebene`, `target`, `order`) VALUES
+INSERT INTO `u_menues` (`name`, `name_low-german`, `name_english`, `name_polish`, `name_vietnamese`, `links`, `obermenue`, `menueebene`, `target`, `order`) VALUES
 ('Suche', 'Suche', 'Suche', NULL, 'TÃ¬m kiáº¿m', 'index.php?go=Bauauskunft_Suche', @obermenue_id, 2, NULL, 0);
