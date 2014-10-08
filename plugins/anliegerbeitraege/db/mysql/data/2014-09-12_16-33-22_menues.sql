0aec7ade8e743c0a15c68de79d9ca6fc45113cc5
srahn <stefan.rahn@gdi-service.de>
2014-10-08 15:39:26 +0200

    seeds

--- plugins/anliegerbeitraege/db/mysql/data/2014-09-12_16-33-22_menues.sql
+++ plugins/anliegerbeitraege/db/mysql/data/2014-09-12_16-33-22_menues.sql
@@ -1,5 +1,5 @@
 INSERT INTO `u_menues` (`name`, `name_low-german`, `name_english`, `name_polish`, `name_vietnamese`, `links`, `obermenue`, `menueebene`, `target`, `order`) VALUES
-('Anliegerbeiträge', NULL, NULL, NULL, NULL, 'index.php?go=changemenue', 0, 1, NULL, 0);
+('Anliegerbeiträge', NULL, NULL, NULL, NULL, 'index.php?go=changemenue', 0, 1, NULL, 50);
 
 SET @obermenue_id = LAST_INSERT_ID();
 
--- plugins/jagdkataster/db/mysql/data/2014-09-12_16-33-22_menues.sql
+++ plugins/jagdkataster/db/mysql/data/2014-09-12_16-33-22_menues.sql
@@ -1,5 +1,5 @@
 INSERT INTO `u_menues` (`name`, `name_low-german`, `name_english`, `name_polish`, `name_vietnamese`, `links`, `obermenue`, `menueebene`, `target`, `order`) VALUES
-('Jagdkataster', NULL, 'Jagdkataster', NULL, NULL, 'index.php?go=changemenue', 0, 1, NULL, 0);
+('Jagdkataster', NULL, 'Jagdkataster', NULL, NULL, 'index.php?go=changemenue', 0, 1, NULL, 60);
 
 SET @obermenue_id = LAST_INSERT_ID();
 
