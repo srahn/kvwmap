13c14a991e7a90f05a69b3dcc09fc94b1b22e76c
srahn <stefan.rahn@gdi-service.de>
2015-03-09 13:23:29 +0100

    Anzeige des aktuellen Branches

--- layouts/snippets/git_status.php
+++ layouts/snippets/git_status.php
@@ -5,17 +5,22 @@ exec('cd '.$folder.' && git status -uno', $ausgabe, $ret);
 if($ret != 0){
 	echo 'Fehler bei der Ausführung von "git status"';
 }
-else{
+else{	
+	$explosion = explode(' ', $ausgabe[0]);
+	$branch = array_pop($explosion);
+	echo '<table><tr><td>Branch:</td><td>'.$branch.'</td></tr>';
+	echo '<tr><td>Status:</td><td>';
 	if(strpos($ausgabe[1], '# Your branch is behind') === 0){
 		$explosion = explode(' ', $ausgabe[1]);
 		for($i = 0; $i < count($explosion); $i++){
 			if($explosion[$i] == 'by')$num_commits_behind = $explosion[$i+1];
 		}
 		if($num_commits_behind != ''){
-			echo 'Anzahl neuer commits auf github: '.$num_commits_behind;
+			echo 'Anzahl neuer commits auf <a target="_blank" href="https://github.com/srahn/kvwmap/commits/'.$branch.'">github</a>: '.$num_commits_behind;
 		}
 	}
 	else echo 'Quellcode aktuell';
+	echo '</td></tr></table>';
 }
 
 ?>
