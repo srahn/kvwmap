b2631d9f2c1d5b0ac4643b7f0428d4a72d5d1672
srahn <stefan.rahn@gdi-service.de>
2015-03-31 14:54:33 +0200

    OWS HTTP-Authentifizierung

--- class/kvwmap.php
+++ class/kvwmap.php
@@ -801,8 +801,8 @@ class GUI {
 						$layer->setMetaData('ows_srs',$layerset[$i]['ows_srs']);
 						$layer->setMetaData('wms_connectiontimeout',$layerset[$i]['wms_connectiontimeout']);
 						$layer->setMetaData('ows_auth_username', $layerset[$i]['wms_auth_username']);
-						$layer->setMetaData('ows_auth_password', '{'.$layerset[$i]['wms_auth_password'].'}');
-						$layer->setMetaData('ows_auth_type', 'any');
+						$layer->setMetaData('ows_auth_password', $layerset[$i]['wms_auth_password']);
+						$layer->setMetaData('ows_auth_type', 'basic');
 						
 						$layer->set('dump', 0);
 						$layer->set('type',$layerset[$i]['Datentyp']);
