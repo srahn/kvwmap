e77c332a83543d42a1b0ae94b6c3b3e27f7ea5ff
srahn <stefan.rahn@gdi-service.de>
2015-04-08 15:46:05 +0200

    Multigeometrien splitten -> Multigeometrien

--- class/spatial_processor.php
+++ class/spatial_processor.php
@@ -47,8 +47,8 @@ class spatial_processor {
   }
   
   function split_multi_geometries($wktgeom, $layer_epsg, $client_epsg){
-  	$sql = "select ST_geometryN(st_transform(st_geomfromtext('".$wktgeom."', ".$client_epsg."), ".$layer_epsg."),"; 
-		$sql.= "generate_series(1, ST_NumGeometries(st_geomfromtext('".$wktgeom."', ".$client_epsg."))))";
+  	$sql = "select st_multi(ST_geometryN(st_transform(st_geomfromtext('".$wktgeom."', ".$client_epsg."), ".$layer_epsg."),"; 
+		$sql.= "generate_series(1, ST_NumGeometries(st_geomfromtext('".$wktgeom."', ".$client_epsg.")))))";
 		$ret = $this->pgdatabase->execSQL($sql,4, 0);
 		if (!$ret[0]) {
       while($rs=pg_fetch_row($ret[1])) {
