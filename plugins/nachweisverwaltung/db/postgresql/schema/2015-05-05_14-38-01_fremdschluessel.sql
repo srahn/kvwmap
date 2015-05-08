c2fb87b944d0071db98ad72cbcae8c48c9284d0c
srahn <stefan.rahn@gdi-service.de>
2015-05-08 14:42:42 +0200

    Nachweistabellen bereinigen

--- plugins/nachweisverwaltung/db/postgresql/schema/2015-05-05_14-38-01_fremdschluessel.sql
+++ plugins/nachweisverwaltung/db/postgresql/schema/2015-05-05_14-38-01_fremdschluessel.sql
@@ -1,5 +1,7 @@
 BEGIN;
 
+DELETE FROM nachweisverwaltung.fp_punkte2antraege WHERE antrag_nr IN (SELECT DISTINCT a.antrag_nr FROM nachweisverwaltung.fp_punkte2antraege as a LEFT JOIN nachweisverwaltung.n_antraege as b ON (a.antrag_nr=b.antr_nr) WHERE b.antr_nr IS NULL);
+DELETE FROM nachweisverwaltung.n_nachweise2antraege WHERE antrag_id IN (SELECT DISTINCT a.antrag_id FROM nachweisverwaltung.n_nachweise2antraege as a LEFT JOIN nachweisverwaltung.n_antraege as b ON (a.antrag_id=b.antr_nr) WHERE b.antr_nr IS NULL);
 
 ALTER TABLE nachweisverwaltung.fp_punkte2antraege
   ADD CONSTRAINT p2a_a_fkey FOREIGN KEY (antrag_nr)
