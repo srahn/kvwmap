e23ab8f4fc4449298f1afe5e506d26b51e0ea956
srahn <stefan.rahn@gdi-service.de>
2015-06-16 10:18:45 +0200

    WMS 1.3.0 Option entfernt

--- layouts/snippets/layer_formular.php
+++ layouts/snippets/layer_formular.php
@@ -447,8 +447,7 @@ else {
 		      			<option value=""><?php echo $this->strPleaseSelect; ?></option>
 		      			<option <? if($this->layerdata['wms_server_version'] == '1.0.0'){echo 'selected';} ?> value="1.0.0">1.0.0</option>
 		      			<option <? if($this->layerdata['wms_server_version'] == '1.1.0'){echo 'selected';} ?> value="1.1.0">1.1.0</option>
-		      			<option <? if($this->layerdata['wms_server_version'] == '1.1.1'){echo 'selected';} ?> value="1.1.1">1.1.1</option>
-		      			<option <? if($this->layerdata['wms_server_version'] == '1.3.0'){echo 'selected';} ?> value="1.3.0">1.3.0</option>		      			
+		      			<option <? if($this->layerdata['wms_server_version'] == '1.1.1'){echo 'selected';} ?> value="1.1.1">1.1.1</option>     			
 		      		</select>
 		  		</td>
 		  	</tr>
