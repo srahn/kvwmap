fa971e1d7e15ce0f91deb1708c57bf883e28d1f8
srahn <stefan.rahn@gdi-service.de>
2015-08-06 09:50:25 +0200

    Anpassung zwischenablage und Themenauswahl laden

--- layouts/snippets/LayerCommentSelectForm.php
+++ layouts/snippets/LayerCommentSelectForm.php
@@ -4,21 +4,13 @@
  ?>
 <h2><?php echo $strTitleLayers; ?></h2>
 <br>
-<table border="1" cellspacing="0" cellpadding="2">
-<tr bgcolor="<?php echo BG_DEFAULT ?>">
-  <th><?php echo $strName; ?></th>
-  <th>&nbsp;</th>
-</tr>
-<?php
-  # 2006-03-20 pk
-  $anzLayerComments=count($this->layerComments);
-  for ($i=0;$i<$anzLayerComments;$i++) {
-  	?>
-  <tr>
-    <td><a href="index.php?go=Layerauswahl_Laden&id=<? echo $this->layerComments[$i]['id']; ?>"><? echo $this->layerComments[$i]['name']; ?></a></td>
-    <td><a href="javascript:Bestaetigung('index.php?go=Layerauswahl_loeschen&id=<?php echo $this->layerComments[$i]['id']; ?>','<? echo $this->strDeleteWarningMessage; ?>');"><?php echo $this->strDelete; ?></a></td>
-  </tr>
-  <?php 
-  }
-  ?>
-</table>
+<table cellpadding="0" cellspacing="0">
+<?	$anzLayerComments=count($this->layerComments);
+  for ($i=0;$i<$anzLayerComments;$i++) { ?>
+		<tr>
+			<td style="background-color:<? echo BG_GLEATTRIBUTE; ?>;border:1px solid #C3C7C3;"><a href="index.php?go=Layerauswahl_Laden&id=<? echo $this->layerComments[$i]['id']; ?>"><div style="padding:4px;"><span class="fett"><? echo $this->layerComments[$i]['name']; ?></span></div></a></td>
+			<td>&nbsp;&nbsp;<a title="<? echo $this->strDelete; ?>" href="javascript:Bestaetigung('index.php?go=Layerauswahl_loeschen&id=<?php echo $this->layerComments[$i]['id']; ?>','<? echo $this->strDeleteWarningMessage; ?>');"><img style="vertical-align:middle; border: 1px solid #C3C7C3" src="<? echo GRAPHICSPATH ?>datensatz_loeschen.png"></a></td>
+		</tr>
+		<tr><td>&nbsp;</td></tr>
+<?		} ?>				
+</table> 
--- layouts/snippets/zwischenablage.php
+++ layouts/snippets/zwischenablage.php
@@ -20,13 +20,13 @@
 		<?	for($i = 0; $i < count($this->layer); $i++){ ?>
 					<tr>
 						<td style="background-color:<? echo BG_GLEATTRIBUTE; ?>;border:1px solid #C3C7C3;border-right:none;">
-							<a href="index.php?go=gemerkte_Datensaetze_anzeigen&layer_id=<? echo $this->layer[$i]['layer_id']; ?>"><div style="padding:5px;"><span class="fett"><? echo $this->layer[$i]['Name']; ?>:</span></div></a>
+							<a href="index.php?go=gemerkte_Datensaetze_anzeigen&layer_id=<? echo $this->layer[$i]['layer_id']; ?>"><div style="padding:4px;"><span class="fett"><? echo $this->layer[$i]['Name']; ?>:</span></div></a>
 						</td>
 						<td style="background-color:<? echo BG_GLEATTRIBUTE; ?>;border:1px solid #C3C7C3;border-left:none;">
-							<a href="index.php?go=gemerkte_Datensaetze_anzeigen&layer_id=<? echo $this->layer[$i]['layer_id']; ?>"><div style="padding:5px;"><span><? echo $this->layer[$i]['count']; if($this->layer[$i]['count'] == 1)echo ' Datensatz'; else echo ' Datensätze';?></span></div></a>
+							<a href="index.php?go=gemerkte_Datensaetze_anzeigen&layer_id=<? echo $this->layer[$i]['layer_id']; ?>"><div style="padding:4px;"><span><? echo $this->layer[$i]['count']; if($this->layer[$i]['count'] == 1)echo ' Datensatz'; else echo ' Datensätze';?></span></div></a>
 						</td>
 						<td>
-							&nbsp;&nbsp;<a title="entfernen" href="index.php?go=Datensaetze_nicht_mehr_merken&chosen_layer_id=<? echo $this->layer[$i]['layer_id']; ?>"><img style="border: 1px solid #C3C7C3" src="<? echo GRAPHICSPATH ?>datensatz_loeschen.png"></a>
+							&nbsp;&nbsp;<a title="entfernen" href="index.php?go=Datensaetze_nicht_mehr_merken&chosen_layer_id=<? echo $this->layer[$i]['layer_id']; ?>"><img style="vertical-align:middle; border: 1px solid #C3C7C3" src="<? echo GRAPHICSPATH ?>datensatz_loeschen.png"></a>
 						</td>
 					</tr>
 					<tr><td>&nbsp;</td></tr>
