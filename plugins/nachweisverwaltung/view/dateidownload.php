ecbd76d0c2296ad7593423c2f14c3d56431dfe58
srahn <stefan.rahn@gdi-service.de>
2015-09-28 14:47:39 +0200

    Nachweisverwaltung

---
+++ plugins/nachweisverwaltung/view/dateidownload.php
@@ -0,0 +1,20 @@
+<table width="100%" border="0" cellpadding="5" cellspacing="0">
+  <tr> 
+    <td bgcolor="<?php echo BG_FORM ?>"><div align="center"><h2><?php echo $this->titel; ?></h2> 
+      </div></td>
+  </tr>
+  <tr> 
+    <td bgcolor="<?php echo BG_FORM ?>"><div align="center">
+	
+	<?php if ($this->Fehlermeldung!='') { include(LAYOUTPATH."snippets/Fehlermeldung.php"); } ?>
+	<strong><font color="#FF0000">
+	<?php if ($this->Meldung!='') { echo $this->Meldung; } ?>
+	</font> </strong>
+	 </div>
+	 
+    </td>
+  </tr>
+  <tr>
+  	<td><a href="<? echo $this->formvars['filename'] ?>"><? echo $this->formvars['filename'] ?></a></td>
+  </tr>
+</table>
