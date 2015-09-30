255bfc9ef6add8c0fafb68352a807e3a2d4b8dfc
srahn <stefan.rahn@gdi-service.de>
2015-09-30 09:27:06 +0200

    export PGCLIENTENCODING=UTF-8

--- class/data_import_export.php
+++ class/data_import_export.php
@@ -673,7 +673,7 @@ class data_import_export {
 			$exportfile = IMAGEPATH.$folder.'/'.$this->formvars['layer_name'];
 			switch($this->formvars['export_format']){
 				case 'Shape' : { 
-					$command = POSTGRESBINPATH.'pgsql2shp -r -u '.$layerdb->user;
+					$command = 'export PGCLIENTENCODING=UTF-8 && '.POSTGRESBINPATH.'pgsql2shp -r -u '.$layerdb->user;
 					if($layerdb->passwd != '')$command.= ' -P "'.$layerdb->passwd.'"';
 					if($layerdb->port != '')$command.=' -p '.$layerdb->port;
 					if($layerdb->host != '') $command .= ' -h ' . $layerdb->host;
