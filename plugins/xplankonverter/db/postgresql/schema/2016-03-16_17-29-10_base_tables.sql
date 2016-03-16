2e87f4f6c9f1414188d4973082b969f81a2a3c98
pkorduan <peter.korduan@gdi-service.de>
2016-03-16 18:19:44 +0100

    Change table konvertierungen and add view for layer.

--- plugins/xplankonverter/db/postgresql/schema/2016-03-16_17-29-10_base_tables.sql
+++ plugins/xplankonverter/db/postgresql/schema/2016-03-16_17-29-10_base_tables.sql
@@ -1,16 +1,18 @@
 BEGIN;
 
-CREATE TYPE konvertierungsstatus AS ENUM ('fertig', 'noch nicht ausgeführt', 'in Arbeit');
+CREATE TYPE konvertierungsstatus AS ENUM ('erstellt','Angaben vollständig','validiert','in Arbeit','fertig');
 
 CREATE TABLE xplankonverter.konvertierungen
 (
   id serial NOT NULL,
   regel_id integer,
-  status konvertierungsstatus,
+  bezeichnung character varying,
+  status konvertierungsstatus DEFAULT 'erstellt',
   stelle_id integer,
   CONSTRAINT konvertierungen_id_pkey PRIMARY KEY (id)
 ) WITH ( OIDS=TRUE );
 COMMENT ON COLUMN xplankonverter.konvertierungen.regel_id IS 'Id der in der Konvertierung angewendeten Regel.';
+COMMENT ON COLUMN xplankonverter.konvertierungen.bezeichnung IS 'Bezeichnung der Konvertierung. (Freitext)';
 COMMENT ON COLUMN xplankonverter.konvertierungen.status IS 'Status der Konvertierung. Enthält ein Wert vom Typ konvertierungsstatus.';
 COMMENT ON COLUMN xplankonverter.konvertierungen.stelle_id IS 'Die Id der Stelle in der die Konvertierung angelegt wurde und genutzt wird.';
 
