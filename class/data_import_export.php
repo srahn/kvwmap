097799fbda37fd02838a6f7769b3385fd35e0565
srahn <stefan.rahn@gdi-service.de>
2016-05-03 14:52:39 +0200

    -lco ENCODING=UTF-8

--- class/data_import_export.php
+++ class/data_import_export.php
@@ -661,7 +661,7 @@ class data_import_export {
   }
 	
 	function ogr2ogr_export($sql, $exportformat, $exportfile, $layerdb){
-		$command = 'export PGCLIENTENCODING=LATIN1;'.OGR_BINPATH.'ogr2ogr -f '.$exportformat.' -sql "'.$sql.'" '.$exportfile.' PG:"dbname='.$layerdb->dbName.' user='.$layerdb->user;
+		$command = 'export PGCLIENTENCODING=UTF-8;'.OGR_BINPATH.'ogr2ogr -f '.$exportformat.' -lco ENCODING=UTF-8 -sql "'.$sql.'" '.$exportfile.' PG:"dbname='.$layerdb->dbName.' user='.$layerdb->user;
 		if($layerdb->passwd != '')$command.= ' password='.$layerdb->passwd;
 		if($layerdb->port != '')$command.=' port='.$layerdb->port;
 		if($layerdb->host != '') $command .= ' host=' . $layerdb->host;
@@ -933,9 +933,6 @@ class data_import_export {
 			switch($this->formvars['export_format']){
 				case 'Shape' : { 
 					$this->ogr2ogr_export($sql, '"ESRI Shapefile"', $exportfile.'.shp', $layerdb);
-					$fp = fopen($exportfile.'.cpg', 'w');
-					fwrite($fp, 'UTF-8');
-					fclose($fp);
 					$zip = true;
 				}break;
 				
