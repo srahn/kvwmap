1bd357da3815f5db013d96b9a6efbbc14f051ab4
Stefen Mader <mader@h2441487.stratoserver.net>
2016-08-31 16:28:46 +0200

    BBSR-2-1 enable PgObject.update() to handle NULL-valued fields.

--- class/PgObject.php
+++ class/PgObject.php
@@ -3,23 +3,23 @@
 # kvwmap - Kartenserver für Kreisverwaltungen                     #
 ###################################################################
 # Lizenz                                                          #
-#                                                                 # 
+#                                                                 #
 # Copyright (C) 2004  Peter Korduan                               #
-#                                                                 # 
+#                                                                 #
 # This program is free software; you can redistribute it and/or   #
-# modify it under the terms of the GNU General Public License as  # 
-# published by the Free Software Foundation; either version 2 of  # 
-# the License, or (at your option) any later version.             # 
-#                                                                 #   
-# This program is distributed in the hope that it will be useful, #  
+# modify it under the terms of the GNU General Public License as  #
+# published by the Free Software Foundation; either version 2 of  #
+# the License, or (at your option) any later version.             #
+#                                                                 #
+# This program is distributed in the hope that it will be useful, #
 # but WITHOUT ANY WARRANTY; without even the implied warranty of  #
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the    #
 # GNU General Public License for more details.                    #
-#                                                                 #  
+#                                                                 #
 # You should have received a copy of the GNU General Public       #
 # License along with this program; if not, write to the Free      #
-# Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,  # 
-# MA 02111-1307, USA.                                             # 
+# Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,  #
+# MA 02111-1307, USA.                                             #
 #                                                                 #
 # Kontakt:                                                        #
 # peter.korduan@gdi-service.de                                    #
@@ -30,7 +30,7 @@
 #############################
 
 class PgObject {
-  
+
   function PgObject($gui, $schema, $tableName) {
     global $debug;
     $this->debug=$debug;
@@ -90,7 +90,7 @@ class PgObject {
   function getKVP() {
     $kvp = array();
     foreach($this->data AS $key => $value) {
-      $kvp[] = "\"" . $key . "\" = '" . $value . "'";
+      $kvp[] = "\"" . $key . "\" = " . ((''.$value) == '' ? "NULL" : "'$value'") . "";
     }
     return $kvp;
   }
