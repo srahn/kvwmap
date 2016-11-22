0d017ce1949a62f7274ab16ade0683b4c5bd7462
Peter Korduan <peter.korduan@gdi-service.de>
2016-11-22 18:04:08 +0100

    dev-12 Accept only 2000.xml files in plugin fortfuehrungslisten.

--- plugins/fortfuehrungslisten/model/loader.php
+++ plugins/fortfuehrungslisten/model/loader.php
@@ -16,14 +16,28 @@ class NASLoader extends DOMDocument {
 
 		$file = pathinfo($file_name);
 		if (strtolower($file['extension']) == 'zip') {
-			unzip($file_name, false, false, true);
-			$original_file = pathinfo($original_file_name);
-			$xml_file_name = $file['dirname'] . '/' . $original_file['filename'];
-			$xml = file_get_contents($xml_file_name);
-			unlink($xml_file_name);
-			if (is_dir($file['dirname'])) {
-				unlink($file['dirname'] . '/__MACOSX/._' . $original_file['filename']);
-				rmdir($file['dirname'] . '/__MACOSX');
+			$unziped_files = unzip($file_name, false, false, true);
+			foreach ($unziped_files AS $unziped_file) {
+				$unziped_file = str_replace('\\', '/', $unziped_file);
+				#echo '<br>Verarbeite ausgepackte Datei: ' . $unziped_file;
+				$pathinfo_unziped_file = pathinfo($unziped_file);
+				if (substr($pathinfo_unziped_file['filename'], -5) == '_2000') {
+					$xml_file_name = $file['dirname'] . '/'. $unziped_file;
+					$xml = file_get_contents($xml_file_name);
+				}
+
+				$rm_file = $file['dirname'] . '/' . $unziped_file;
+				#echo '<br>Lösche Datei: ' . $rm_file;
+				unlink(str_replace('\\', '/', $file['dirname'] . '/' . $unziped_file));
+
+				if (!empty($pathinfo_unziped_file['dirname'])) {
+					$rmdir = $file['dirname'] . '/' . $pathinfo_unziped_file['dirname'];
+					#echo '<br>Lösche Verzeichnis: ' . $rmdir;
+					@rmdir($rmdir);
+				}
+			}
+			if (empty($xml_file_name)) {
+				$err_msg = "Keine Datei mit der Endung _2000 in Zip-Datei gefunden. Prüfen Sie bitte ob Sie die richtige Zip-Datei hochgeladen haben.";
 			}
 		}
 		else {
@@ -31,97 +45,108 @@ class NASLoader extends DOMDocument {
 			$xml = file_get_contents($xml_file_name);
 		}
 
-		$this->loadXML($xml, LIBXML_NOBLANKS);
-
-		# Lese und speicher Attribute zum Auftrag
-		$nodes = $this->getElementsByTagName('datumDerAusgabe');
-		$node = $nodes[0];
-		$ff_auftrag->set('datumderausgabe', $node->nodeValue);
-
-		$nodes = $this->getElementsByTagName('AX_Fortfuehrungsauftrag');
-		$auftrag_node = $nodes[0];
-		foreach($auftrag_node->childNodes AS $child_node) {
-			$tag = strtolower($child_node->localName);
-			if (in_array($tag, array(
-				'profilkennung',
-				'auftragsnummer',
-				'impliziteloeschungderreservierung',
-				'verarbeitungsart',
-				'geometriebehandlung',
-				'mittemporaeremarbeitsbereich', 
-				'mitobjektenimfortfuehrungsgebiet',
-				'mitfortfuehrungsnachweis'
-			))) {
-				$ff_auftrag->set($tag, $child_node->nodeValue);
-			}
+		if (empty($xml_file_name)) {
+			$err_msg = "Keine Datei gefunden. Prüfen Sie ob Sie schon eine Datei hochgeladen haben.";
 		}
+		else {
+			#echo '<br>Lade Datei: ' . $xml_file_name;
+			$this->loadXML($xml, LIBXML_NOBLANKS);
 
-		# Finde Gebäude und deren Anlass
-		$this->gebaeude_nodes = $this->getElementsByTagName('AX_Gebaeude');
-		if ($this->gebaeude_nodes->length > 0) {
-			foreach($this->gebaeude_nodes[0]->childNodes AS $child_node) {
-				$tag = strtolower($child_node->localName);
-				if ($tag == 'anlass') {
-					$ff_auftrag->set('gebaeude', $child_node->nodeValue);
-				}
-			}
-		}
+			# Lese und speicher Attribute zum Auftrag
+			$nodes = $this->getElementsByTagName('datumDerAusgabe');
+			$node = $nodes[0];
+			$ff_auftrag->set('datumderausgabe', $node->nodeValue);
 
-		# speicher Auftrag
-		$ff_auftrag->update();
-
-		# Finde Flurstüecke und deren Anlässe
-		$this->flst_nodes = $this->getElementsByTagName('AX_Flurstueck');
-		$flurstuecke = array();
-		foreach($this->flst_nodes AS $flst_node) {
-			foreach($flst_node->childNodes AS $child_node) {
-				$tag = strtolower($child_node->localName);
-				if ($tag == 'flurstueckskennzeichen') {
-					$flst['flurstueckskennzeichen'] = $child_node->nodeValue;
-				}
-				if ($tag == 'anlass') {
-					$flst['anlass'] = $child_node->nodeValue;
-				}
-			}
-			$anlaesse[$flst['flurstueckskennzeichen']] = $flst['anlass'];
-		}
-
-		# Lösche vorhandene Fälle des Auftrages
-		$ff = new Fortfuehrungsfall($this->gui);
-		$ff->delete_by('ff_auftrag_id', $ff_auftrag->get('id'));
-
-		# Lege Fälle des Auftrages an
-		$this->fall_nodes = $this->getElementsByTagName('AX_Fortfuehrungsfall');
-		foreach($this->fall_nodes AS $fall_node) {
-			$ff = new Fortfuehrungsfall($this->gui);
-			$ff->set('ff_auftrag_id', $ff_auftrag->get('id'));
-			foreach($fall_node->childNodes AS $child_node) {
+			$nodes = $this->getElementsByTagName('AX_Fortfuehrungsauftrag');
+			$auftrag_node = $nodes[0];
+			foreach($auftrag_node->childNodes AS $child_node) {
 				$tag = strtolower($child_node->localName);
 				if (in_array($tag, array(
-					'fortfuehrungsfallnummer',
-					'laufendenummer',
-					'ueberschriftimfortfuehrungsnachweis'
+					'profilkennung',
+					'auftragsnummer',
+					'impliziteloeschungderreservierung',
+					'verarbeitungsart',
+					'geometriebehandlung',
+					'mittemporaeremarbeitsbereich', 
+					'mitobjektenimfortfuehrungsgebiet',
+					'mitfortfuehrungsnachweis'
 				))) {
-					$ff->set($tag, $child_node->nodeValue);
+					$ff_auftrag->set($tag, $child_node->nodeValue);
 				}
-				if (in_array($tag, array(
-					'zeigtaufaltesflurstueck',
-					'zeigtaufneuesflurstueck'
-				))) {
-					$ff->set_array($tag, $child_node->nodeValue);
-					if ($tag == 'zeigtaufneuesflurstueck') {
-						$ff->set_array('anlassarten', $anlaesse[$child_node->nodeValue]);
+			}
+
+			# Finde Gebäude und deren Anlass
+			$this->gebaeude_nodes = $this->getElementsByTagName('AX_Gebaeude');
+			if ($this->gebaeude_nodes->length > 0) {
+				foreach($this->gebaeude_nodes[0]->childNodes AS $child_node) {
+					$tag = strtolower($child_node->localName);
+					if ($tag == 'anlass') {
+						$ff_auftrag->set('gebaeude', $child_node->nodeValue);
 					}
 				}
 			}
-			$anlassarten = $ff->get('anlassarten');
-			if (!empty($anlassarten)) {
-				$ff->set('anlassart', $anlassarten[0]);
+
+			# speicher Auftrag
+			$ff_auftrag->update();
+
+			# Finde Flurstüecke und deren Anlässe
+			$this->flst_nodes = $this->getElementsByTagName('AX_Flurstueck');
+			$flurstuecke = array();
+			foreach($this->flst_nodes AS $flst_node) {
+				foreach($flst_node->childNodes AS $child_node) {
+					$tag = strtolower($child_node->localName);
+					if ($tag == 'flurstueckskennzeichen') {
+						$flst['flurstueckskennzeichen'] = $child_node->nodeValue;
+					}
+					if ($tag == 'anlass') {
+						$flst['anlass'] = $child_node->nodeValue;
+					}
+				}
+				$anlaesse[$flst['flurstueckskennzeichen']] = $flst['anlass'];
 			}
-			$ff->create();
-			$this->fortfuehrungsfaelle[] = $ff;
+
+			# Lösche vorhandene Fälle des Auftrages
+			$ff = new Fortfuehrungsfall($this->gui);
+			$ff->delete_by('ff_auftrag_id', $ff_auftrag->get('id'));
+
+			# Lege Fälle des Auftrages an
+			$this->fall_nodes = $this->getElementsByTagName('AX_Fortfuehrungsfall');
+			foreach($this->fall_nodes AS $fall_node) {
+				$ff = new Fortfuehrungsfall($this->gui);
+				$ff->set('ff_auftrag_id', $ff_auftrag->get('id'));
+				foreach($fall_node->childNodes AS $child_node) {
+					$tag = strtolower($child_node->localName);
+					if (in_array($tag, array(
+						'fortfuehrungsfallnummer',
+						'laufendenummer',
+						'ueberschriftimfortfuehrungsnachweis'
+					))) {
+						$ff->set($tag, $child_node->nodeValue);
+					}
+					if (in_array($tag, array(
+						'zeigtaufaltesflurstueck',
+						'zeigtaufneuesflurstueck'
+					))) {
+						$ff->set_array($tag, $child_node->nodeValue);
+						if ($tag == 'zeigtaufneuesflurstueck') {
+							$ff->set_array('anlassarten', $anlaesse[$child_node->nodeValue]);
+						}
+					}
+				}
+				$anlassarten = $ff->get('anlassarten');
+				if (!empty($anlassarten)) {
+					$ff->set('anlassart', $anlassarten[0]);
+				}
+				$ff->create();
+				$this->fortfuehrungsfaelle[] = $ff;
+			}
 		}
-		return $this->fortfuehrungsfaelle;
+		$result = array(
+			'success' => $success,
+			'err_msg' => $err_msg,
+			'fortfuehrungsfaelle' => $this->fortfuehrungsfaelle
+		);
+		return $result;
 	}
 
 	function nodeToArray( $dom, $node) {
