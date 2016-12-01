738443f9217f2afb764f8353e3a3074e7ff5aae1
Peter Korduan <peter.korduan@gdi-service.de>
2016-12-01 22:32:22 +0100

    dev-12 Delete double migration files.

--- plugins/fortfuehrungslisten/db/mysql/data/2016-11-21_17-26-22_menues.sql
+++
@@ -1,7 +0,0 @@
-SET @obermenue_id = LAST_INSERT_ID();
-SET @layer_id_ff_auftraege = 782;
-SET @layer_id_ff_faelle = 783;
-
-INSERT INTO `u_menues` (`name`, `name_low-german`, `name_english`, `name_polish`, `name_vietnamese`, `links`, `obermenue`, `menueebene`, `target`, `order`) VALUES
-('Fortführungsaufträge', NULL, NULL, NULL, NULL, concat('index.php?go=Layer-Suche_Suchen&selected_layer_id=', @layer_id_ff_aufraege, '&anzahl=30'), @obermenue_id, 2, NULL, 0),
-('Fortführungsfälle', NULL, NULL, NULL, NULL, concat('index.php?go=Layer-Suche_Suchen&selected_layer_id=', @layer_id_ff_faelle, '&anzahl=30'), @obermenue_id, 2, NULL, 0);
