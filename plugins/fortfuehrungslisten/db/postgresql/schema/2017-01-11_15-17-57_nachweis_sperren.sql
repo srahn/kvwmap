38a947b4ce3734d7c47770859a30fd5aaee7f3ce
Peter Korduan <peter.korduan@gdi-service.de>
2017-01-11 17:36:39 +0100

    VG-1 Fix nachweis sperren if new and old gesperrt true.

--- plugins/fortfuehrungslisten/db/postgresql/schema/2017-01-11_15-17-57_nachweis_sperren.sql
+++ plugins/fortfuehrungslisten/db/postgresql/schema/2017-01-11_15-17-57_nachweis_sperren.sql
@@ -4,7 +4,7 @@ ALTER TABLE fortfuehrungslisten.ff_auftraege ADD COLUMN gesperrt boolean NOT NUL
 
 CREATE OR REPLACE RULE r2_auftrag_sperren AS
     ON UPDATE TO fortfuehrungslisten.ff_auftraege
-   WHERE ( SELECT old.gesperrt ) DO INSTEAD  SELECT 'Die Bearbeitung des Fortführungsnachweises ist gesperrt.',
+   WHERE ( SELECT old.gesperrt AND new.gesperrt) DO INSTEAD  SELECT 'Die Bearbeitung des Fortführungsnachweises ist gesperrt.',
     'error' AS msg_type;
 COMMENT ON RULE r2_auftrag_sperren ON fortfuehrungslisten.ff_auftraege IS 'Die Regel prüft ob das Attribut an_sperren auf true steht. Wenn ja, wir die Änderung nicht gespeichert und statt dessen eine Fehlermeldung zurück geliefert.';
 
