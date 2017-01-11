0f0aaaa7749424fdfc3e8b941e51b77a43822c7a
Peter Korduan <peter.korduan@gdi-service.de>
2017-01-11 17:01:24 +0100

    VG-1 Add rule to block updates on ff_auftrage. Make wrong gemarkung only as warning.

--- plugins/fortfuehrungslisten/db/postgresql/schema/2017-01-06_17-19-00_auftragsdatei_pruefen.sql
+++ plugins/fortfuehrungslisten/db/postgresql/schema/2017-01-06_17-19-00_auftragsdatei_pruefen.sql
@@ -1,5 +1,8 @@
-CREATE OR REPLACE RULE auftragsdatei_pruefen AS
-    ON UPDATE TO fortfuehrungslisten.ff_auftraege
-   WHERE ( SELECT new.auftragsdatei != '' AND new.antragsnr::text <> "substring"(split_part(new.auftragsdatei::text, 'original_name='::text, 2), 1, 10)) DO INSTEAD  SELECT 'Die Antragsnummer im Dateinamen entspricht nicht der Antragsnummer im Formular oben.',
-    'error' AS msg_type;
-COMMENT ON RULE auftragsdatei_pruefen ON fortfuehrungslisten.ff_auftraege IS 'Die Regel prüft ob die ersten 10 Zeichen des Dateinamen der Auftragsdatei (auftragsdatei) mit der Antragsnummer (antragsnr) übereinstimmen. Überprüft wird beim Update und der Dateiname wird aus dem String auftragsdatei hinter dem Textbestandteil original_name= extrahiert.';
+BEGIN;
+
+CREATE OR REPLACE RULE r1_auftragsdatei_pruefen AS
+	ON UPDATE TO fortfuehrungslisten.ff_auftraege
+	WHERE ( SELECT new.auftragsdatei != '' AND new.antragsnr::text <> "substring"(split_part(new.auftragsdatei::text, 'original_name='::text, 2), 1, 10)) DO INSTEAD  SELECT 'Die Antragsnummer im Dateinamen entspricht nicht der Antragsnummer im Formular oben.', 'error' AS msg_type;
+COMMENT ON RULE r1_auftragsdatei_pruefen ON fortfuehrungslisten.ff_auftraege IS 'Die Regel prüft ob die ersten 10 Zeichen des Dateinamen der Auftragsdatei (auftragsdatei) mit der Antragsnummer (antragsnr) übereinstimmen. Überprüft wird beim Update und der Dateiname wird aus dem String auftragsdatei hinter dem Textbestandteil original_name= extrahiert.';
+
+COMMIT;
