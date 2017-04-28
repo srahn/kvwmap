b4bb0b5c4e91d91813515ebe06b70691534d8112
pkorduan <peter.korduan@gdi-service.de>
2017-04-28 09:59:02 +0200

    dev Fix format issues.

--- layouts/gui.php
+++ layouts/gui.php
@@ -20,29 +20,30 @@
 									include(LAYOUTPATH."snippets/".HEADER); ?>
 								</td>
 							</tr>
+							<tr>
+								<td id="menuebar" valign="top" align="center" background="<?php echo GRAPHICSPATH."bg.gif"; ?>" style="border: 1px solid; border-color: #FFFFFF #CCCCCC #CCCCCC; border-bottom: 0px"><?php
+									include(SNIPPETS . "menue.php"); ?>
+								</td>
+								<td align="center" width="100%" valign="top" background="<?php echo GRAPHICSPATH; ?>bg.gif" style="border-right: 1px solid; border-color: #FFFFFF #CCCCCC #CCCCCC;">
+									<div style=" position: relative; overflow: hidden; ">
+										<script type="text/javascript">
+											currentform = document.GUI;
+										</script><?php
+										$this->debug->write("<br>Include <b>".$this->main."</b> in gui.php",4);
+										if(file_exists($this->main)){
+											include($this->main);			# Pluginviews
+										}
+										else {
+											include(LAYOUTPATH."snippets/".$this->main);		# normale snippets
+										} ?>
+									</div>
+								</td>
+							</tr>
 							<tr> 
-							<td id="menuebar" valign="top" align="center" background="<?php echo GRAPHICSPATH."bg.gif"; ?>" style="border: 1px solid; border-color: #FFFFFF #CCCCCC #CCCCCC; border-bottom: 0px"> <?php
-								include(SNIPPETS . "menue.php"); ?>
-							</td>
-							<td align="center" width="100%" valign="top" background="<?php echo GRAPHICSPATH; ?>bg.gif" style="border-right: 1px solid; border-color: #FFFFFF #CCCCCC #CCCCCC;">
-								<div style=" position: relative; overflow: hidden; ">
-									<script type="text/javascript">
-									currentform = document.GUI;
-									</script><?php
-									$this->debug->write("<br>Include <b>".$this->main."</b> in gui.php",4);
-									if(file_exists($this->main)){
-										include($this->main);			# Pluginviews
-									}
-									else {
-										include(LAYOUTPATH."snippets/".$this->main);		# normale snippets
-									} ?>
-                </div>
-							</td>
-						</tr>
-						<tr> 
-							<td colspan="2" id="footer"><?php
-								$this->debug->write("<br>Include <b>".LAYOUTPATH."snippets/".FOOTER."</b> in gui.php",4);    
-								include(LAYOUTPATH."snippets/".FOOTER); ?></td>
+								<td colspan="2" id="footer"><?php
+									$this->debug->write("<br>Include <b>".LAYOUTPATH."snippets/".FOOTER."</b> in gui.php",4);    
+									include(LAYOUTPATH."snippets/".FOOTER); ?>
+								</td>
 							</tr>
 						</table>
 						<input type="hidden" name="overlayx" value="<? echo $this->user->rolle->overlayx; ?>">
