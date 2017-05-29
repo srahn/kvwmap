23047df33176e04f73834013749596b978f19778
Stefan Rahn <stefan.rahn@gdi-service.de>
2017-05-29 14:11:41 +0200

    <title> von gui_head.php in gui.php verschoben

--- layouts/snippets/gui_head.php
+++ layouts/snippets/gui_head.php
@@ -1,4 +1,3 @@
-<title><? echo TITLE; ?></title>
 <meta http-equiv=Content-Type content="text/html; charset=UTF-8">
 <script src="<?php echo JQUERY_PATH; ?>jquery-1.12.0.min.js"></script>
 <link rel="stylesheet" href="<?php echo FONTAWESOME_PATH; ?>css/font-awesome.min.css" type="text/css">
