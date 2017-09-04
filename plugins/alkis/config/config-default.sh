7b09f4f4a3d396aeeed451c5c41bbd8006aef963
Stefan Rahn <stefan.rahn@gdi-service.de>
2017-09-04 10:52:41 +0200

    import-nas.sh: PG-connection aus config.php

--- plugins/alkis/config/config-default.sh
+++ plugins/alkis/config/config-default.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-POSTGRES_HOST="pgsql"
-POSTGRES_PORT="5432"
-POSTGRES_USER="kvwmap"
-POSTGRES_PASSWORD="secret1"
-POSTGRES_DBNAME="kvwmapsp"
-POSTGRES_SCHEMA="alkis"
 
+POSTGRES_SCHEMA="alkis"
 GFS_TEMPLATE="../config/alkis-schema.gfs"
 DATA_PATH="/var/www/data/alkis/ff/eingang"
 IMPORT_PATH="/var/www/data/alkis/ff/import"
--- plugins/alkis/tools/import_nas.sh
+++ plugins/alkis/tools/import_nas.sh
@@ -3,51 +3,6 @@
 #############
 # functions
 
-convert_nas_files() {
-	find ${IMPORT_PATH}/NAS -iname '*.xml' | sort |  while read NAS_FILE ; do
-		NAS_FILE_=${NAS_FILE// /_} # ersetzt Leerzeichen durch _ in Dateiname
-		if [ ! "$NAS_FILE" = "$NAS_FILE_" ] ; then
-			echo "Benenne Datei ${NAS_FILE} um in ${NAS_FILE_}"
-			mv $NAS_FILE $NAS_FILE_
-			$NAS_FILE=$NAS_FILE_
-		fi
-		NAS_DIR=$(dirname "${NAS_FILE}")
-		NAS_FILENAME=${NAS_FILE##*/}
-		NAS_BASENAME=${NAS_FILENAME%.*}
-		SQL_FILE="${NAS_DIR}/${NAS_BASENAME}.sql"
-		GFS_FILE="${NAS_DIR}/${NAS_BASENAME}.gfs"
-		
-		check_dublicate=`psql -q -t -h $POSTGRES_HOST -U $POSTGRES_USER -c "SELECT count(*) FROM ${POSTGRES_SCHEMA}.import WHERE datei = '${NAS_FILENAME}' AND status='eingelesen';" $POSTGRES_DBNAME`
-		if [ ! $check_dublicate = 0 ] ; then
-			log "Die NAS-Datei ${NAS_FILENAME} wurde bereits eingelesen"
-			break
-		else
-			log "ogr2ogr konvertiert Datei: ${NAS_FILE}"
-		fi
-
-		${OGR_BINPATH}/ogr2ogr -f PGDump -append -a_srs EPSG:${EPSG_CODE} -nlt CONVERT_TO_LINEAR -lco SCHEMA=${POSTGRES_SCHEMA} -lco CREATE_SCHEMA=OFF -lco CREATE_TABLE=OFF --config PG_USE_COPY YES --config NAS_GFS_TEMPLATE "$SCRIPT_PATH/$GFS_TEMPLATE" --config NAS_NO_RELATION_LAYER YES ${SQL_FILE} ${NAS_FILE} > ${LOG_PATH}/${LOG_FILE} 2> ${LOG_PATH}/${ERROR_FILE}
-	
-		#/usr/local/gdal/bin/ogr2ogr -f PGDump -append -a_srs EPSG:25833 -nlt CONVERT_TO_LINEAR -lco SCHEMA=alkis -lco CREATE_SCHEMA=OFF -lco CREATE_TABLE=OFF --config PG_USE_COPY YES --config NAS_GFS_TEMPLATE "../config/alkis-schema.gfs" --config NAS_NO_RELATION_LAYER YES /var/www/data/alkis/ff/import/NAS/nba_landmv_lro_160112_1207von2024_288000_5986000.sql /var/www/data/alkis/ff/import/NAS/nba_landmv_lro_160112_1207von2024_288000_5986000.xml
-
-		if [ -n "$(grep -i 'Error\|Fehler' ${LOG_PATH}/${ERROR_FILE})" ] ; then
-			err "Fehler beim Einlesen der Datei: ${NAS_FILE}."
-			head -n 30 ${LOG_PATH}/${ERROR_FILE}
-			break
-		else
-			if [ ! -f "${IMPORT_PATH}/import_transaction.sql" ] ; then
-				echo "BEGIN; SET search_path = ${POSTGRES_SCHEMA},public;" > ${IMPORT_PATH}/import_transaction.sql
-			fi
-			sed -i -e "s/BEGIN;//g" -e "s/END;//g" -e "s/COMMIT;//g" ${SQL_FILE}
-			cat ${SQL_FILE} >> ${IMPORT_PATH}/import_transaction.sql
-echo "\." >> ${IMPORT_PATH}/import_transaction.sql		# ogr-Bug Workaround
-			echo "INSERT INTO ${POSTGRES_SCHEMA}.import (datei, status) VALUES ('${NAS_FILENAME}', 'eingelesen');" >> ${IMPORT_PATH}/import_transaction.sql
-			rm ${NAS_FILE}
-			rm ${SQL_FILE}
-			rm -f ${GFS_FILE}
-		fi
-	done
-}
-
 extract_zip_files() {
 	if [ ! "$(ls -A ${IMPORT_PATH})" ] ; then
 		if [ "$(ls -A $DATA_PATH)" ] ; then
@@ -67,6 +22,60 @@ extract_zip_files() {
 	fi
 }
 
+convert_nas_files() {
+	if [ "$(ls -A ${IMPORT_PATH})" ] ; then
+		find ${IMPORT_PATH}/NAS -iname '*.xml' | sort |  while read NAS_FILE ; do
+			NAS_FILE_=${NAS_FILE// /_} # ersetzt Leerzeichen durch _ in Dateiname
+			if [ ! "$NAS_FILE" = "$NAS_FILE_" ] ; then
+				echo "Benenne Datei ${NAS_FILE} um in ${NAS_FILE_}"
+				mv $NAS_FILE $NAS_FILE_
+				$NAS_FILE=$NAS_FILE_
+			fi
+			NAS_DIR=$(dirname "${NAS_FILE}")
+			NAS_FILENAME=${NAS_FILE##*/}
+			NAS_BASENAME=${NAS_FILENAME%.*}
+			SQL_FILE="${NAS_DIR}/${NAS_BASENAME}.sql"
+			GFS_FILE="${NAS_DIR}/${NAS_BASENAME}.gfs"
+			
+			check_dublicate=`psql -q -t -h $POSTGRES_HOST -U $POSTGRES_USER -c "SELECT count(*) FROM ${POSTGRES_SCHEMA}.import WHERE datei = '${NAS_FILENAME}' AND status='eingelesen';" $POSTGRES_DBNAME 2> ${LOG_PATH}/${ERROR_FILE}`
+			if [ -n "$(grep -i 'Error\|Fehler\|FATAL' ${LOG_PATH}/${ERROR_FILE})" ] ; then
+				err "Fehler beim Abfragen der import-Tabelle."
+				head -n 30 ${LOG_PATH}/${ERROR_FILE}
+				break
+			else
+				if [ ! $check_dublicate = 0 ] ; then
+					log "Die NAS-Datei ${NAS_FILENAME} wurde bereits eingelesen"
+					break
+				else
+					log "ogr2ogr konvertiert Datei: ${NAS_FILE}"
+				fi
+			fi
+
+			${OGR_BINPATH}/ogr2ogr -f PGDump -append -a_srs EPSG:${EPSG_CODE} -nlt CONVERT_TO_LINEAR -lco SCHEMA=${POSTGRES_SCHEMA} -lco CREATE_SCHEMA=OFF -lco CREATE_TABLE=OFF --config PG_USE_COPY YES --config NAS_GFS_TEMPLATE "$SCRIPT_PATH/$GFS_TEMPLATE" --config NAS_NO_RELATION_LAYER YES ${SQL_FILE} ${NAS_FILE} >> ${LOG_PATH}/${LOG_FILE} 2> ${LOG_PATH}/${ERROR_FILE}
+		
+			#/usr/local/gdal/bin/ogr2ogr -f PGDump -append -a_srs EPSG:25833 -nlt CONVERT_TO_LINEAR -lco SCHEMA=alkis -lco CREATE_SCHEMA=OFF -lco CREATE_TABLE=OFF --config PG_USE_COPY YES --config NAS_GFS_TEMPLATE "../config/alkis-schema.gfs" --config NAS_NO_RELATION_LAYER YES /var/www/data/alkis/ff/import/NAS/nba_landmv_lro_160112_1207von2024_288000_5986000.sql /var/www/data/alkis/ff/import/NAS/nba_landmv_lro_160112_1207von2024_288000_5986000.xml
+
+			if [ -n "$(grep -i 'Error\|Fehler\|FATAL' ${LOG_PATH}/${ERROR_FILE})" ] ; then
+				err "Fehler beim Konvertieren der Datei: ${NAS_FILE}."
+				head -n 30 ${LOG_PATH}/${ERROR_FILE}
+				break
+			else
+				if [ ! -f "${IMPORT_PATH}/import_transaction.sql" ] ; then
+					echo "BEGIN; SET search_path = ${POSTGRES_SCHEMA},public;" > ${IMPORT_PATH}/import_transaction.sql
+				fi
+				sed -i -e "s/BEGIN;//g" -e "s/END;//g" -e "s/COMMIT;//g" ${SQL_FILE}
+				cat ${SQL_FILE} >> ${IMPORT_PATH}/import_transaction.sql
+				echo "INSERT INTO ${POSTGRES_SCHEMA}.import (datei, status) VALUES ('${NAS_FILENAME}', 'eingelesen');" >> ${IMPORT_PATH}/import_transaction.sql
+				rm ${NAS_FILE}
+				rm ${SQL_FILE}
+				rm -f ${GFS_FILE}
+			fi
+		done
+	else
+		log "${IMPORT_PATH} ist leer, keine NAS-Dateien zum Konvertieren vorhanden"
+	fi
+}
+
 execute_sql_transaction() {
 	if [ ! "$(ls -A ${IMPORT_PATH}/NAS)" ] ; then
 		# ogr2ogr read all xml files successfully
@@ -74,22 +83,22 @@ execute_sql_transaction() {
 			# execute transaction sql file
 			log "Lese Transaktionsdatei ein"
 			echo "END;COMMIT;" >> ${IMPORT_PATH}/import_transaction.sql
-			psql -h $POSTGRES_HOST -U $POSTGRES_USER -f ${IMPORT_PATH}/import_transaction.sql $POSTGRES_DBNAME > ${LOG_PATH}/${LOG_FILE} 2> ${LOG_PATH}/${ERROR_FILE}
-			if [ -n "$(grep -i 'Error\|Fehler' ${LOG_PATH}/${ERROR_FILE})" ] ; then
+			psql -h $POSTGRES_HOST -U $POSTGRES_USER -f ${IMPORT_PATH}/import_transaction.sql $POSTGRES_DBNAME >> ${LOG_PATH}/${LOG_FILE} 2> ${LOG_PATH}/${ERROR_FILE}
+			if [ -n "$(grep -i 'Error\|Fehler\|FATAL' ${LOG_PATH}/${ERROR_FILE})" ] ; then
 				err "Fehler beim Einlesen der Transaktions-Datei: ${IMPORT_PATH}/import_transaction.sql."
 				head -n 30 ${LOG_PATH}/${ERROR_FILE}
 			else
 				log "Einlesevorgang erfolgreich"
 				clear_import_folder
 				log "Post-Processing wird ausgeführt"
-				psql -h $POSTGRES_HOST -U $POSTGRES_USER -c "SELECT ${POSTGRES_SCHEMA}.postprocessing();" $POSTGRES_DBNAME > > ${LOG_PATH}/${LOG_FILE} 2> ${LOG_PATH}/${ERROR_FILE}
-				if [ -n "$(grep -i 'Error\|Fehler' ${LOG_PATH}/${ERROR_FILE})" ] ; then
+				psql -h $POSTGRES_HOST -U $POSTGRES_USER -c "SELECT ${POSTGRES_SCHEMA}.postprocessing();" $POSTGRES_DBNAME >> ${LOG_PATH}/${LOG_FILE} 2> ${LOG_PATH}/${ERROR_FILE}
+				if [ -n "$(grep -i 'Error\|Fehler\|FATAL' ${LOG_PATH}/${ERROR_FILE})" ] ; then
 					err "Fehler beim Ausführen der Post-Processing-Funktion : ${POSTGRES_SCHEMA}.postprocessing()"
 					head -n 30 ${LOG_PATH}/${ERROR_FILE}
 				else
 					find ${POSTPROCESSING_PATH} -iname '*.sql' | sort |  while read PP_FILE ; do
-						psql -h $POSTGRES_HOST -U $POSTGRES_USER -f ${PP_FILE} $POSTGRES_DBNAME > ${LOG_PATH}/${LOG_FILE} 2> ${LOG_PATH}/${ERROR_FILE}
-						if [ -n "$(grep -i 'Error\|Fehler' ${LOG_PATH}/${ERROR_FILE})" ] ; then
+						psql -h $POSTGRES_HOST -U $POSTGRES_USER -f ${PP_FILE} $POSTGRES_DBNAME >> ${LOG_PATH}/${LOG_FILE} 2> ${LOG_PATH}/${ERROR_FILE}
+						if [ -n "$(grep -i 'Error\|Fehler\|FATAL' ${LOG_PATH}/${ERROR_FILE})" ] ; then
 							err "Fehler beim Ausführen der Post-Processing-Datei : ${PP_FILE}"
 						else
 							log "Post-Processing erfolgreich ausgeführt"
@@ -135,8 +144,17 @@ err() {
 # Load and set config params
 SCRIPT_PATH=$(dirname $(realpath $0))
 CONFIG_PATH=$(realpath ${SCRIPT_PATH}/../config)
+CONFIG_PHP=$(realpath ${SCRIPT_PATH}/../../../config.php)
+
 if [ -e "${CONFIG_PATH}/config.sh" ] ; then
   source ${CONFIG_PATH}/config.sh
+	POSTGRES_HOST=$(grep "'POSTGRES_HOST'" $CONFIG_PHP | cut -d"'" -f4)
+	POSTGRES_USER=$(grep "'POSTGRES_USER'" $CONFIG_PHP | cut -d"'" -f4)
+	POSTGRES_PASSWORD=$(grep "'POSTGRES_PASSWORD'" $CONFIG_PHP | cut -d"'" -f4)
+	POSTGRES_DBNAME=$(grep "'POSTGRES_DBNAME'" $CONFIG_PHP | cut -d"'" -f4)
+	if [ "$POSTGRES_DBNAME" = "" ] ; then
+		POSTGRES_DBNAME=$(grep "pgdbname=" $CONFIG_PHP | cut -d"'" -f2)
+	fi
   log "Konfigurationsdatei: ${CONFIG_PATH}/config.sh gelesen."
 	log " Starte Import ALKIS-Daten mit Script import_nas.sh"
   log "Loglevel: ${LOG_LEVEL}"
