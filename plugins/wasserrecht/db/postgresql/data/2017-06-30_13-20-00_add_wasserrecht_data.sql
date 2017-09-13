cd1ba56d7163845496a25c06268764d903e3ae65
Henry Rotzoll <h.rotzoll@dvz-mv.de>
2017-09-13 12:02:03 +0200

    wr weitere Gewässerbenutzung in 2017-06-30_13-20-00_add_wasserrecht_data.sql hinzugefügt

--- plugins/wasserrecht/db/postgresql/data/2017-06-30_13-20-00_add_wasserrecht_data.sql
+++ plugins/wasserrecht/db/postgresql/data/2017-06-30_13-20-00_add_wasserrecht_data.sql
@@ -220,6 +220,7 @@ INSERT INTO wasserrecht.fiswrv_gewaesserbenutzungen_lage(name, namekurz, namelan
 
 INSERT INTO wasserrecht.fiswrv_gewaesserbenutzungen(kennnummer, wasserbuchnummer, freitext_art, art, freitext_zweck, zweck, umfang, gruppe_wee, lage, wasserrechtliche_zulassungen) VALUES ('1-1-2-1', '3572', 'Test Freitext Art 1', 1, 'Test Freitext Zweck 1', 6, 1, false, 1, 1);
 INSERT INTO wasserrecht.fiswrv_gewaesserbenutzungen(kennnummer, wasserbuchnummer, freitext_art, art, freitext_zweck, zweck, umfang, gruppe_wee, lage, wasserrechtliche_zulassungen) VALUES ('2-1-2-2', '0000', 'Test Freitext Art 2', 2, 'Test Freitext Zweck 2', 6, 1, false, 1, 2);
+INSERT INTO wasserrecht.fiswrv_gewaesserbenutzungen(kennnummer, wasserbuchnummer, freitext_art, art, freitext_zweck, zweck, umfang, gruppe_wee, lage, wasserrechtliche_zulassungen) VALUES ('3-1-2-1', '3572', 'Test Freitext Art 3', 1, 'Test Freitext Zweck 3', 6, 1, false, 1, 3);
 
 INSERT INTO wasserrecht.fiswrv_teilgewaesserbenutzungen(art, zweck, umfang, wiedereinleitung_nutzer, wiedereinleitung_bearbeiter,mengenbestimmung,art_benutzung,befreiungstatbestaende,entgeltsatz,teilgewaesserbenutzungen_art, gewaesserbenutzungen) VALUES(1,1,2,true,true,3,2,false,2,2,1);
 
