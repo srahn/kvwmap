d11065f9624afcedc699a941aa26defc4e84a99f
Peter Korduan <peter.korduan@gdi-service.de>
2017-11-16 18:14:57 +0100

    fix last alkis migration.

--- plugins/alkis/db/postgresql/schema/2017-11-16_17-29-18_rename_description_to_AX_Datenerhebung_Punktort.sql
+++ plugins/alkis/db/postgresql/schema/2017-11-16_17-29-18_rename_description_to_AX_Datenerhebung_Punktort.sql
@@ -1,12 +1,12 @@
 BEGIN;
 
-ALTER TABLE alkis.ax_punktortau ALTER COLUMN processstep_description RENAME TO processstep_ax_datenerhebung_punktort;
-ALTER TABLE alkis.ax_punktortau ALTER COLUMN herkunft_source_source_description RENAME TO herkunft_source_source_ax_datenerhebung_punktort;
+ALTER TABLE alkis.ax_punktortau RENAME COLUMN processstep_description TO processstep_ax_datenerhebung_punktort;
+ALTER TABLE alkis.ax_punktortau RENAME COLUMN herkunft_source_source_description TO herkunft_source_source_ax_datenerhebung_punktort;
 
-ALTER TABLE alkis.ax_punktortag ALTER COLUMN processstep_description RENAME TO processstep_ax_datenerhebung_punktort;
-ALTER TABLE alkis.ax_punktortag ALTER COLUMN herkunft_source_source_description RENAME TO herkunft_source_source_ax_datenerhebung_punktort;
+ALTER TABLE alkis.ax_punktortag RENAME COLUMN processstep_description TO processstep_ax_datenerhebung_punktort;
+ALTER TABLE alkis.ax_punktortag RENAME COLUMN herkunft_source_source_description TO herkunft_source_source_ax_datenerhebung_punktort;
 
-ALTER TABLE alkis.ax_punktortta ALTER COLUMN processstep_description RENAME TO processstep_ax_datenerhebung_punktort;
-ALTER TABLE alkis.ax_punktortta ALTER COLUMN herkunft_source_source_description RENAME TO herkunft_source_source_ax_datenerhebung_punktort;
+ALTER TABLE alkis.ax_punktortta RENAME COLUMN processstep_description TO processstep_ax_datenerhebung_punktort;
+ALTER TABLE alkis.ax_punktortta RENAME COLUMN herkunft_source_source_description TO herkunft_source_source_ax_datenerhebung_punktort;
 
 COMMIT;
