cbad45abd8abff8b0733a852d9368740cf73674c
Henry Rotzoll <h.rotzoll@dvz-mv.de>
2017-11-28 15:26:15 +0100

    wr db/postgresql/data/2017-05-30_13-20-00_test_personen_adressen_korrigiert.sql mit Beispieldaten für Adressen und Personen hinzugefügt

--- plugins/wasserrecht/db/postgresql/data/2017-06-30_13-20-00_add_wasserrecht_data.sql
+++ plugins/wasserrecht/db/postgresql/data/2017-06-30_13-20-00_add_wasserrecht_data.sql
@@ -3,10 +3,10 @@ BEGIN;
 --INSERT INTO spatial_ref_sys VALUES (35833, 'EPSG', 35833, 'PROJCS["ETRS89/UTM 33N RW+33.000.000 MV",GEOGCS["ETRS89",DATUM["European_Terrestrial_Reference_System_1989",SPHEROID["GRS1980",6378137,298.257222101,AUTHORITY["EPSG","7019"]],AUTHORITY["EPSG","6258"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.01745329251994328,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4258"]],PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",0],PARAMETER["central_meridian",15],PARAMETER["scale_factor",0.9996],PARAMETER["false_easting",33500000],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AUTHORITY["EPSG","25833"]]', '+proj=tmerc +lat_0=0 +lon_0=15 +k=0.9996 +x_0=33500000 +y_0=0 +ellps=GRS80 +units=m +no_defs towgs84=0,0,0<>');
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-INSERT INTO wasserrecht.fiswrv_adresse (strasse, hausnummer, plz, ort) VALUES ('Lübecker Str.', '283', 19059, 'Schwerin');
-INSERT INTO wasserrecht.fiswrv_adresse (strasse, hausnummer, plz, ort) VALUES ('Marienplatz', '1', 19053, 'Schwerin');
-INSERT INTO wasserrecht.fiswrv_adresse (strasse, hausnummer, plz, ort) VALUES ('Am Wall', '3-5', 18273, 'Güstrow');
-INSERT INTO wasserrecht.fiswrv_adresse (strasse, hausnummer, plz, ort) VALUES ('Putlitzer Str.', '25', 19370, 'Parchim');
+INSERT INTO wasserrecht.fiswrv_adresse (id, strasse, hausnummer, plz, ort) VALUES (9001, 'Lübecker Str.', '283', 19059, 'Schwerin');
+INSERT INTO wasserrecht.fiswrv_adresse (id, strasse, hausnummer, plz, ort) VALUES (9002, 'Marienplatz', '1', 19053, 'Schwerin');
+INSERT INTO wasserrecht.fiswrv_adresse (id, strasse, hausnummer, plz, ort) VALUES (9003, 'Am Wall', '3-5', 18273, 'Güstrow');
+INSERT INTO wasserrecht.fiswrv_adresse (id, strasse, hausnummer, plz, ort) VALUES (9004, 'Putlitzer Str.', '25', 19370, 'Parchim');
 INSERT INTO wasserrecht.fiswrv_konto (name, iban, bic, bankname, verwendungszweck, personenkonto, kassenzeichen) VALUES ('Testkonto 1', 'DE 124455678990', '123456789', 'Dt. Bundesbank, Filiale Rostock', 'Test Verwendungszweck 1', 'r12345678', '12345');
 INSERT INTO wasserrecht.fiswrv_konto (name, iban, bic, bankname, verwendungszweck, personenkonto, kassenzeichen) VALUES ('Testkonto 2', 'DE 098765432100', '987654332', 'Sparkasse', 'Test Verwendungszweck 2', 'r98765432', '98765');
 
@@ -19,8 +19,8 @@ INSERT INTO wasserrecht.fiswrv_mengenbestimmung (name) VALUES ('Schätzung');
 
 INSERT INTO wasserrecht.fiswrv_behoerde_art(name, abkuerzung) VALUES ('Untere Wasserbehörde', 'UWB');
 INSERT INTO wasserrecht.fiswrv_behoerde_art(name, abkuerzung) VALUES ('Staatliches Amt für Landwirtschaft und Umwelt', 'StALU');
-INSERT INTO wasserrecht.fiswrv_behoerde (name, abkuerzung, art, adresse, konto) VALUES ('Landrat Landkreis Rostock', 'LR LRO', 1, 3, 1);
-INSERT INTO wasserrecht.fiswrv_behoerde (name, abkuerzung, art, adresse, konto) VALUES ('Landrat Ludwigslust-Parchim', 'LR LUP', 1, 4, 2);
+INSERT INTO wasserrecht.fiswrv_behoerde (name, abkuerzung, art, adresse, konto) VALUES ('Landrat Landkreis Rostock', 'LR LRO', 1, 9003, 1);
+INSERT INTO wasserrecht.fiswrv_behoerde (name, abkuerzung, art, adresse, konto) VALUES ('Landrat Ludwigslust-Parchim', 'LR LUP', 1, 9004, 2);
 INSERT INTO wasserrecht.fiswrv_behoerde (name, abkuerzung, art) VALUES ('Landrat Mecklenburgische Seenplatte', 'LR MSE', 1);
 INSERT INTO wasserrecht.fiswrv_behoerde (name, abkuerzung, art) VALUES ('Landrat Nordwestmecklenburg', 'LR NWM', 1);
 INSERT INTO wasserrecht.fiswrv_behoerde (name, abkuerzung, art) VALUES ('Landrat Vorpommern-Greifswald', 'LR VG', 1);
@@ -179,19 +179,19 @@ INSERT INTO wasserrecht.fiswrv_koerperschaft (name, art) VALUES ('MUSTER ABWASSE
 INSERT INTO wasserrecht.fiswrv_koerperschaft (name, art) VALUES ('MUSTER TRINKWASSER KOERPERSCHAFT', 1);
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-INSERT INTO wasserrecht.fiswrv_personen (name, abkuerzung, behoerde, adresse, wrzadressat) VALUES ('MAX MUSTERMANN', 'MM', 1, 1, 'ja');
-INSERT INTO wasserrecht.fiswrv_personen (name, abkuerzung, behoerde, adresse, wrzadressat) VALUES ('FRAU MUSTERMANN', 'FM', 2, 2, 'ja');
-INSERT INTO wasserrecht.fiswrv_personen (name, abkuerzung, behoerde, adresse, wrzadressat) VALUES ('TEST ADRESSAT', 'TA', 1, 3, 'ja');
-INSERT INTO wasserrecht.fiswrv_personen (name, abkuerzung, bearbeiter) VALUES ('MUSTER BEARBEITER', 'MB', 'ja');
-INSERT INTO wasserrecht.fiswrv_personen (name, abkuerzung, betreiber) VALUES ('MUSTER BETREIBER', 'MB', 'ja');
-INSERT INTO wasserrecht.fiswrv_personen (name, abkuerzung, wrzaussteller) VALUES ('MUSTER WRZ AUSSTELLER', 'MWA', 'ja');
-INSERT INTO wasserrecht.fiswrv_personen (name, abkuerzung, betreiber) VALUES ('Agargenossenschaft Holldorf', 'AH', 'ja');
-INSERT INTO wasserrecht.fiswrv_personen (name, abkuerzung, betreiber) VALUES ('Boddenland', 'BL', 'ja');
-INSERT INTO wasserrecht.fiswrv_personen (name, abkuerzung, betreiber) VALUES ('Danisco Sugar GmbH', 'DSG', 'ja');
-INSERT INTO wasserrecht.fiswrv_personen (name, abkuerzung, betreiber) VALUES ('Denissen Landwirtschaft Wöbbelin', 'DLW', 'ja');
-INSERT INTO wasserrecht.fiswrv_personen (name, abkuerzung, betreiber) VALUES ('DMK Altentreptow', 'DA', 'ja');
-INSERT INTO wasserrecht.fiswrv_personen (name, abkuerzung, betreiber) VALUES ('EURAWASSER Nord', 'EN', 'ja');
-INSERT INTO wasserrecht.fiswrv_personen (name, abkuerzung, betreiber) VALUES ('Gartenbau Warin', 'GW', 'ja');
+INSERT INTO wasserrecht.fiswrv_personen (id, name, abkuerzung, behoerde, adresse, wrzadressat) VALUES (9001, 'MAX MUSTERMANN', 'MM', 1, 9001, 'ja');
+INSERT INTO wasserrecht.fiswrv_personen (id, name, abkuerzung, behoerde, adresse, wrzadressat) VALUES (9002, 'FRAU MUSTERMANN', 'FM', 2, 9002, 'ja');
+INSERT INTO wasserrecht.fiswrv_personen (id, name, abkuerzung, behoerde, adresse, wrzadressat) VALUES (9003, 'TEST ADRESSAT', 'TA', 1, 9003, 'ja');
+INSERT INTO wasserrecht.fiswrv_personen (id, name, abkuerzung, bearbeiter) VALUES (9004, 'MUSTER BEARBEITER', 'MB', 'ja');
+INSERT INTO wasserrecht.fiswrv_personen (id, name, abkuerzung, betreiber) VALUES (9005, 'MUSTER BETREIBER', 'MB', 'ja');
+INSERT INTO wasserrecht.fiswrv_personen (id, name, abkuerzung, wrzaussteller) VALUES (9006, 'MUSTER WRZ AUSSTELLER', 'MWA', 'ja');
+INSERT INTO wasserrecht.fiswrv_personen (id, name, abkuerzung, betreiber) VALUES (9007, 'Agargenossenschaft Holldorf', 'AH', 'ja');
+INSERT INTO wasserrecht.fiswrv_personen (id, name, abkuerzung, betreiber) VALUES (9008, 'Boddenland', 'BL', 'ja');
+INSERT INTO wasserrecht.fiswrv_personen (id, name, abkuerzung, betreiber) VALUES (9009, 'Danisco Sugar GmbH', 'DSG', 'ja');
+INSERT INTO wasserrecht.fiswrv_personen (id, name, abkuerzung, betreiber) VALUES (9010, 'Denissen Landwirtschaft Wöbbelin', 'DLW', 'ja');
+INSERT INTO wasserrecht.fiswrv_personen (id, name, abkuerzung, betreiber) VALUES (9011, 'DMK Altentreptow', 'DA', 'ja');
+INSERT INTO wasserrecht.fiswrv_personen (id, name, abkuerzung, betreiber) VALUES (9012, 'EURAWASSER Nord', 'EN', 'ja');
+INSERT INTO wasserrecht.fiswrv_personen (id, name, abkuerzung, betreiber) VALUES (9013, 'Gartenbau Warin', 'GW', 'ja');
 
 INSERT INTO wasserrecht.fiswrv_personen_status (name) VALUES ('[aktuell]');
 INSERT INTO wasserrecht.fiswrv_personen_status (name) VALUES ('[historisch 1994–2011]');
@@ -209,7 +209,7 @@ INSERT INTO wasserrecht.fiswrv_anlagen_klasse (name) VALUES ('Wasserwerk');
 INSERT INTO wasserrecht.fiswrv_anlagen_klasse (name) VALUES ('Landwirtschaftsbetrieb');
 INSERT INTO wasserrecht.fiswrv_anlagen_klasse (name) VALUES ('Sport- und Erhohlungsanlage');
 
-INSERT INTO wasserrecht.fiswrv_anlagen (name, klasse, zustaend_uwb, zustaend_stalu, betreiber, abwasser_koerperschaft, trinkwasser_koerperschaft,kommentar,the_geom) VALUES ('Musterholzwerk Musterstadt', 1, 1, 1, 4, 1, 2, NULL, ST_Transform(ST_GeomFromText('POINT(12 54)', 4326), 35833));
+INSERT INTO wasserrecht.fiswrv_anlagen (name, klasse, zustaend_uwb, zustaend_stalu, betreiber, abwasser_koerperschaft, trinkwasser_koerperschaft,kommentar,the_geom) VALUES ('Musterholzwerk Musterstadt', 1, 1, 1, 9005, 1, 2, NULL, ST_Transform(ST_GeomFromText('POINT(12 54)', 4326), 35833));
 INSERT INTO wasserrecht.fiswrv_anlagen (name, klasse, zustaend_uwb, zustaend_stalu, abwasser_koerperschaft, trinkwasser_koerperschaft,kommentar,the_geom) VALUES ('Wasserwerk Musterstadt', 3, 5, 2, 1, 2, NULL, ST_Transform(ST_GeomFromText('POINT(12 53.5)', 4326), 35833));
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -217,12 +217,12 @@ INSERT INTO wasserrecht.fiswrv_ort (name) VALUES ('Güstrow');
 INSERT INTO wasserrecht.fiswrv_ort (name) VALUES ('Rostock');
 INSERT INTO wasserrecht.fiswrv_ort (name) VALUES ('Hansestadt Rostock');
 
-INSERT INTO wasserrecht.fiswrv_wasserrechtliche_zulassungen (typus, aktenzeichen, ort, datum, ausstellbehoerde, zustaendige_behoerde, status, gueltig_seit, befristet_bis, aktuell, bearbeiter, adressat, anlage, fassung_nummer, fassung_datum) VALUES (1, '§ 1', 1, current_date, 1, 1, 2, '2016-07-01', '2017-07-06', true, 2, 2, 1, 1, '2017-07-01');
-INSERT INTO wasserrecht.fiswrv_wasserrechtliche_zulassungen (typus, aktenzeichen, ort, datum, ausstellbehoerde, zustaendige_behoerde, status, gueltig_seit, befristet_bis, aktuell, bearbeiter, adressat, anlage) VALUES (1, '§ 2', 1, current_date, 1, 1, 2, '2017-07-01', '2017-07-06', false, 2, 2, 2);
-INSERT INTO wasserrecht.fiswrv_wasserrechtliche_zulassungen (typus, aktenzeichen, ort, datum, ausstellbehoerde, zustaendige_behoerde, status, gueltig_seit, befristet_bis, aktuell, bearbeiter, adressat, anlage) VALUES (1, '§ 3', 1, current_date, 2, 2, 2, '2017-07-01', '2017-07-06', false, 2, 1, 2);
-INSERT INTO wasserrecht.fiswrv_wasserrechtliche_zulassungen (typus, aktenzeichen, ort, datum, ausstellbehoerde, zustaendige_behoerde, status, gueltig_seit, befristet_bis, aktuell, bearbeiter, adressat, anlage) VALUES (1, '§ 4', 1, current_date, 2, 2, 2, '2017-07-01', '2017-12-31', false, 2, 1, 1);
-INSERT INTO wasserrecht.fiswrv_wasserrechtliche_zulassungen (typus, aktenzeichen, ort, datum, ausstellbehoerde, zustaendige_behoerde, status, gueltig_seit, aktuell, bearbeiter, adressat, anlage) VALUES (1, '§ 5', 1, current_date, 1, 1, 2, '2017-07-01', false, 2, 2, 1);
-INSERT INTO wasserrecht.fiswrv_wasserrechtliche_zulassungen (typus, aktenzeichen, ort, datum, ausstellbehoerde, zustaendige_behoerde, status, gueltig_seit, befristet_bis, aktuell, bearbeiter, adressat, anlage, fassung_nummer, fassung_datum) VALUES (1, '§ 6', 1, current_date, 1, 1, 2, '2016-01-01', '2019-07-06', true, 2, 3, 1, 15, '2017-09-01');
+INSERT INTO wasserrecht.fiswrv_wasserrechtliche_zulassungen (typus, aktenzeichen, ort, datum, ausstellbehoerde, zustaendige_behoerde, status, gueltig_seit, befristet_bis, aktuell, bearbeiter, adressat, anlage, fassung_nummer, fassung_datum) VALUES (1, '§ 1', 1, current_date, 1, 1, 2, '2016-07-01', '2017-07-06', true, 9002, 9002, 1, 1, '2017-07-01');
+INSERT INTO wasserrecht.fiswrv_wasserrechtliche_zulassungen (typus, aktenzeichen, ort, datum, ausstellbehoerde, zustaendige_behoerde, status, gueltig_seit, befristet_bis, aktuell, bearbeiter, adressat, anlage) VALUES (1, '§ 2', 1, current_date, 1, 1, 2, '2017-07-01', '2017-07-06', false, 9002, 9002, 2);
+INSERT INTO wasserrecht.fiswrv_wasserrechtliche_zulassungen (typus, aktenzeichen, ort, datum, ausstellbehoerde, zustaendige_behoerde, status, gueltig_seit, befristet_bis, aktuell, bearbeiter, adressat, anlage) VALUES (1, '§ 3', 1, current_date, 2, 2, 2, '2017-07-01', '2017-07-06', false, 9002, 9001, 2);
+INSERT INTO wasserrecht.fiswrv_wasserrechtliche_zulassungen (typus, aktenzeichen, ort, datum, ausstellbehoerde, zustaendige_behoerde, status, gueltig_seit, befristet_bis, aktuell, bearbeiter, adressat, anlage) VALUES (1, '§ 4', 1, current_date, 2, 2, 2, '2017-07-01', '2017-12-31', false, 9002, 9001, 1);
+INSERT INTO wasserrecht.fiswrv_wasserrechtliche_zulassungen (typus, aktenzeichen, ort, datum, ausstellbehoerde, zustaendige_behoerde, status, gueltig_seit, aktuell, bearbeiter, adressat, anlage) VALUES (1, '§ 5', 1, current_date, 1, 1, 2, '2017-07-01', false, 9002, 2, 1);
+INSERT INTO wasserrecht.fiswrv_wasserrechtliche_zulassungen (typus, aktenzeichen, ort, datum, ausstellbehoerde, zustaendige_behoerde, status, gueltig_seit, befristet_bis, aktuell, bearbeiter, adressat, anlage, fassung_nummer, fassung_datum) VALUES (1, '§ 6', 1, current_date, 1, 1, 2, '2016-01-01', '2019-07-06', true, 9002, 9003, 1, 15, '2017-09-01');
 
 UPDATE wasserrecht.fiswrv_wasserrechtliche_zulassungen SET freigegeben = true WHERE id = 1;
 UPDATE wasserrecht.fiswrv_wasserrechtliche_zulassungen SET freigegeben = true WHERE id = 2;
