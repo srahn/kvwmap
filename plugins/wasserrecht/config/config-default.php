3fd396594a946f27c564ea31e577fe0095f1bb74
Henry Rotzoll <h.rotzoll@dvz-mv.de>
2018-01-08 16:05:42 +0100

    wr wr URL Pfade zu den erstellten Bescheid Word Dateien auch in der config-default.php korrigiert

--- plugins/wasserrecht/config/config-default.php
+++ plugins/wasserrecht/config/config-default.php
@@ -1,9 +1,9 @@
 <?php
-// define('WASSERRECHT_DOCUMENT_PATH', SHAPEPATH . 'wasserrecht/');
-// define('WASSERRECHT_DOCUMENT_URL_PATH', '/../../../data/wasserrecht/');
+define('WASSERRECHT_DOCUMENT_PATH', SHAPEPATH . 'wasserrecht/');
+define('WASSERRECHT_DOCUMENT_URL_PATH', '/../../../data/wasserrecht/');
 
-define('WASSERRECHT_DOCUMENT_PATH', PLUGINS . 'wasserrecht/results/');
-define('WASSERRECHT_DOCUMENT_URL_PATH', '/../plugins/wasserrecht/results/');
+// define('WASSERRECHT_DOCUMENT_PATH', PLUGINS . 'wasserrecht/results/');
+// define('WASSERRECHT_DOCUMENT_URL_PATH', '/../plugins/wasserrecht/results/');
 
 define('WASSERRECHT_ERKLAERUNG_ENTNAHME_TEILGEWAESSERBENUTZUNGEN_COUNT', 6);
 define('WASSERRECHT_ERKLAERUNG_ENTNAHME_TEILGEWAESSERBENUTZUNGEN_ENTGELTSATZ', 2);
