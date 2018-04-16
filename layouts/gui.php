41221da05a743539b52f2961c98f3672f7c64787
Stefan Rahn <stefan.rahn@gdi-service.de>
2018-04-16 10:20:16 +0200

    letzten commit rückgängig gemacht

--- layouts/gui.php
+++ layouts/gui.php
@@ -27,7 +27,7 @@
 									include(SNIPPETS . "menue.php"); ?>
 								</td>
 								<td align="center" width="100%" height="100%" valign="top" background="<?php echo GRAPHICSPATH; ?>bg.gif" style="border-right: 1px solid; border-color: #FFFFFF #CCCCCC #CCCCCC;">
-									<div style="position: relative;">
+									<div style="height:100%; position: relative; overflow: hidden; ">		<!-- overflow wird für rausfliegende Legende benötigt und height:100% für den Box-Shadow unter der MapFunctionsBar und Legende -->
 										<script type="text/javascript">
 											currentform = document.GUI;
 											<? $this->currentform = 'document.GUI'; ?>
