f2e296f94117c60c90c75ee4a295575317239f63
Stefan Rahn <stefan.rahn@gdi-service.de>
2018-04-17 13:21:01 +0200

    Encoding und Formating

--- end.php
+++ end.php
@@ -5,14 +5,14 @@ if(!empty($GUI->allowed_documents)){
 	$GUI->write_document_loader();
 }
 
-# Schließen der offenen Datenbankverbindungen
+# SchlieÃŸen der offenen Datenbankverbindungen
 if (isset($user)) {
   if ($user->database->dbConn>0) { $user->database->close(); }
 }  
 if ($GUI->database->dbConn>0) { $GUI->database->close(); }
 if ($GUI->pgdatabase->dbConn>0) { $GUI->pgdatabase->close(); } 
 if (DEBUG_LEVEL>0) { $debug->close(); }
-# Schließen des Postgres Logfiles
+# SchlieÃŸen des Postgres Logfiles
 if (LOG_LEVEL>0){
   $log_postgres->close();
 }
