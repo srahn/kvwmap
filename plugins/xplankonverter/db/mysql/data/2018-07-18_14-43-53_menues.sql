6faaff728a0a48269247aeddbf54eb4af01820c0
pkorduan <peter.korduan@gdi-service.de>
2018-07-18 16:06:51 +0200

    fix menues in xplankonverter plugin.

--- plugins/xplankonverter/db/mysql/data/2018-07-18_14-43-53_menues.sql
+++ plugins/xplankonverter/db/mysql/data/2018-07-18_14-43-53_menues.sql
@@ -6,4 +6,4 @@ INSERT INTO `u_menues` (`name`, `name_low-german`, `name_english`, `name_polish`
 ('B-Pläne', '', '', '', '', 'index.php?go=xplankonverter_plaene_index&planart=BP-Plan', '', @obermenue_id, 2, '', 20, '', ''),
 ('F-Pläne', '', '', '', '', 'index.php?go=xplankonverter_plaene_index&planart=FP-Plan', '', @obermenue_id, 2, '', 22, '', ''),
 ('SO-Pläne', '', '', '', '', 'index.php?go=xplankonverter_plaene_index&planart=SO-Plan', '', @obermenue_id, 2, '', 24, '', ''),
-('R-Pläne', '', '', '', '', 'index.php?go=plankonverter_plaene_index&planart=RP-Plan', '', @obermenue_id, 2, '', 26, '', '');
+('R-Pläne', '', '', '', '', 'index.php?go=xplankonverter_plaene_index&planart=RP-Plan', '', @obermenue_id, 2, '', 26, '', '');
