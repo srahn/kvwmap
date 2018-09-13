047bc95f892723f1c52ca2df0e0881a5dcb9beae
Stefan Rahn <stefan.rahn@gdi-service.de>
2018-09-13 16:17:21 +0200

    Bugs IE

--- funktionen/gui_functions.php
+++ funktionen/gui_functions.php
@@ -11,8 +11,24 @@ function Bestaetigung(link,text) {
 	}
 }
 
+// closest() for IE
+if (!Element.prototype.matches)
+    Element.prototype.matches = Element.prototype.msMatchesSelector || 
+                                Element.prototype.webkitMatchesSelector;
+
+if (!Element.prototype.closest)
+    Element.prototype.closest = function(s) {
+        var el = this;
+        if (!document.documentElement.contains(el)) return null;
+        do {
+            if (el.matches(s)) return el;
+            el = el.parentElement || el.parentNode;
+        } while (el !== null && el.nodeType === 1); 
+        return null;
+    };
+
 function roundNumber(num, scale){
-  if(!("" + num).includes("e")) {
+  if(!("" + num).indexOf("e") != -1) {
     return +(Math.round(num + "e+" + scale)  + "e-" + scale);  
   } else {
     var arr = ("" + num).split("e");
