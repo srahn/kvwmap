330d4491ef78c388245b24e08b7356928f727acb
Stefan Rahn <stefan.rahn@gdi-service.de>
2018-09-13 16:16:46 +0200

    Daten-Export: jetzt ohne temp-Tabelle

--- class/data_import_export.php
+++ class/data_import_export.php
@@ -798,7 +798,8 @@ class data_import_export {
 		$command = 'export PGDATESTYLE="ISO, MDY";export PGCLIENTENCODING=UTF-8;'.OGR_BINPATH.'ogr2ogr -f '.$exportformat.' -lco ENCODING=UTF-8 -sql "'.$sql.'" '.$exportfile.' PG:"dbname='.$layerdb->dbName.' user='.$layerdb->user;
 		if($layerdb->passwd != '')$command.= ' password='.$layerdb->passwd;
 		if($layerdb->port != '')$command.=' port='.$layerdb->port;
-		if($layerdb->host != '') $command .= ' host=' . $layerdb->host;
+		if($layerdb->host != '')$command .= ' host=' . $layerdb->host;
+		if($layerdb->schema != '')$command .= ' active_schema='.$layerdb->schema;
 		exec($command.'"');
 	}
 
@@ -1066,9 +1067,9 @@ class data_import_export {
 		$data_sql = $sql;
 		#echo $sql;
 
-    $temp_table = 'shp_export_'.rand(1, 10000);
-    $sql = 'CREATE TABLE public.'.$temp_table.' AS '.$sql;		# temporäre Tabelle erzeugen, damit das/die Schema/ta berücksichtigt werden
-    $ret = $layerdb->execSQL($sql,4, 0);
+    #$temp_table = 'shp_export_'.rand(1, 10000);
+    #$sql = 'CREATE TABLE public.'.$temp_table.' AS '.$sql;		# temporäre Tabelle erzeugen, damit das/die Schema/ta berücksichtigt werden
+    #$ret = $layerdb->execSQL($sql,4, 0);
 
 		for($s = 0; $s < count($selected_attributes); $s++){
 			# Transformieren der Geometrie
@@ -1078,9 +1079,8 @@ class data_import_export {
 				if (in_array($selected_attr_types[$s], array('text', 'varchar'))) $selected_attributes[$s] = $selected_attributes[$s].'::varchar(254)';
 			}
 		}
-		$sql = "
-			SELECT " . implode(', ', $selected_attributes) . " FROM public." . $temp_table . "
-		"; # auf die ausgewählten Attribute einschränken
+		#$sql = "SELECT " . implode(', ', $selected_attributes) . " FROM public." . $temp_table; # auf die ausgewählten Attribute einschränken
+		$sql = "SELECT " . implode(', ', $selected_attributes) . " FROM (".$sql.") as foo"; # auf die ausgewählten Attribute einschränken
 		$ret = $layerdb->execSQL($sql,4, 0);
 		if (!$ret[0]) {
 			$count = pg_num_rows($ret[1]);
@@ -1209,9 +1209,9 @@ class data_import_export {
 				$contenttype = 'application/octet-stream';
 			}
 			# temp. Tabelle wieder löschen
-			$sql = 'DROP TABLE '.$temp_table;
-			$ret = $layerdb->execSQL($sql,4, 0);
-			if($this->formvars['export_format'] != 'CSV')$user->rolle->setConsumeShape($currenttime,$this->formvars['selected_layer_id'],$count);
+			// $sql = 'DROP TABLE '.$temp_table;
+			// $ret = $layerdb->execSQL($sql,4, 0);
+			// if($this->formvars['export_format'] != 'CSV')$user->rolle->setConsumeShape($currenttime,$this->formvars['selected_layer_id'],$count);
 
 			ob_end_clean();
 			header('Content-type: '.$contenttype);
