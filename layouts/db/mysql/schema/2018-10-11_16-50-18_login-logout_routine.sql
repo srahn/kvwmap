b4810b48fee0439ad3e24beea0ad1951fbc2c474
Stefan Rahn <stefan.rahn@gdi-service.de>
2018-10-11 17:09:54 +0200

    login_routine

--- layouts/db/mysql/schema/2018-10-11_16-50-18_login-logout_routine.sql
+++ layouts/db/mysql/schema/2018-10-11_16-50-18_login-logout_routine.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 INSERT INTO `config` (`name`, `prefix`, `value`, `description`, `type`, `group`, `plugin`, `saved`) VALUES 
-('LOGIN_ROUTINE', '', '', 'hier kann eine PHP-Datei angegeben werden, welche beim Login-Vorgang ausgeführt wird', 'string', 'Administration', NULL, 0),
-('LOGOUT_ROUTINE', '', '', 'hier kann eine PHP-Datei angegeben werden, welche beim Logout-Vorgang ausgeführt wird', 'string', 'Administration', NULL, 0);
+('LOGIN_ROUTINE', 'SNIPPETS', '', 'hier kann eine PHP-Datei angegeben werden, welche beim Login-Vorgang ausgeführt wird', 'string', 'Administration', NULL, 0),
+('LOGOUT_ROUTINE', 'SNIPPETS', '', 'hier kann eine PHP-Datei angegeben werden, welche beim Logout-Vorgang ausgeführt wird', 'string', 'Administration', NULL, 0);
 
 COMMIT;
