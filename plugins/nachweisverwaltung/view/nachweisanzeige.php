2708500810c3f59d90622d1b0e42105767488795
Stefan Rahn <stefan.rahn@gdi-service.de>
2018-11-13 11:25:19 +0100

    Bugfix 2.9.25

--- plugins/nachweisverwaltung/view/nachweisanzeige.php
+++ plugins/nachweisverwaltung/view/nachweisanzeige.php
@@ -5,7 +5,8 @@ var nachweise = new Array();
 
 <?
 	for($i = 0; $i < count($this->nachweis->Dokumente); $i++){
-		echo "nachweise.push(JSON.parse('".json_encode($this->nachweis->Dokumente[$i])."'));";
+		$json = str_replace('\\r', '\\\r', str_replace('\\n', '\\\n', json_encode($this->nachweis->Dokumente[$i])));
+		echo "nachweise.push(JSON.parse('".$json."'));\n";
 	}
 ?>
 
