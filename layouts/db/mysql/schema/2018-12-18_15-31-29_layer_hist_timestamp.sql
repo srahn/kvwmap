a1942e482b4ebabc637c526bdc18752c4f523063
Stefan Rahn <stefan.rahn@gdi-service.de>
2018-12-19 14:48:56 +0100

    Bug im Bugfix 2.9.55

--- layouts/db/mysql/schema/2018-12-18_15-31-29_layer_hist_timestamp.sql
+++ layouts/db/mysql/schema/2018-12-18_15-31-29_layer_hist_timestamp.sql
@@ -1,4 +1,6 @@
 BEGIN;
 
+UPDATE `layer` SET Pfad = REPLACE(Pfad, "CASE WHEN '$hist_timestamp' = '' THEN endet IS NULL ELSE beginnt::text <= '$hist_timestamp' and ('$hist_timestamp' < endet::text or endet IS NULL) END", "CASE WHEN '$hist_timestamp' = '' THEN endet IS NULL ELSE to_char(beginnt, 'YYYY-MM-DD HH:MI:SS') <= '$hist_timestamp' and ('$hist_timestamp' < to_char(endet, 'YYYY-MM-DD HH:MI:SS') or endet IS NULL) END"),
+Data = REPLACE(DATA, "CASE WHEN '$hist_timestamp' = '' THEN endet IS NULL ELSE beginnt::text <= '$hist_timestamp' and ('$hist_timestamp' < endet::text or endet IS NULL) END", "CASE WHEN '$hist_timestamp' = '' THEN endet IS NULL ELSE to_char(beginnt, 'YYYY-MM-DD HH:MI:SS') <= '$hist_timestamp' and ('$hist_timestamp' < to_char(endet, 'YYYY-MM-DD HH:MI:SS') or endet IS NULL) END");
 
 COMMIT;
