761e848caea35b0b7825c37f80b7e78e7cc758ae
Peter Korduan <peter.korduan@gdi-service.de>
2019-02-14 17:01:19 +0100

    add uuid-ossp extension to ukos schema.

--- plugins/ukos/db/postgresql/schema/2019-02-14_16-30-17_ukos_schema.sql
+++ plugins/ukos/db/postgresql/schema/2019-02-14_16-30-17_ukos_schema.sql
@@ -2,13 +2,7 @@
 -- PostgreSQL database dump
 --
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET client_min_messages = warning;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 --
 -- Name: ukos_base; Type: SCHEMA; Schema: -; Owner: -
