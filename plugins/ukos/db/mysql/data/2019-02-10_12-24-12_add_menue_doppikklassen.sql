1f0ffcc57edd72cb55d06cbe23ea8f6edea693e9
Peter Korduan <peter.korduan@gdi-service.de>
2019-02-14 17:22:39 +0100

    Typo

--- plugins/ukos/db/mysql/data/2019-02-10_12-24-12_add_menue_doppikklassen.sql
+++ plugins/ukos/db/mysql/data/2019-02-10_12-24-12_add_menue_doppikklassen.sql
@@ -10,7 +10,7 @@ BEGIN;
 		SELECT name FROM u_menues WHERE name = 'Datensätze anlegen'
 	);
 
-	INSERT INTO u_menues (name, links, obermenue, menueebene, title, order)
+	INSERT INTO u_menues (name, links, obermenue, menueebene, title, `order`)
 	SELECT
 		*
 	FROM (
@@ -20,7 +20,7 @@ BEGIN;
 			(SELECT id FROM u_menues WHERE name = 'Datensätze anlegen') AS obermenue,
 			2 AS menueebene,
 			'Doppikobjekte anlegen' AS title,
-			100 AS order
+			100 AS `order`
 	) tmp
 	WHERE NOT EXISTS (
 		SELECT name FROM u_menues WHERE name = 'Doppikobjekte'
