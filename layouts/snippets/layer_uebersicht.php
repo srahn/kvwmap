00ceaa12c48849ff580f46a585a8ffffd84313a0
Stefan Rahn <stefan.rahn@gdi-service.de>
2019-02-22 14:38:14 +0100

    Bug Themen-Übersicht

--- layouts/snippets/layer_uebersicht.php
+++ layouts/snippets/layer_uebersicht.php
@@ -6,35 +6,37 @@
 		$group_layer_ids = $GUI->layers['layers_of_group'][$group['id']];
 		$anzahl_layer = count($group_layer_ids);
 		if($anzahl_layer > 0 OR $group['untergruppen'] != ''){
-			echo '
+			$output = '
 						<tr>
 							<td colspan="3" bgcolor="'.BG_GLEATTRIBUTE.'" class="px17 fett" style="height: 30px; border:1px solid #C3C7C3;"><div style="margin-left: '.$indent.'px;">';
-			if($indent > 0)echo '<img src="graphics/pfeil_unten-rechts.gif">';
-			echo $group['Gruppenname'].'</div></td>
+			if($indent > 0)$output.= '<img src="graphics/pfeil_unten-rechts.gif">';
+			$output.= $group['Gruppenname'].'</div></td>
 						</tr>
 					';
 			if($group['untergruppen'] != ''){
 				foreach($group['untergruppen'] as $untergruppe){
-					$GUI->outputGroup($GUI->groups[$untergruppe], $indent + 10);
+					$output_groups.= $GUI->outputGroup($GUI->groups[$untergruppe], $indent + 10);
 				}
 			}
 			for($i = 0; $i < $anzahl_layer; $i++){
-				$GUI->outputLayer($group_layer_ids[$i], $indent + 10);
+				$output_layers.= $GUI->outputLayer($group_layer_ids[$i], $indent + 10);
 			}
 		}
+		if($output_layers != '' OR $output_groups)return $output.$output_groups.$output_layers;
 	};
 	
 	$this->outputLayer = function($i, $indent = 0) use ($GUI){
-		echo '
+		$output = '
       	<tr>
 		        <td style="padding-left: '.$indent.'px;border-right:1px solid #C3C7C3; border-bottom:1px solid #C3C7C3" valign="top">';
-							if($GUI->layers['alias'][$i] != '')echo $GUI->layers['alias'][$i];
-							else echo $GUI->layers['Bezeichnung'][$i];
-		echo '	</td>
+							if($GUI->layers['alias'][$i] != '')$output.= $GUI->layers['alias'][$i];
+							else $output.= $GUI->layers['Bezeichnung'][$i];
+		$output.= '	</td>
 		        <td style="border-right:1px solid #C3C7C3; border-bottom:1px solid #C3C7C3" valign="top"><div style="width: 400px">'.htmlentities($GUI->layers['Kurzbeschreibung'][$i]).'</div></td>
 		        <td style="border-bottom:1px solid #C3C7C3"><div style="width: 200px" valign="top">'.$GUI->layers['Datenherr'][$i].'</div></td>
 		      </tr>
       	';
+		return $output;
 	};
 
  # 2008-01-12 pkvvm
@@ -62,7 +64,7 @@ function Bestaetigung(link,text) {
 	      <?
 				foreach($this->groups as $group){
 					if($group['obergruppe'] == ''){
-						$this->outputGroup($group);
+						echo $this->outputGroup($group);
 					}
 				}
 				?>
