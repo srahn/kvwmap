b360677016afa0fc010cbadcaac169d39fc5c8d6
Peter Korduan <peter.korduan@gdi-service.de>
2019-05-07 12:13:46 +0200

    Add trigger to add mimetype.

---
+++ plugins/xplankonverter/db/postgresql/schema/2019-05-06_13-06-12_trigger_set_mimetype.sql
@@ -0,0 +1,68 @@
+BEGIN;
+
+CREATE OR REPLACE FUNCTION xplankonverter.set_mimetype()
+  RETURNS trigger AS
+$BODY$
+DECLARE
+	mimetype json := '{
+		"xls" : "application/msexcel",
+		"xlsx" : "application/msexcel",
+		"doc" : "application/msword",
+		"docx" : "application/msword",
+		"odt" : "application/odt",
+		"ods" : "application/odt",
+		"pdf" : "application/pdf",
+		"gml" : "application/vnd.ogc.gml",
+		"xml" : "application/xml",
+		"zip" : "application/zip",
+		"ecw" : "image/ecw",
+		"jpg" : "image/jpg",
+		"jpeg" : "image/jpg",
+		"png" : "image/png",
+		"svg" : "image/svg+xml",
+		"tif" : "image/tiff",
+		"tiff" : "image/tiff",
+		"php" : "text/html", 
+		"htm" : "text/html",
+		"html" : "text/html",
+		"txt" : "text/plain"
+		}';
+BEGIN
+	IF ((TG_OP = 'INSERT' AND NEW.externereferenz is NOT NULL) OR (TG_OP = 'UPDATE' AND NEW.externereferenz IS NOT NULL AND NEW.externereferenz != OLD.externereferenz)) THEN
+		SELECT
+			ARRAY_AGG(
+				ROW(
+					(g).georefurl,
+					(select row(codespace, id, value) from xplan_gml.xp_mimetypes WHERE id = mimetype ->> lower(regexp_replace((g).georefurl, '^.*\.', ''))),
+					(g).art,
+					(g).informationssystemurl,
+					(g).referenzname,
+					(g).referenzurl,
+					(select row(codespace, id, value) from xplan_gml.xp_mimetypes WHERE id = mimetype ->> lower(regexp_replace((g).referenzurl, '^.*\.', ''))),
+					(g).beschreibung,
+					(g).datum,
+					(g).typ
+				)
+			)
+		INTO
+			NEW.externereferenz
+		FROM
+			(
+				SELECT unnest(NEW.externereferenz) g
+			) AS ext;
+
+	END IF;
+	RETURN NEW;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+	
+	
+CREATE TRIGGER set_mimetype
+  BEFORE INSERT OR UPDATE
+  ON xplan_gml.bp_plan
+  FOR EACH ROW
+  EXECUTE PROCEDURE xplankonverter.set_mimetype();	
+
+COMMIT;
