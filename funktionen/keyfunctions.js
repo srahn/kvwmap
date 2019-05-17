19181ec0f9dc9623e63f5ce999970d48939d6366
Stefan Rahn <stefan.rahn@gdi-service.de>
2019-05-17 11:19:30 +0200

    Strg s keyfunctions

--- funktionen/keyfunctions.js
+++ funktionen/keyfunctions.js
@@ -1,6 +1,6 @@
 var keyp = function(e) {
 	// ctrl s
-	if (e.ctrlKey &&	e.key && (e.key == 's' || e.key == 'S')) {
+	if (e.ctrlKey && e.key && (e.key == 's' || e.key == 'S')) {
 		if (typeof keypress_bound_ctrl_s_button_id !== 'undefined') {
 			e.preventDefault();
 			// ToDo vorher muss der Change des Feldes in dem man gerade ist durchgeführt werden, weil sonst der alte Wert gespeichert wird.
