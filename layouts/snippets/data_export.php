601b4f9fc22244cd8cd77c74bec9c4cf1fcc34c6
Stefan Rahn <stefan.rahn@gdi-service.de>
2019-07-29 14:05:38 +0200

    Bugfix 2.11.2

--- layouts/snippets/data_export.php
+++ layouts/snippets/data_export.php
@@ -259,26 +259,28 @@ $j=0;
 					for($s = 0; $s < 4; $s++){ ?>
 						<div style="float: left; padding: 4px;"><?
 							for($i = 0; $i < $floor+$r; $i++) {
-								if($this->data_import_export->attributes['group'][$j] != '') $groupnames = true;
-								if($this->data_import_export->attributes['form_element_type'][$j] == 'Dokument'){$document_attributes = true; $document_ids[] = $j;} ?>
-								<div style="padding: 4px;
-							<? 	if($this->data_import_export->attributes['name'][$j] == $this->data_import_export->attributes['the_geom']){
-										if($this->formvars['export_format'] == 'CSV' OR $this->data_import_export->layerdaten['export_privileg'][$selectindex] != 1){echo 'display:none"';} echo '" id="geom_div"';
-									} ?>
-								">
-									<input id="check_attribute_<? echo $j; ?>" type="checkbox" <? if($this->formvars['load'] OR $this->formvars['check_'.$this->data_import_export->attributes['name'][$j]] == 1)echo 'checked'; ?> value="1" name="check_<? echo $this->data_import_export->attributes['name'][$j]; ?>"><?php
-									if($this->data_import_export->attributes['alias'][$j] != ''){
-										echo $this->data_import_export->attributes['alias'][$j];
-									}
-									else {
-										if($this->data_import_export->attributes['name'][$j] == $this->data_import_export->attributes['the_geom']){
-											echo $strNameGeometryField;
+								if($this->data_import_export->attributes['form_element_type'][$j] != 'dynamicLink'){
+									if($this->data_import_export->attributes['group'][$j] != '') $groupnames = true;
+									if($this->data_import_export->attributes['form_element_type'][$j] == 'Dokument'){$document_attributes = true; $document_ids[] = $j;} ?>
+									<div style="padding: 4px;
+								<? 	if($this->data_import_export->attributes['name'][$j] == $this->data_import_export->attributes['the_geom']){
+											if($this->formvars['export_format'] == 'CSV' OR $this->data_import_export->layerdaten['export_privileg'][$selectindex] != 1){echo 'display:none"';} echo '" id="geom_div"';
+										} ?>
+									">
+										<input id="check_attribute_<? echo $j; ?>" type="checkbox" <? if($this->formvars['load'] OR $this->formvars['check_'.$this->data_import_export->attributes['name'][$j]] == 1)echo 'checked'; ?> value="1" name="check_<? echo $this->data_import_export->attributes['name'][$j]; ?>"><?php
+										if($this->data_import_export->attributes['alias'][$j] != ''){
+											echo $this->data_import_export->attributes['alias'][$j];
 										}
 										else {
-											echo $this->data_import_export->attributes['name'][$j];
-										}
-									} ?>
-								</div><?
+											if($this->data_import_export->attributes['name'][$j] == $this->data_import_export->attributes['the_geom']){
+												echo $strNameGeometryField;
+											}
+											else {
+												echo $this->data_import_export->attributes['name'][$j];
+											}
+										} ?>
+									</div><?
+								}
 								$j++;
 							} ?>
 						</div><?
