fcac21ff4461339004417db0335781459cc916d5
Stefan Rahn <stefan.rahn@gdi-service.de>
2019-11-11 10:56:37 +0100

    Bug Radiobutton label

--- layouts/snippets/generic_form_parts.php
+++ layouts/snippets/generic_form_parts.php
@@ -322,7 +322,7 @@
 							$datapart .= 'checked ';
 						}
 						if($attributes['nullable'][$j] != '0')$datapart .= ' onclick="'.($attribute_privileg == '0'? 'return false;' : '').'if(this.checked2 == undefined){this.checked2 = true;}this.checked = this.checked2; if(this.checked===false){var evt = document.createEvent(\'HTMLEvents\');evt.initEvent(\'change\', false, true); this.dispatchEvent(evt);}" onmousedown="this.checked2 = !this.checked;"';							
-						$datapart .= 'value="'.$enum_value[$e].'"><label for="'.$layer_id.'_'.$name.'_'.$k.'_'.$e.'">'.$enum_output[$e].'</label>&nbsp;&nbsp;&nbsp;&nbsp;';
+						$datapart .= 'value="'.$enum_value[$e].'"><label for="'.$layer_id.'_'.$name.'_'.$e.'_'.$k.'">'.$enum_output[$e].'</label>&nbsp;&nbsp;&nbsp;&nbsp;';
 						if(!$attributes['horizontal'][$j])$datapart .= '<br>';
 					}
 				}break;				
