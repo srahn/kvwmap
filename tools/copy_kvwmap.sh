bb6739cf604ef9a653ef4015136c7fcb00d02b62
Peter Korduan <peter.korduan@gdi-service.de>
2019-11-19 12:18:11 +0100

    Fix bugs in copy_kvwmap.

--- tools/copy_kvwmap.sh
+++ tools/copy_kvwmap.sh
@@ -24,7 +24,12 @@ usage() {
 
 exec_sql() {
   echo "Execute: psql -h $PGSQL_SERVER -U $PGSQL_USER -c "${sql}" ${SOURCE_PGSQL_DBNAME}";
-  psql -h $PGSQL_SERVER -U $PGSQL_USER -c "${sql}" ${SOURCE_PGSQL_DBNAME}
+  docker exec pgsql-server psql -U $PGSQL_USER -c "${sql}" ${SOURCE_PGSQL_DBNAME}
+}
+
+exec_mysql() {
+  #echo "Execute: mysql -h ${MYSQL_SERVER} -u kvwmap --password=${MYSQL_PASSWORD} ${MYSQL_DBNAME} -e \"${sql}\""
+  docker exec web mysql -h ${MYSQL_SERVER} -u kvwmap --password=${MYSQL_PASSWORD} $MYSQL_DBNAME -e "${sql}"
 }
 
 # Parameter abfragen
@@ -66,6 +71,7 @@ if [ -z $1 ] ; then
 else
   SOURCE=$1
 fi
+echo "SOURCE: ${SOURCE}"
 
 if [ -z $2 ] ; then
   echo "TARGET fehlt im 2. Parameter!"
@@ -74,53 +80,80 @@ if [ -z $2 ] ; then
 else
   TARGET=$2
 fi
+echo "TARGET ${TARGET}"
 
 # Setze konstante Variablen
-WWW_DIR="/var/www"
+WWW_DIR="/home/gisadmin/www"
+echo "WWW_DIR: ${WWW_DIR}"
 APPS_DIR="${WWW_DIR}/apps"
-DUMP_DIR="${WWW_DIR}/backups"
-CREDENTIALS_FILE="${APPS_DIR}/${SOURCE_APP_NAME}/credentials.php"
-CONFIG_FILE="${APPS_DIR}/${SOURCE_APP_NAME}/config.php"
+echo "APPS_DIR: ${APPS_DIR}"
+DUMP_DIR="www/backups"
+echo "DUMP_DIR: ${DUMP_DIR}"
+SCRIPT_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+echo "SCRIPT_DIR: ${SCRIPT_DIR}"
+COPY_CONFIG_FILE="${SCRIPT_DIR}/copy_kvwmap_config.json"
+if [ ! -f "$COPY_CONFIG_FILE" ] ; then
+  echo "copy_kvwmap.config.json nicht gefunden. Ich nehme copy_kvwmap_config.json.sample statt dessen."
+  COPY_CONFIG_FILE="${COPY_CONFIG_FILE}.sample"
+fi
+echo "COPY_CONFIG_FILE: ${COPY_CONFIG_FILE}"
+SOURCE_APP_NAME="$(jq -r .$SOURCE.app_dir $COPY_CONFIG_FILE)"
+echo "SOURCE_APP_NAME: ${SOURCE_APP_NAME}"
+SOURCE_APP_DIR="${APPS_DIR}/${SOURCE_APP_NAME}"
+echo "SOURCE_APP_DIR: ${SOURCE_APP_DIR}"
+SOURCE_DATA_DIR="${WWW_DIR}/$(jq -r .$SOURCE.data_dir $COPY_CONFIG_FILE)"
+echo "SOURCE_DATA_DIR: ${SOURCE_DATA_DIR}"
+SOURCE_MYSQL_DBNAME="$(jq -r .$SOURCE.mysql_dbname $COPY_CONFIG_FILE)"
+echo "SOURCE_MYSQL_DBNAME: ${SOURCE_MYSQL_DBNAME}"
+SOURCE_PGSQL_DBNAME="$(jq -r .${SOURCE}.pgsql_dbname $COPY_CONFIG_FILE)"
+echo "SOURCE_PGSQL_DBNAME: ${SOURCE_PGSQL_DBNAME}"
+TARGET_APP_NAME="$(jq -r .$TARGET.app_dir $COPY_CONFIG_FILE)"
+echo "TARGET_APP_NAME: ${TARGET_APP_NAME}"
+TARGET_APP_DIR="${APPS_DIR}/${TARGET_APP_NAME}"
+echo "TARGET_APP_DIR: ${TARGET_APP_DIR}"
+TARGET_DATA_NAME="$(jq -r .$TARGET.data_dir $COPY_CONFIG_FILE)"
+echo "TARGET_DATA_NAME: ${TARGET_DATA_NAME}"
+TARGET_DATA_DIR="${WWW_DIR}/${TARGET_DATA_NAME}"
+echo "TARGET_DATA_DIR: ${TARGET_DATA_DIR}"
+TARGET_DATA_HANDLER="$(jq -r .$TARGET.data_handler $COPY_CONFIG_FILE)"
+echo "TARGET_DATA_HANDLER: ${TARGET_DATA_HANDLER}"
+TARGET_MYSQL_DBNAME="$(jq -r .$TARGET.mysql_dbname $COPY_CONFIG_FILE)"
+echo "TARGET_MYSQL_DBNAME: ${TARGET_MYSQL_DBNAME}"
+TARGET_PGSQL_DBNAME="$(jq -r .$TARGET.pgsql_dbname $COPY_CONFIG_FILE)"
+echo "TARGET_PGSQL_DBNAME: ${TARGET_PGSQL_DBNAME}"
+
+CONFIG_FILE="${SOURCE_APP_DIR}/config.php"
+echo "CONFIG_FILE: ${CONFIG_FILE}"
+CREDENTIALS_FILE="${SOURCE_APP_DIR}/credentials.php"
+echo "CREDENTIALS_FILE: ${CREDENTIALS_FILE}"
 MYSQL_SERVER="mysql"
+echo "MYSQL_SERVER: ${MYSQL_SERVER}"
 MYSQL_USER=$(grep MYSQL_USER $CREDENTIALS_FILE | cut -d "'" -f 2)
+echo "MYSQL_USER: ${MYSQL_USER}"
 #echo "MYSQL_USER: ${MYSQL_USER}"
 MYSQL_PASSWORD=$(grep MYSQL_PASSWORD $CREDENTIALS_FILE | cut -d "'" -f 2)
 #echo "MYSQL_PASSWORD: ${MYSQL_PASSWORD}"
+#echo "MYSQL_PASSWORD: ${MYSQL_PASSWORD}"
 PGSQL_SERVER="pgsql"
+echo "PGSQL_SERVER: ${PGSQL_SERVER}"
 PGSQL_USER=$(grep POSTGRES_USER $CONFIG_FILE | cut -d "'" -f 4)
+echo "PGSQL_USER: ${PGSQL_USER}"
 #echo "PGSQL_USER: ${PGSQL_USER}"
 #echo "POSTGRES_PASSWORD wird aus .pgpass entnommen"
 PGSQL_PASSWORD=$(grep POSTGRES_PASSWORD $CONFIG_FILE | cut -d "'" -f 4)
 #echo "PGSQL_PASSWORD: ${PGSQL_PASSWORD}"
+#echo "PGSQL_PASSWORD: ${PGSQL_PASSWORD}"
 PGSQL_BIN="/usr/lib/postgresql/9.6/bin"
-PGPASS_ENTRY="${PGSQL_SERVER}:5432:${TARGET_PGSQL_DBNAME}:${PGSQL_USER}:${PGSQL_PASSWORD}"
-grep -qxF "${PGPASS_ENTRY}" ~/.pgpass || echo "${PGPASS_ENTRY}" >> ~/.pgpass
-
-# Setzte variable Variablen
-if [ -f copy_kvwmap.config.json ] ; then
-  CONFIG_FILE=copy_kvwmap.config.json
-else
-  CONFIG_FILE=copy_kvwmap.config.json.sample
-fi
-SOURCE_APP_DIR="${APPS_DIR}/$(jq -r .$SOURCE.app_dir $CONFIG_FILE)"
-SOURCE_DATA_DIR="${WWW_DIR}/$(jq -r .$SOURCE.data_dir $CONFIG_FILE)"
-SOURCE_MYSQL_DBNAME="$(jq -r .$SOURCE.mysql_dbname $CONFIG_FILE)"
-SOURCE_PGSQL_DBNAME="$(jq -r .$SOURCE.pgsql_dbname $CONFIG_FILE)"
-TARGET_APP_DIR="${APPS_DIR}/$(jq -r .$TARGET.app_dir $CONFIG_FILE)"
-TARGET_DATA_DIR="${WWW_DIR}/$(jq -r .$TARGET.data_dir $CONFIG_FILE)"
-TARGET_DATA_HANDLER="$(jq -r .$TARGET.data_handler $CONFIG_FILE)"
-TARGET_MYSQL_DBNAME="$(jq -r .$TARGET.mysql_dbname $CONFIG_FILE)"
-TARGET_PGSQL_DBNAME="$(jq -r .$TARGET.pgsql_dbname $CONFIG_FILE)"
+echo "PGSQL_BIN: ${PGSQL_BIN}"
+PGPASS_ENTRY="localhost:5432:${TARGET_PGSQL_DBNAME}:${PGSQL_USER}:${PGSQL_PASSWORD}"
+#echo "PGPASS_ENTRY: ${PGPASS_ENTRY}"
+grep -qxF "${PGPASS_ENTRY}" /home/gisadmin/etc/postgresql/.pgpass || echo "${PGPASS_ENTRY}" >> /home/gisadmin/etc/postgresql/.pgpass
+PGPASS_ENTRY="localhost:5432:${SOURCE_PGSQL_DBNAME}:${PGSQL_USER}:${PGSQL_PASSWORD}"
+grep -qxF "${PGPASS_ENTRY}" /home/gisadmin/etc/postgresql/.pgpass || echo "${PGPASS_ENTRY}" >> /home/gisadmin/etc/postgresql/.pgpass
 
 # Prüfe Variablen
-if [ ! -d $SOURCE_APP_DIR ] ; then
+if [ ! -d "$SOURCE_APP_DIR" ] ; then
   echo "SOURCE Apps-Verzeichnis ${SOURCE_APP_DIR} existiert nicht!"
-  usage
-  exit 1
-fi
-
-if [ ! -d $SOURCE_DATA_DIR} ] ; then
-  echo "SOURCE Data-Verzeichnis ${SOURCE_DATA_DIR} existiert nicht!"
   usage
   exit 1
 fi
@@ -154,7 +187,7 @@ echo "Ersetze Web-Alias in config.php"
 sed -i -e "s|${SOURCE_APP_NAME}/|${TARGET_APP_NAME}/|g" ${TARGET_APP_DIR}/config.php
 
 echo "Sicherung der Datenbank ${SOURCE_PGSQL_DBNAME} in ${TARGET_PGSQL_DBNAME}.dump"
-${PGSQL_BIN}/pg_dump -h $PGSQL_SERVER -U $PGSQL_USER -Fc -f ${DUMP_DIR}/${TARGET_PGSQL_DBNAME}.dump ${SOURCE_PGSQL_DBNAME}
+docker exec pgsql-server ${PGSQL_BIN}/pg_dump -U $PGSQL_USER -Fc -f /var/${DUMP_DIR}/${TARGET_PGSQL_DBNAME}.dump ${SOURCE_PGSQL_DBNAME}
 
 echo "Schliesse idle Prozesse in ${TARGET_PGSQL_DBNAME}"
 sql="
@@ -182,19 +215,31 @@ sql="
 exec_sql
 
 echo "Stelle Produktionsdatenbank ${SOURCE_PGSQL_DBNAME} in Datenbank ${TARGET_PGSQL_DBNAME} wieder her"
-${PGSQL_BIN}/pg_restore -h $PGSQL_SERVER -U $PGSQL_USER -Fc -d ${TARGET_PGSQL_DBNAME} ${DUMP_DIR}/${TARGET_PGSQL_DBNAME}.dump
+docker exec pgsql-server ${PGSQL_BIN}/pg_restore -U $PGSQL_USER -Fc -d ${TARGET_PGSQL_DBNAME} /var/${DUMP_DIR}/${TARGET_PGSQL_DBNAME}.dump
 
+MYSQL_DBNAME=$SOURCE_MYSQ_DBNAME
 echo "Lösche Testdatenbank ${TARGET_MYSQL_DBNAME}, erzeuge eine neue leere und spiele die Sicherung von ${SOURCE_MYSQL_DBNAME} ein"
-echo "DROP DATABASE IF EXISTS ${TARGET_MYSQL_DBNAME}" | mysql -h ${MYSQL_SERVER} -u kvwmap --password=${MYSQL_PASSWORD}
-echo "CREATE DATABASE ${TARGET_MYSQL_DBNAME}" | mysql -h ${MYSQL_SERVER} -u kvwmap --password=${MYSQL_PASSWORD}
-mysqldump -h ${MYSQL_SERVER} -u $MYSQL_USER --password=${MYSQL_PASSWORD} ${SOURCE_MYSQL_DBNAME} > ${DUMP_DIR}/${TARGET_MYSQL_DBNAME}.dump
-mysql -h ${MYSQL_SERVER} -u $MYSQL_USER --password=${MYSQL_PASSWORD} ${TARGET_MYSQL_DBNAME} < ${DUMP_DIR}/${TARGET_MYSQL_DBNAME}.dump
+sql="DROP DATABASE IF EXISTS ${TARGET_MYSQL_DBNAME}"
+exec_mysql
 
+sql="CREATE DATABASE ${TARGET_MYSQL_DBNAME}"
+exec_mysql
+
+echo "exec: mysqldump -h ${MYSQL_SERVER} -u $MYSQL_USER --password=${MYSQL_PASSWORD} ${SOURCE_MYSQL_DBNAME} > /home/gisadmin/${DUMP_DIR}/${TARGET_MYSQL_DBNAME}.dump"
+docker exec web mysqldump -h ${MYSQL_SERVER} -u $MYSQL_USER --password=${MYSQL_PASSWORD} ${SOURCE_MYSQL_DBNAME} > /home/gisadmin/${DUMP_DIR}/${TARGET_MYSQL_DBNAME}.dump
+echo "exec: mysql -h ${MYSQL_SERVER} -u $MYSQL_USER --password=${MYSQL_PASSWORD} ${TARGET_MYSQL_DBNAME} < /home/gisadmin/${DUMP_DIR}/${TARGET_MYSQL_DBNAME}.dump"
+docker exec -i web mysql -h ${MYSQL_SERVER} -u $MYSQL_USER --password=${MYSQL_PASSWORD} ${TARGET_MYSQL_DBNAME} < /home/gisadmin/${DUMP_DIR}/${TARGET_MYSQL_DBNAME}.dump
+
+MYSQL_DBNAME=$TARGET_MYSQL_DBNAME
 echo "Nehme Ersetzungen in MySQL-Datenbank ${TARGET_MYSQL_DBNAME} vor"
-echo "UPDATE layer SET \`connection\` = replace(connection, 'dbname=${SOURCE_PGSQL_DBNAME}', 'dbname=${TARGET_PGSQL_DBNAME}') WHERE \`connection\` LIKE '%dbname=${SOURCE_PGSQL_DBNAME}%'" | mysql -h ${MYSQL_SERVER} -u $MYSQL_USER --password=${MYSQL_PASSWORD} ${TARGET_MYSQL_DBNAME}
-echo "UPDATE config SET \`value\` = '${TARGET_APP_NAME}/' WHERE \`name\` = 'APPLVERSION'" | mysql -h ${MYSQL_SERVER} -u $MYSQL_USER --password=${MYSQL_PASSWORD} ${TARGET_MYSQL_DBNAME}
-echo "UPDATE config SET \`value\` = '${TARGET_DATA_DIR}/' WHERE \`name\` = 'SHAPEPATH'" | mysql -h ${MYSQL_SERVER} -u $MYSQL_USER --password=${MYSQL_PASSWORD} ${TARGET_MYSQL_DBNAME}
-echo "UPDATE config SET \`value\` = '${TARGET_PGSQL_DBNAME}' WHERE \`name\` = 'POSTGRES_DBNAME'" | mysql -h ${MYSQL_SERVER} -u $MYSQL_USER --password=${MYSQL_PASSWORD} ${TARGET_MYSQL_DBNAME}
+sql="UPDATE layer SET \`connection\` = replace(connection, 'dbname=${SOURCE_PGSQL_DBNAME}', 'dbname=${TARGET_PGSQL_DBNAME}') WHERE \`connection\` LIKE '%dbname=${SOURCE_PGSQL_DBNAME}%'"
+exec_mysql
+sql="UPDATE config SET \`value\` = '${TARGET_APP_NAME}/' WHERE \`name\` = 'APPLVERSION'"
+exec_mysql
+sql="UPDATE config SET \`value\` = '${TARGET_DATA_NAME}/' WHERE \`name\` = 'SHAPEPATH'"
+exec_mysql
+sql="UPDATE config SET \`value\` = '${TARGET_PGSQL_DBNAME}' WHERE \`name\` = 'POSTGRES_DBNAME'"
+exec_mysql
 
 # Behandle Datenverzeichnis
 # Fälle von TARGET_DATA_HANDLER
@@ -203,27 +248,31 @@ echo "UPDATE config SET \`value\` = '${TARGET_PGSQL_DBNAME}' WHERE \`name\` = 'P
 # link Lösche Verzeichnis SOURCE_DATA_DIR wenn es ein Verzeichnis ist.
 #      Lösche Link SOURCE_DATA_DIR wenn es ein Link ist
 #      Lege Link SOURCE_DATA_DIR an der auf TARGET_DATA_DIR zeigt
-if [ "${TARGET_DATA_HANDLER}" == "copy" ]; then
-  if [ -d "${TARGET_DATA_DIR}" ]; then 
-    echo "Lösche TARGET Datenverzeichnis ${TARGET_DATA_DIR}"
-    rm -Rf ${TARGET_DATA_DIR}; 
-  fi
-  echo "Kopiere SOURCE Datenverzeichnis ${SOURCE_DATA_DIR} nach TARGET Datenverzeichnis ${TARGET_DATA_DIR}"
-  cp -R ${SOURCE_DATA_DIR} ${TARGET_DATA_DIR}
-  chown -R gisadmin.gisadmin ${TARGET_DATA_DIR}
-  chmod -R g+w ${TARGET_DATA_DIR}
-else
-  if [[ -L "${TARGET_DATA_DIR}" ]]; then
-    # Target Dir ist ein LINK
-    echo "Lösche Link auf Daten Verzeichnis ${TARGET_DATA_DIR}"
-    rm ${TARGET_DATA_DIR}; 
-  else
+if [ "${TARGET_DATA_HANDLER}" = "copy" ] ; then
+  if [ ! "${TARGET_DATA_DIR}" = "${SOURCE_DATA_DIR}" ] ; then
     if [ -d "${TARGET_DATA_DIR}" ]; then
-      echo "Lösche Test Daten Verzeichnis ${TARGET_DATA_DIR}"
-      rm -Rf ${TARGET_DATA_DIR};
+      echo "Lösche TARGET Datenverzeichnis ${TARGET_DATA_DIR}"
+      rm -Rf ${TARGET_DATA_DIR}; 
     fi
+    echo "Kopiere SOURCE Datenverzeichnis ${SOURCE_DATA_DIR} nach TARGET Datenverzeichnis ${TARGET_DATA_DIR}"
+    cp -R ${SOURCE_DATA_DIR} ${TARGET_DATA_DIR}
+    chown -R gisadmin.gisadmin ${TARGET_DATA_DIR}
+    chmod -R g+w ${TARGET_DATA_DIR}
   fi
-  echo "Erzeuge Link von Testverzeichnis ${TARGET_DATA_DIR} auf Productionsverzeichnis ${SOURCE_DATA_DIR}"
-  ln -s ${SOURCE_DATA_DIR} ${TARGET_DATA_DIR}
-  chown -h gisadmin.gisadmin ${TARGET_DATA_DIR}
+else
+  if [ "${TARGET_DATA_DIR}" = "link" ] ; then
+    if [[ -L "${TARGET_DATA_DIR}" ]]; then
+      # Target Dir ist ein LINK
+      echo "Lösche Link auf Daten Verzeichnis ${TARGET_DATA_DIR}"
+      rm ${TARGET_DATA_DIR}; 
+    else
+      if [ -d "${TARGET_DATA_DIR}" ]; then
+        echo "Lösche Target Daten Verzeichnis ${TARGET_DATA_DIR}"
+        rm -Rf ${TARGET_DATA_DIR};
+      fi
+    fi
+    echo "Erzeuge Link von Testverzeichnis ${TARGET_DATA_DIR} auf Productionsverzeichnis ${SOURCE_DATA_DIR}"
+    ln -s ${SOURCE_DATA_DIR} ${TARGET_DATA_DIR}
+    chown -h gisadmin.gisadmin ${TARGET_DATA_DIR}
+  fi
 fi
