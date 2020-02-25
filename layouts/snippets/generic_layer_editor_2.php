880b0a6d8045f7d5e85a3790389824611efb0052
Stefan Rahn <stefan.rahn@gdi-service.de>
2020-02-25 11:24:07 +0100

    GLE class für die Attributvalues hinzugefügt

--- layouts/snippets/generic_layer_editor_2.php
+++ layouts/snippets/generic_layer_editor_2.php
@@ -165,7 +165,7 @@
 							######### Attributwert #########
 							$cell['content'] = attribute_value($this, $layer, NULL, $j, $k, NULL, $size2, $select_width2, $this->user->rolle->fontsize_gle);
 							$cell['id'] = 'value_'.$layer['Layer_ID'].'_'.$layer['attributes']['name'][$j].'_'.$k;
-							$cell['properties'] = get_td_class_or_style(array($layer['shape'][$k][$layer['attributes']['style']], 'gle_attribute_value'));
+							$cell['properties'] = get_td_class_or_style(array($layer['shape'][$k][$layer['attributes']['style']], 'gle_attribute_value value_'.$layer['Layer_ID'].'_'.$layer['attributes']['name'][$j]));
 							if($nl){
 								$next_row['cells'][] = $cell;
 							}
