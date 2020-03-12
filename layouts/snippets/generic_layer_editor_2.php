24ea651a3681ab7e1e4dbc149687b98b8aeea14e
Stefan Rahn <stefan.rahn@gdi-service.de>
2020-03-12 11:37:35 +0100

    Layername bei requires-Layern anzeigen im GLE

--- layouts/snippets/generic_layer_editor_2.php
+++ layouts/snippets/generic_layer_editor_2.php
@@ -32,7 +32,7 @@
 ?>
 <div id="layer" onclick="remove_calendar();">
 <input type="hidden" value="" id="changed_<? echo $layer['Layer_ID']; ?>" name="changed_<? echo $layer['Layer_ID']; ?>">
-<? if($this->new_entry != true AND $layer['requires'] == ''){ ?>
+<? if($this->new_entry != true){ ?>
 <table border="0" cellpadding="0" cellspacing="0" width="100%">
 	<tr>
 		<td>
