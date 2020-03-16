b4a6cd0e80e62f0a1392cf0e358beb7b4c30584f
Peter Korduan <peter.korduan@gdi-service.de>
2020-03-16 19:25:16 +0100

    Datatype for planaufstellende Gemeinde.

---
+++ plugins/xplankonverter/db/postgresql/schema/2020-03-16_10-45-23_add_datatyp_for_planaufstellende_gemeinde.sql
@@ -0,0 +1,31 @@
+BEGIN;
+  -- Tabelle für planaufstellende Gemeinden
+  CREATE TABLE xplankonverter.planaufstellende_gebietseinheiten WITH OIDS AS
+  SELECT * FROM xplankonverter.gebietseinheiten;
+  ALTER TABLE xplankonverter.planaufstellende_gebietseinheiten
+  ADD CONSTRAINT planaufstellende_gebietseinheiten_pkey PRIMARY KEY (id_ot);
+  CREATE INDEX stelle_id_idx ON xplankonverter.planaufstellende_gebietseinheiten
+  USING btree (stelle_id ASC NULLS LAST);
+
+  -- Umstellung auf extra Datentyp für planaufstellende Gemeinden
+  CREATE TYPE xplan_gml.xp_planaufstellendegemeinde AS
+  (
+  	ags character varying,
+  	rs character varying,
+  	gemeindename character varying,
+  	ortsteilname character varying
+  );
+
+  ALTER TABLE xplan_gml.bp_plan
+  ALTER COLUMN planaufstellendegemeinde TYPE xplan_gml.xp_planaufstellendegemeinde[]
+  USING planaufstellendegemeinde::text::xplan_gml.xp_planaufstellendegemeinde[];
+
+  ALTER TABLE xplan_gml.fp_plan
+  ALTER COLUMN planaufstellendegemeinde TYPE xplan_gml.xp_planaufstellendegemeinde[]
+  USING planaufstellendegemeinde::text::xplan_gml.xp_planaufstellendegemeinde[];
+
+  ALTER TABLE xplan_gml.so_plan
+  ALTER COLUMN planaufstellendegemeinde TYPE xplan_gml.xp_planaufstellendegemeinde[]
+  USING planaufstellendegemeinde::text::xplan_gml.xp_planaufstellendegemeinde[];
+
+COMMIT;
