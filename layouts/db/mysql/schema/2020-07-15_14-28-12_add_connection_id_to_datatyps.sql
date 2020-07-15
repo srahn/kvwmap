e802bf2fb621a3ec85ef9269aac46dcae8aac41c
Peter Korduan <peter.korduan@gdi-service.de>
2020-07-15 15:47:30 +0200

    Add connection_id to datatypes table.

---
+++ layouts/db/mysql/schema/2020-07-15_14-28-12_add_connection_id_to_datatyps.sql
@@ -0,0 +1,28 @@
+BEGIN;
+
+#  ALTER TABLE `datatypes` DROP FOREIGN KEY fk_datatypes_connection_id;
+#  ALTER TABLE `datatypes` DROP COLUMN `connection_id`;
+  ALTER TABLE `datatypes` ADD COLUMN `connection_id` bigint(20) UNSIGNED;
+  ALTER TABLE `datatypes` ADD CONSTRAINT fk_datatypes_connection_id FOREIGN KEY (connection_id) REFERENCES connections(id);
+  
+  UPDATE
+    `datatypes` dt,
+    (
+      SELECT
+        min(id) AS id,
+        host,
+        port,
+        dbname
+      FROM
+        `connections`
+      GROUP BY
+        host, port, dbname
+    ) AS cn
+  SET
+    dt.connection_id = cn.id
+  WHERE
+    dt.host = cn.host AND
+    dt.port = cn.port AND
+    dt.dbname = cn.dbname;
+
+COMMIT;
