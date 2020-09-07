1ce922db04e1de64efc41a81bc97e31d4c562353
Peter Korduan <peter.korduan@gdi-service.de>
2020-09-07 15:44:28 +0200

    Fix part 2 validation issues attribute_is_not_null_if_other_attribute_exists

--- plugins/xplankonverter/db/postgresql/schema/2020-09-07_15-32-50_fix_konformitaetbedingungen_attribute_is_not_null_if_other_attribute_exists_part_2.sql
+++ plugins/xplankonverter/db/postgresql/schema/2020-09-07_15-32-50_fix_konformitaetbedingungen_attribute_is_not_null_if_other_attribute_exists_part_2.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 UPDATE xplankonverter.validierungen
-SET functionsargumente = '{("detaillierteDachform IS NULL ) OR (dachform IS NOT NULL AND detaillierteDachform IS NOT NULL)"}'
+SET functionsargumente = '{"(detaillierteDachform IS NULL ) OR (dachform IS NOT NULL AND detaillierteDachform IS NOT NULL)"}'
 WHERE
 konformitaet_nummer = '4.3.1.2' AND konformitaet_version_von = '4.1';
 
