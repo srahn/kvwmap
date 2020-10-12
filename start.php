678c00eb88f57126192f806bede9aa95bdf70bfa
Stefan Rahn <stefan.rahn@gdi-service.de>
2020-10-12 12:51:19 +0200

    Bug Logout

--- start.php
+++ start.php
@@ -729,6 +729,7 @@ function logout() {
 	$_SESSION = array();
 	if (ini_get("session.use_cookies")){
 		$params = session_get_cookie_params();
+		$params['path'] = explode(';', $params['path'])[0];
 		setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
 	}
 	session_destroy();
