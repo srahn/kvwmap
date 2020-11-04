ff1b21f142269595a9b64cae721970f137204d4d
Stefan Rahn <stefan.rahn@gdi-service.de>
2020-11-04 11:00:31 +0100

    Bug menues Layer-Suche-Suchen

--- layouts/db/mysql/schema/2020-11-04_10-54-07_menues_Layer-Suche-Suchen.sql
+++ layouts/db/mysql/schema/2020-11-04_10-54-07_menues_Layer-Suche-Suchen.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 UPDATE `u_menues` 
-SET links = "javascript:void(0)", onclick = CONCAT("overlay_link('", replace(`links`, 'index.php?', ''), "', true)") 
+SET onclick = CONCAT("overlay_link('", replace(`links`, 'index.php?', ''), "', true)") , links = "javascript:void(0)"
 WHERE `links` like 'index.php?go=Layer-Suche_Suchen%';
 
 COMMIT;
