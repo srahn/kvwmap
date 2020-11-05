f6698a7b01c0bafa9208b29b55959c3fb9c5e0ab
Stefan Rahn <stefan.rahn@gdi-service.de>
2020-11-05 11:46:55 +0100

    Bugs extra Fenster Flurstücke

--- layouts/snippets/overlay.php
+++ layouts/snippets/overlay.php
@@ -22,7 +22,12 @@
 							<img src="<? echo GRAPHICSPATH.'leer.gif'; ?>" onload="currentform = document.GUI2;">
 				<?
 				$this->currentform = 'document.GUI2';
-				include (LAYOUTPATH.'snippets/'.$this->main);
+				if (file_exists($this->main)) {
+					include($this->main); # Pluginviews
+				}
+				else {
+					include(LAYOUTPATH . "snippets/" . $this->main);		# normale snippets
+				}
 				?>
 						</td>
 					</tr>
