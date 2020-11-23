315a93d3416465df73fd9460ba50ed193a97e104
Stefan Rahn <stefan.rahn@gdi-service.de>
2020-11-23 10:05:54 +0100

    Stoppe Migrationen bei Fehler

--- class/administration.php
+++ class/administration.php
@@ -246,6 +246,7 @@ class administration{
 				if ($result[0]) {
 					$err_msgs[] = getTimestamp('H:i:s', 4) . ': Fehler beim Ausführen von migration-Datei:<br>' . $file . '<br>in Pfad: ' . str_replace(WWWROOT.APPLVERSION, '../', $filepath) . '<br>' . $result[1];
 					$result = $this->pgdatabase->execSQL('ROLLBACK;', 0, 0, false);
+					break;
 				}
 				else{
 					$sql = "
