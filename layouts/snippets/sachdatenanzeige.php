89ec9eb6696082e679d211ab796b352474c20b6f
Stefan Rahn <stefan.rahn@gdi-service.de>
2021-06-17 09:32:32 +0200

    3.0.228

--- layouts/snippets/sachdatenanzeige.php
+++ layouts/snippets/sachdatenanzeige.php
@@ -266,7 +266,7 @@ if($this->formvars['window_type'] == 'overlay'){ ?>
 			}
 */
 	  	if($this->formvars['printversion'] == '' AND $this->formvars['keinzurueck'] == '' AND $this->formvars['subform_link'] == ''){
-				echo '<a href="javascript:currentform.go.value=\'get_last_search\';currentform.submit();" title="'.$strbackToSearch.'"><i class="fa fa-arrow-left hover-border" style="margin: 5px" aria-hidden="true"></i></a>';
+				echo '<a href="javascript:currentform.go.value=\'get_last_search\';currentform.submit();" target="root" title="'.$strbackToSearch.'"><i class="fa fa-arrow-left hover-border" style="margin: 5px" aria-hidden="true"></i></a>';
 	  	}
   	}
   	else{
