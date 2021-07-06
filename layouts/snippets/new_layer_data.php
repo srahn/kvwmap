5e9516f0500bdf45e1d0bb52d01c19dac388d236
Peter Korduan <peter.korduan@gdi-service.de>
2021-07-06 12:47:56 +0200

    Bugfix 3.0.232

--- layouts/snippets/new_layer_data.php
+++ layouts/snippets/new_layer_data.php
@@ -114,7 +114,7 @@ if($this->formvars['selected_layer_id'] AND $this->Fehler == ''){
 <div id="nds_submit">
 	<div>
 		<? if($this->formvars['subform'] == 'true'){ ?>
-		<input type="button" name="abort" value="<? echo $this->strCancel; ?>" onclick="currentform.go.value='get_last_query';currentform.submit();">
+		<input type="button" name="abort" value="<? echo $this->strCancel; ?>" onclick="currentform.go.value='get_last_query';overlay_submit(currentform);">
 		<? } ?>
 		<input type="button" name="go_plus" id="sachdatenanzeige_save_button" value="<? echo $strSave; ?>" onclick="save_new_dataset();">
 		<input type="checkbox" name="weiter_erfassen" value="1" <? if($this->formvars['weiter_erfassen'] == 1)echo 'checked="true"'; ?>><span><? echo $strCreateAnotherOne; ?></span>
