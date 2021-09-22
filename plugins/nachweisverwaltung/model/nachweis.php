90e16cec8bea2d922217b2db73f63516aa9dba4a
Stefan Rahn <stefan.rahn@gdi-service.de>
2021-09-22 12:06:42 +0000

    Bugfix 3.0.260

--- plugins/nachweisverwaltung/model/nachweis.php
+++ plugins/nachweisverwaltung/model/nachweis.php
@@ -1306,14 +1306,18 @@ class Nachweis {
     return $result;
   }
 	
-	function Geometrieuebernahme($ref_geom, $id){
+	function Geometrieuebernahme($ref_geom, $id, $gepruefte_Nachweise_bearbeiten){
+		if (!$gepruefte_Nachweise_bearbeiten) {
+			$condition = ' AND geprueft = 0';
+		}
 		$sql = "UPDATE nachweisverwaltung.n_nachweise n 
 						SET the_geom = (
 							SELECT st_union(n2.the_geom) 
 							FROM nachweisverwaltung.n_nachweise n2
 							WHERE n2.id IN (".implode(',', $ref_geom).")
 						)
-						WHERE n.id IN (".implode(',', $id).")";
+						WHERE n.id IN (".implode(',', $id).") " .
+						$condition;
 		$ret=$this->database->execSQL($sql,4, 1);
     if ($ret[0])$result[0]='Fehler bei der Geometrieübernahme!';
     else $result[1]='Geometrien erfolgreich übernommen!';
