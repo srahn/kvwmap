998305e67a24f8173e35f2b665fdbd43429eab81
rkraetschmer <robert.kraetschmer@gdi-service.de>
2021-12-23 09:56:35 +0000

    xplan bugfix add_gmlas_values_aufschuettung_abgrabung

--- plugins/xplankonverter/db/postgresql/schema/2021-12-23_09-52-08_add_gmlas_values_aufschuettung_abgrabung.sql
+++ plugins/xplankonverter/db/postgresql/schema/2021-12-23_09-52-08_add_gmlas_values_aufschuettung_abgrabung.sql
@@ -1,10 +1,10 @@
 BEGIN;
 INSERT INTO
-	xplankonverter.mappingtable_gmlas_to_gml (id,feature_class,o_table,o_column,o_data_type,t_table,t_column,t_datatype,codespace,complex_type,regel)
+	xplankonverter.mappingtable_gmlas_to_gml (id,feature_class,o_table,o_column,o_data_type,t_table,t_column,t_data_type,codespace,complex_type,regel)
 VALUES
-	(4430,'fp_aufschuettung','aufschuettungsmaterial','character varying','fp_aufschuettung','aufschuettungsmaterial','character varying',NULL,NULL,'gml.aufschuettungsmaterial AS aufschuettungsmaterial') ON CONFLICT DO NOTHING;
+	(4430,TRUE,'fp_aufschuettung','aufschuettungsmaterial','character varying','fp_aufschuettung','aufschuettungsmaterial','character varying',NULL,NULL,'gml.aufschuettungsmaterial AS aufschuettungsmaterial') ON CONFLICT DO NOTHING;
 INSERT INTO
-	xplankonverter.mappingtable_gmlas_to_gml (id,feature_class,o_table,o_column,o_data_type,t_table,t_column,t_datatype,codespace,complex_type,regel)
+	xplankonverter.mappingtable_gmlas_to_gml (id,feature_class,o_table,o_column,o_data_type,t_table,t_column,t_data_type,codespace,complex_type,regel)
 VALUES
-(4431,'fp_abgrabung','abbaugut','character varying','fp_abgrabung','abbaugut','character varying',NULL,NULL,'gml.abbaugut AS abbaugut') ON CONFLICT DO NOTHING;
+(4431,TRUE,'fp_abgrabung','abbaugut','character varying','fp_abgrabung','abbaugut','character varying',NULL,NULL,'gml.abbaugut AS abbaugut') ON CONFLICT DO NOTHING;
 COMMIT;
