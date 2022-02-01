fc3332734404d4eedd03f6c3e0808931b5a0187f
Stefan Rahn <stefan.rahn@gdi-service.de>
2022-02-01 10:51:01 +0000

    LENRIS Worker HRO

--- plugins/lenris/tools/Worker HRO/index.php
+++ plugins/lenris/tools/Worker HRO/index.php
@@ -2,7 +2,7 @@
 
 	include("settings.php");
 
-	$dbconn = pg_connect('host=' . $host . 'dbname=' . $dbname . 'user=' . $user . 'password=' . $password)
+	$dbconn = pg_connect('host=' . $host . ' dbname=' . $dbname . ' user=' . $user . ' password=' . $password)
 						or die('Could not connect: ' . pg_last_error());
 
 	pg_set_client_encoding($dbconn, UNICODE);
