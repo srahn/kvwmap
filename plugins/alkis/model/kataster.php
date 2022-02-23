6b23a0ae796eacd343a975f72e01305852775291
Stefan Rahn <stefan.rahn@gdi-service.de>
2022-02-23 12:19:45 +0000

    Bugfix 3.1.78

--- plugins/alkis/model/kataster.php
+++ plugins/alkis/model/kataster.php
@@ -637,7 +637,12 @@ class flurstueck {
   function getKlassifizierung() {
     if ($this->FlurstKennz=="") { return 0; }
     $this->debug->write("<br>kataster.php->flurstueck->getKlassifizierung Abfrage der Klassifizierungen zum Flurstück<br>".$sql,4);
-    $ret=$this->database->getKlassifizierung($this->FlurstKennz);
+		if (AEQUIVALENZ_BEWERTUNG) {
+			$ret=$this->database->getKlassifizierungAequivalenz($this->FlurstKennz);
+		}
+		else {
+			$ret=$this->database->getKlassifizierung($this->FlurstKennz);
+		}
     if ($ret[0] AND DBWRITE) { $this->debug->write("<br>Abbruch Zeile: ".__LINE__,4); return 0; }
     $Klassifizierung=$ret[1];
     return $Klassifizierung;
