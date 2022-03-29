0b41c616665802e0fe464d337e4ae5835710618a
Stefan Rahn <stefan.rahn@gdi-service.de>
2022-03-29 14:54:50 +0000

    Bugfix 3.2.20 Nachtrag

--- funktionen/allg_funktionen.php
+++ funktionen/allg_funktionen.php
@@ -8,6 +8,7 @@ function urlencode2($str){
 	$str = urlencode($str);
 	$str = str_replace('%3F', '?', $str);
 	$str = str_replace('%26', '&', $str);
+	$str = str_replace('%3D', '=', $str);
 	return $str;
 }
 
