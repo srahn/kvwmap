45e157d4645f1eaf357387d81fb8fb9cd77caf59
Stefan Rahn <stefan.rahn@gdi-service.de>
2022-05-23 07:02:10 +0000

    Bugfix 3.2.51 Nachtrag

--- class/kvwmap.php
+++ class/kvwmap.php
@@ -8444,6 +8444,7 @@ SET @connection_id = {$this->pgdatabase->connection_id};
 
 	function GenerischeSuche_Suchen() {
 		$this->formvars['search'] = true;
+		$this->formvars['selected_layer_id'] = intval($this->formvars['selected_layer_id']);
 		if($this->last_query != '') {
 			$this->formvars['selected_layer_id'] = $this->last_query['layer_ids'][0];
 		}
@@ -9069,6 +9070,7 @@ SET @connection_id = {$this->pgdatabase->connection_id};
 			$this->layerdaten = $this->Stelle->getqueryableVectorLayers(NULL, $this->user->id, $this->formvars['selected_group_id']);
 		}
 		if (value_of($this->formvars, 'selected_layer_id')) {
+			$this->formvars['selected_layer_id'] = intval($this->formvars['selected_layer_id']);
 			$data = $mapdb->getData($this->formvars['selected_layer_id']);
 			$data_explosion = explode(' ', $data);
 			$this->formvars['columnname'] = $data_explosion[0];
