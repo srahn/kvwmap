db8ed78b04c87935265519f8bf484aabd1ce1328
Peter Korduan <peter.korduan@gdi-service.de>
2022-06-13 08:36:14 +0000

    Bugfix 3.2.53

--- plugins/xplankonverter/view/header.php
+++ plugins/xplankonverter/view/header.php
@@ -5,19 +5,19 @@
 <link rel="stylesheet" href="<?php echo BOOTSTRAP_PATH; ?>css/bootstrap.min.css" type="text/css">
 <link rel="stylesheet" href="<?php echo BOOTSTRAPTABLE_PATH; ?>bootstrap-table.min.css" type="text/css">
 
-<script src="<?php echo JQUERY_PATH; ?>jquery-<?php echo $dir_parts[1]; ?>.min.js"></script>
-<script src="<?php echo JQUERY_PATH; ?>jquery.base64.js"></script>
+<script type="text/javascript" src="<?php echo JQUERY_PATH; ?>jquery<?php echo ($dir_parts[1] != '3.6.0' ? '-' . $dir_parts[1] : '') ; ?>.min.js"></script>
+<script type="text/javascript" src="<?php echo JQUERY_PATH; ?>jquery.base64.js"></script>
 
-<script src="<?php echo BOOTSTRAP_PATH; ?>js/bootstrap.min.js"></script>
-<script src="<?php echo BOOTSTRAP_PATH; ?>js/bootstrap-table-flatJSON.js"></script>
-<script src="<?php echo BOOTSTRAPTABLE_PATH; ?>bootstrap-table.min.js"></script>
-<script src="<?php echo BOOTSTRAPTABLE_PATH; ?>extensions/export/bootstrap-table-export.min.js"></script><?
+<script type="text/javascript" src="<?php echo BOOTSTRAP_PATH; ?>js/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?php echo BOOTSTRAP_PATH; ?>js/bootstrap-table-flatJSON.js"></script>
+<script type="text/javascript" src="<?php echo BOOTSTRAPTABLE_PATH; ?>bootstrap-table.min.js"></script>
+<script type="text/javascript" src="<?php echo BOOTSTRAPTABLE_PATH; ?>extensions/export/bootstrap-table-export.min.js"></script><?
 if (BOOTSTRAPTABLE_PATH == '../3rdparty/bootstrap-table-1.18.2/') { ?>
-  <script src="<?php echo BOOTSTRAPTABLE_PATH; ?>extensions/filter-control/bootstrap-table-filter-control.min.js"></script><?
+  <script type="text/javascript" src="<?php echo BOOTSTRAPTABLE_PATH; ?>extensions/filter-control/bootstrap-table-filter-control.min.js"></script><?
 } ?>
-<script src="<?php echo BOOTSTRAPTABLE_PATH; ?>libs/FileSaver/FileSaver.min.js"></script>
-<script src="<?php echo BOOTSTRAPTABLE_PATH; ?>libs/js-xlsx/xlsx.core.min.js"></script>
-<script src="<?php echo BOOTSTRAPTABLE_PATH; ?>locale/bootstrap-table-de-DE.min.js"></script>
+<script type="text/javascript" src="<?php echo BOOTSTRAPTABLE_PATH; ?>libs/FileSaver/FileSaver.min.js"></script>
+<script type="text/javascript" src="<?php echo BOOTSTRAPTABLE_PATH; ?>libs/js-xlsx/xlsx.core.min.js"></script>
+<script type="text/javascript" src="<?php echo BOOTSTRAPTABLE_PATH; ?>locale/bootstrap-table-de-DE.min.js"></script>
 
 <style>
 	body {
