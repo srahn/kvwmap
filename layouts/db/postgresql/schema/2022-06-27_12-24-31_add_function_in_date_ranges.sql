d279c382571ae8d864dfdc990afe0aa61de51919
Peter Korduan <peter.korduan@gdi-service.de>
2022-06-27 14:01:41 +0000

    Extent function to check date in date range arrays.

--- layouts/db/postgresql/schema/2022-06-27_12-24-31_add_function_in_date_ranges.sql
+++ layouts/db/postgresql/schema/2022-06-27_12-24-31_add_function_in_date_ranges.sql
@@ -2,22 +2,23 @@ BEGIN;
 	CREATE OR REPLACE FUNCTION public.gdi_in_date_ranges(
 		day date,
 		startdates date[],
-		enddates date[]
-	)
-	RETURNS boolean
-	LANGUAGE 'sql'
-	COST 100
-	STABLE
+		enddates date[])
+	    RETURNS boolean
+	    LANGUAGE 'sql'
+	    COST 100
+	    STABLE
 	AS $BODY$
 		SELECT
-			COALESCE(true = ANY(array_agg(results)), true)
+	  		COALESCE(true = ANY(array_agg(daterange(startdate, CASE WHEN startdate > enddate THEN NULL ELSE enddate END) @> now()::date)), true)
 		FROM
 			(
 				SELECT
-					daterange(unnest(startdates) , (unnest(enddates) + interval '1' day)::date) @> day AS results
-			) AS foo
+	  				unnest(startdates) AS startdate,
+	  				(unnest(enddates) + interval '1' day)::date AS enddate
+			) foo
 	$BODY$;
 
-	COMMENT ON FUNCTION public.gdi_in_date_ranges(date, date[], date[]) IS 'Liefert den Wert wahr zurück, wenn sich der übergebene Zeitpunkt innerhalb der gegebeenen Start und End Datumsangaben befindet und wenn keine Zeitfenster angegeben sind.';
+	COMMENT ON FUNCTION public.gdi_in_date_ranges(date, date[], date[])
+	    IS 'Liefert den Wert wahr zurück, wenn sich der übergebene Zeitpunkt innerhalb der gegebeenen Start und End Datumsangaben befindet und wenn keine Zeitfenster angegeben sind. Ist ein Startdatum größer als das dazugehörige Enddatum, wird das Enddatum ignoriert.';
 
 COMMIT;
