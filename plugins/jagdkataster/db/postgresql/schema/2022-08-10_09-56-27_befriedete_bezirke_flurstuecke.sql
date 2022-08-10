1eac0df2c8dd9630f094165bf26098c8b8ce7e4e
Stefan Rahn <stefan.rahn@gdi-service.de>
2022-08-10 10:29:55 +0000

    Bugfix 3.2.83 Nachtrag

--- plugins/jagdkataster/db/postgresql/schema/2022-08-10_09-56-27_befriedete_bezirke_flurstuecke.sql
+++ plugins/jagdkataster/db/postgresql/schema/2022-08-10_09-56-27_befriedete_bezirke_flurstuecke.sql
@@ -11,10 +11,10 @@ CREATE OR REPLACE VIEW jagdkataster.befriedete_bezirke_flurstuecke AS
     round((st_area(st_intersection(b.the_geom, st_intersection(n.wkb_geometry, a.wkb_geometry)))::double precision * (a.amtlicheflaeche / st_area(a.wkb_geometry)::double precision))::numeric, 0) AS na_teilflaeche_abs,
     round((st_area(st_intersection(b.the_geom, st_intersection(n.wkb_geometry, a.wkb_geometry)))::double precision * (a.amtlicheflaeche / st_area(a.wkb_geometry)::double precision) / (st_area(st_intersection(b.the_geom, a.wkb_geometry))::double precision * (a.amtlicheflaeche / st_area(a.wkb_geometry)::double precision)) * 100::double precision)::numeric, 1) AS na_teilflaeche_proz
    FROM jagdkataster.befriedete_bezirke b
-     LEFT JOIN alkis_alt.ax_flurstueck a ON st_intersects(b.the_geom, a.wkb_geometry) AND a.endet IS NULL AND (st_area(st_intersection(b.the_geom, a.wkb_geometry)) / st_area(a.wkb_geometry) * 100::numeric) > 0.1
-     LEFT JOIN alkis_alt.n_nutzung n ON st_intersects(n.wkb_geometry, a.wkb_geometry) AND st_area(st_intersection(n.wkb_geometry, a.wkb_geometry)) > 0.001 AND st_area(st_intersection(b.the_geom, st_intersection(n.wkb_geometry, a.wkb_geometry))) > 0.1
-     LEFT JOIN alkis_alt.n_nutzungsartenschluessel nas ON n.nutzungsartengruppe = nas.nutzungsartengruppe AND n.werteart1 = nas.werteart1 AND n.werteart2 = nas.werteart2
-     LEFT JOIN alkis_alt.n_nutzungsartengruppe nag ON nas.nutzungsartengruppe = nag.schluessel
+     LEFT JOIN alkis.ax_flurstueck a ON st_intersects(b.the_geom, a.wkb_geometry) AND a.endet IS NULL AND (st_area(st_intersection(b.the_geom, a.wkb_geometry)) / st_area(a.wkb_geometry) * 100::numeric) > 0.1
+     LEFT JOIN alkis.n_nutzung n ON st_intersects(n.wkb_geometry, a.wkb_geometry) AND st_area(st_intersection(n.wkb_geometry, a.wkb_geometry)) > 0.001 AND st_area(st_intersection(b.the_geom, st_intersection(n.wkb_geometry, a.wkb_geometry))) > 0.1
+     LEFT JOIN alkis.n_nutzungsartenschluessel nas ON n.nutzungsartengruppe = nas.nutzungsartengruppe AND n.werteart1 = nas.werteart1 AND n.werteart2 = nas.werteart2
+     LEFT JOIN alkis.n_nutzungsartengruppe nag ON nas.nutzungsartengruppe = nag.schluessel
      LEFT JOIN jagdkataster.befriedete_bezirke_nutzungen bn ON bn.schluessel::text = (((nas.nutzungsartengruppe::text || nas.nutzungsart::text) || nas.untergliederung1::text) || nas.untergliederung2::text)
   ORDER BY a.flurstueckskennzeichen, round((st_area(st_intersection(b.the_geom, st_intersection(n.wkb_geometry, a.wkb_geometry)))::double precision * (a.amtlicheflaeche / st_area(a.wkb_geometry)::double precision) / (st_area(st_intersection(b.the_geom, a.wkb_geometry))::double precision * (a.amtlicheflaeche / st_area(a.wkb_geometry)::double precision)) * 100::double precision)::numeric, 1) DESC;
 
