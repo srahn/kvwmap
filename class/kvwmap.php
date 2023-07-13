c2c35b97231385715ea5bb6c430dfa8e1cd1ccdd
Stefan Rahn <stefan.rahn@gdi-service.de>
2023-07-13 09:27:12 +0200

    Bugfix 3.5.30

--- class/kvwmap.php
+++ class/kvwmap.php
@@ -8729,7 +8729,7 @@ SET @connection_id = {$this->pgdatabase->connection_id};
 		$this->invitations = Invitation::find(
 			$this,
 			(in_array($this->Stelle->id, $admin_stellen) ? 'true' : 'inviter_id = ' . $this->user->id),
-			($this->formvars['order'] == '' ? 'email' : '')
+			$this->formvars['order'] ?: 'email'
 		);
 
 		$this->main = 'invitations.php';
