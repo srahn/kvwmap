04989cc4ddc1716da6a79703d126b21bd9660cdf
Peter Korduan <peter.korduan@gdi-service.de>
2023-08-23 11:41:18 +0200

    Link between layer and layergroup editor.

--- layouts/snippets/layergruppe_formular.php
+++ layouts/snippets/layergruppe_formular.php
@@ -2,26 +2,39 @@
 	include(LAYOUTPATH . 'languages/layergruppe_formular_' . $this->user->rolle->language . '.php');
 ?>
 <script language="JavaScript" src="funktionen/selectformfunctions.js" type="text/javascript"></script>
+<script>
+	function gotoLayerEditor(layerId) {
+		location.href = 'index.php?go=Layereditor&selected_layer_id=' + layerId + '&csrf_token=<? echo $_SESSION['csrf_token']; ?>';
+	}
+</script>
+<style>
+	.layergruppe-button {
+		margin-left: 5px;
+	}
+</style>
 <div class="center-outerdiv">
 	<div class="input-form">
 		<h2><?php echo $strTitel; ?></h2>
-		<em><span class="px13">Werte mit * mussen eingetragen werden</span></em><br><?php
+		<em><span class="px13">Werte mit * mussen eingetragen werden</span></em><p><?php
 		echo $this->layergruppe->as_form_html(); ?>
 		<div class="clear"></div>
 		<div style="text-align: -webkit-center">
-			<table border="0" cellpadding="5" cellspacing="0" bgcolor="<?php echo $bgcolor; ?>">
+			<table width="100%" border="0" cellpadding="5" cellspacing="0" bgcolor="<?php echo $bgcolor; ?>">
 				<tr>
 					<td align="center">
 						<input type="hidden" name="go_plus" id="go_plus" value="">
-						<input value="zurück" title="Zeigt die ganze Liste an." type="button" name="go" onclick="document.location.href='index.php?go=Layergruppen_Anzeigen&csrf_token=<? echo $_SESSION['csrf_token']; ?>#group_<?php echo $this->layergruppe->get('id'); ?>'">&nbsp;<?php
+						<input value="zurück" title="Zeigt die ganze Liste an." type="button" name="go" onclick="document.location.href='index.php?go=Layergruppen_Anzeigen&csrf_token=<? echo $_SESSION['csrf_token']; ?>#group_<?php echo $this->layergruppe->get('id'); ?>'" class="layergruppe-button"><?php
 						if ($this->layergruppe->get('id') != '') { ?>
-							<input value="Ändern" title="Änderungen Speichern" type="button" name="dummy" onclick="submitWithValue('GUI','go_plus','Ändern')">&nbsp;
-							<input value="Zurücksetzen" title="Setzt alle Werte zurück auf die vorhigen." type="reset" name="reset1">&nbsp;
-							<input type="hidden" name="selected_group_id" value="<?php echo  $this->layergruppe->get('id'); ?>"><?php
+							<input value="Ändern" title="Änderungen Speichern" type="button" name="dummy" onclick="submitWithValue('GUI','go_plus','Ändern')" class="layergruppe-button">
+							<input value="Zurücksetzen" title="Setzt alle Werte zurück auf die vorhigen." type="reset" name="reset1" class="layergruppe-button">
+							<input type="hidden" name="selected_group_id" value="<?php echo  $this->layergruppe->get('id'); ?>" class="layergruppe-button"><?php
+							if ($this->formvars['selected_layer_id']) { ?>
+								<input value="zurück zum Layer" title="zurück zum Layer" type="button" name="selected_layer_id" class="layergruppe-button" onclick="gotoLayerEditor(<? echo $this->formvars['selected_layer_id']; ?>)"><?
+							}
 						}
 						else { ?>
-							<input value="Speichern" title="Speichern" type="button" name="dummy" onclick="submitWithValue('GUI','go_plus','Speichern')">&nbsp;
-							<input value="Zurücksetzen" title="Setzt die Werte zurück auf den letzten Stand." type="button" onclick="document.location.href='index.php?go=Layergruppe_Editor&selected_group_id=<?php echo  $this->layergruppe->get('id'); ?>'">&nbsp;<?
+							<input value="Speichern" title="Speichern" type="button" name="dummy" onclick="submitWithValue('GUI','go_plus','Speichern')" class="layergruppe-button">
+							<input value="Zurücksetzen" title="Setzt die Werte zurück auf den letzten Stand." type="button" onclick="document.location.href='index.php?go=Layergruppe_Editor&selected_group_id=<?php echo  $this->layergruppe->get('id'); ?>'" class="layergruppe-button"><?
 						} ?>
 						<!--input type="button" name="dummy" value="Als neue Gruppe eintragen" onclick="submitWithValue('GUI','go_plus','Als neue Gruppe eintragen')"-->
 				  </td>
