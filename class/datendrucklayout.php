3482e7e9fc5d07d3f85d444d2db4af3b7cd743a3
Stefan Rahn <stefan.rahn@gdi-service.de>
2023-09-12 11:56:24 +0200

    Formatierung als Zahl mit Tausendertrenner auch beim Sachdatendruck

--- class/datendrucklayout.php
+++ class/datendrucklayout.php
@@ -975,7 +975,10 @@ class ddl {
 			case 'Checkbox' : {
 				$option = (json_decode($this->attributes['options'][$j]));
 				$output = ($value != 'f' ? ($option->print->true != '' ? $option->print->true : 'ja') : ($option->print->false != '' ? $option->print->false : 'nein'));
-			} break;			
+			} break;
+			case 'Zahl': {
+				$output = tausenderTrenner($value);
+			} break;
 			default: {
 				if(!$preview AND $this->attributes['type'][$j] == 'bool'){
 					$value = str_replace('t', "ja", $value);	
