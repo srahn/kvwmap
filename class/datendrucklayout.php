45fab33690a0b14297e1157c2a1df3dcd7cea64f
Peter Korduan <peter.korduan@gdi-service.de>
2023-09-12 14:07:04 +0200

    Bug everypage_elements im Margin

--- class/datendrucklayout.php
+++ class/datendrucklayout.php
@@ -846,7 +846,7 @@ class ddl {
 	}
 
 	function putText($text, $fontsize, $width, $x, $y, $offsetx, $border = false, $type = 'running'){	
-		if($y < $this->layout['margin_bottom']){
+		if($y < $this->pdf->ez['bottomMargin']){
 			$nextpage = $this->getNextPage($this->pdf->currentContents);
 			if($nextpage != NULL){
 				$this->pdf->reopenObject($nextpage);
