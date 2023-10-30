0d2cf2de226eced8753cf83c9146fd1252e6f309
Peter Korduan <peter.korduan@gdi-service.de>
2023-10-30 11:20:41 +0100

    fix fk for menue

--- layouts/db/mysql/schema/2023-10-30_09-28-37_add_menue_fk.sql
+++ layouts/db/mysql/schema/2023-10-30_09-28-37_add_menue_fk.sql
@@ -1,9 +1,9 @@
 BEGIN;
 
 	-- Wenn Menü gelöscht wird, werden die Zuordnungen zu den Stellen in u_menue2stelle gelöscht.
-	ALTER TABLE `u_menue2stelle` ADD CONSTRAINT `fk_menue2stelle_meune` FOREIGN KEY IF NOT EXISTS (`menue_id`) REFERENCES `u_menues`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+	ALTER TABLE `u_menue2stelle` ADD CONSTRAINT `fk_menue2stelle_meune` FOREIGN KEY (`menue_id`) REFERENCES `u_menues`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 	-- Wenn Stelle gelöscht wird, werden die Zuordnungen zu den Menüs in u_menue2stelle gelöscht.
-	ALTER TABLE `u_menue2stelle` ADD CONSTRAINT `fk_menue2stelle_stelle` FOREIGN KEY IF NOT EXISTS (`stelle_id`) REFERENCES `stelle`(`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+	ALTER TABLE `u_menue2stelle` ADD CONSTRAINT `fk_menue2stelle_stelle` FOREIGN KEY (`stelle_id`) REFERENCES `stelle`(`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 	-- Wenn Zuordnung in u_menue2stelle gelöscht wird, werden die Zuordnungen zur Rolle in u_menue2rolle gelöscht. 
 	DELETE `u_menue2rolle`
 	FROM
@@ -12,8 +12,8 @@ BEGIN;
 	WHERE
 		u_menue2stelle.stelle_id IS NULL AND
 		u_menue2stelle.menue_id IS NULL;
-	ALTER TABLE `u_menue2rolle` ADD CONSTRAINT `fk_menue2rolle_menue2stelle` FOREIGN KEY IF NOT EXISTS (`menue_id`,`stelle_id`) REFERENCES `u_menue2stelle`(`menue_id`, `stelle_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+	ALTER TABLE `u_menue2rolle` ADD CONSTRAINT `fk_menue2rolle_menue2stelle` FOREIGN KEY (`menue_id`,`stelle_id`) REFERENCES `u_menue2stelle`(`menue_id`, `stelle_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 	-- Wenn Rolle gelöscht wird, werden die Zuordnungen der Rolle zu Menues in u_menue2rolle gelöscht.
-	ALTER TABLE `u_menue2rolle` ADD CONSTRAINT `fk_menue2rolle_rolle` FOREIGN KEY IF NOT EXISTS (`user_id`,`stelle_id`) REFERENCES `rolle`(`user_id`, `stelle_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+	ALTER TABLE `u_menue2rolle` ADD CONSTRAINT `fk_menue2rolle_rolle` FOREIGN KEY (`user_id`,`stelle_id`) REFERENCES `rolle`(`user_id`, `stelle_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 COMMIT;
