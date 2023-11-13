df0decf07316eaf809528f0d6e846943eb24749b
Stefan Rahn <stefan.rahn@gdi-service.de>
2023-11-13 11:12:30 +0100

    Bugfix 3.6.3

--- funktionen/allg_funktionen.php
+++ funktionen/allg_funktionen.php
@@ -1237,6 +1237,7 @@ function umlaute_umwandeln($name) {
 	$name = str_replace('*', '_', $name);
 	$name = str_replace('$', '', $name);
 	$name = str_replace('&', '_', $name);
+	$name = str_replace('#', '_', $name);
 	$name = iconv("UTF-8", "UTF-8//IGNORE", $name);
 	return $name;
 }
