86156864bb430ba9086bf5548da4e420327925a2
Peter Korduan <peter.korduan@gdi-service.de>
2024-01-14 16:19:21 +0100

    Change identifier declaration in class Referenzkarte.

--- class/Referenzkarte.php
+++ class/Referenzkarte.php
@@ -3,16 +3,16 @@ include_once(CLASSPATH . 'MyObject.php');
 class Referenzkarte extends MyObject {
 
 	static $write_debug = true;
-	static $identifier = 'ID';
 
 	function __construct($gui) {
 		parent::__construct($gui, 'referenzkarten');
+		$this->identifier = 'ID';
 		$this->validations = array();
 	}
 
 	public static	function find_by_id($gui, $id) {
 		$referenzkarte = new Referenzkarte($gui);
-		return $referenzkarte->find_by(Referenzkarte::$identifier, $id);
+		return $referenzkarte->find_by($referenzkarte->identifier, $id);
 	}
 
 	public static	function find($gui, $where, $order) {
