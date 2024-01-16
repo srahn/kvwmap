4135bb1f4ea2056bb93bc79320c1308adc34b2cc
Stefan Rahn <stefan.rahn@gdi-service.de>
2024-01-16 11:53:08 +0100

    Bugfix 3.6.37

--- class/stelle.php
+++ class/stelle.php
@@ -1945,7 +1945,7 @@ class stelle {
 				ul.`symbolscale`, 
 				ul.`logconsume`, 
 				ul.`start_aktiv`, 
-				ul.`use_geom`
+				ul.`use_geom`,
 				parent_id,
 				GROUP_CONCAT(ul2.Stelle_ID) as used_layer_parent_id,
 				GROUP_CONCAT(s.Bezeichnung) as used_layer_parent_bezeichnung
