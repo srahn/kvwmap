b3be21d3a9fcd1e4e5baaa083e1465d522a66635
Stefan Rahn <stefan.rahn@gdi-service.de>
2024-06-25 13:29:02 +0200

    Bugfix 3.8.28

--- layouts/snippets/SVG_map.php
+++ layouts/snippets/SVG_map.php
@@ -240,7 +240,7 @@ chmod(IMAGEPATH . $svgfile, 0666);
 $svg = '<?xml version="1.0"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
   "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg id="svgmap" zoomAndPan="disable" width="' . $res_x . '" height="' . $res_y . '" onload="init();" onmousemove="mouse_move(evt);top.drag(evt);" onmouseup="top.dragstop(evt)"
+<svg id="svgmap" zoomAndPan="disable" width="' . $res_x . '" height="' . $res_y . '" onload="init();" onmousemove="mouse_move(evt);top.drag(evt);" onmouseup="top.dragstop(evt)" onmouseenter="mouse_enter(evt)" onmouseleave="mouse_leave(evt)"
   xmlns="http://www.w3.org/2000/svg" version="1.1"
   xmlns:xlink="http://www.w3.org/1999/xlink">
 <title> kvwmap </title><desc> kvwmap - WebGIS application - kvwmap.sourceforge.net </desc>
@@ -899,6 +899,14 @@ function world2pixelsvg(pathWelt){
 
 
 // -------------------------mausinteraktionen auf canvas------------------------------
+
+function mouse_enter(evt){
+	prevent = 0;		// Objektschnellinfo wieder mehr machen
+}
+
+function mouse_leave(evt){
+	prevent = 1;		// Objektschnellinfo nicht mehr machen
+}
 
 function mouse_move(evt){
 	top.coords_anzeige(evt, null);
