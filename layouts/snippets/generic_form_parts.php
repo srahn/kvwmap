fcbcff69eeb7e7fdad3ae48618ba3e7ece9ded5c
Stefan Rahn <stefan.rahn@gdi-service.de>
2024-08-28 14:22:20 +0200

    Bugfix 3.8.62

--- layouts/snippets/generic_form_parts.php
+++ layouts/snippets/generic_form_parts.php
@@ -1135,9 +1135,8 @@
 		}
 		if ($privileg == '0') {
 			$auswahlfeld_output = $enums[$value]['output'];
-			$auswahlfeld_output_laenge = (strlen($auswahlfeld_output) > 0 ? strlen($auswahlfeld_output) : 19) + 1;
-			$datapart = '<input readonly id="' . $layer_id . '_' . $name . '_' . $k . '" style="border:0px;background-color:transparent;" size="' . $auswahlfeld_output_laenge . '" type="text" value="' . htmlspecialchars($auswahlfeld_output) . '">';
-			$datapart .= '<input type="hidden" name="' . $fieldname . '" class="' . $field_class . '" onchange="' . $onchange . '" value="' . htmlspecialchars($value) . '">'; // falls das Attribut ein visibility-changer ist
+			$datapart = '<div class="readonly_text">' . htmlspecialchars($auswahlfeld_output) . '</div>';
+			$datapart .= '<input type="hidden" name="' . $fieldname . '" id="' . $layer_id . '_' . $name . '_' . $k . '" class="' . $field_class . '" onchange="' . $onchange . '" value="' . htmlspecialchars($value) . '">'; // falls das Attribut ein visibility-changer ist
 			$auswahlfeld_output = '';
 			$auswahlfeld_output_laenge = '';
 		}
