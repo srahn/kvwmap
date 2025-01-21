dd891212137f0dc1fba51be0e8f840032635db30
Peter Korduan <peter.korduan@gdi-service.de>
2025-01-21 22:26:39 +0100

    Bessere init db.

--- plugins/metadata/db/mysql/schema/2024-07-09_15-19-45_add_constant_max_update_processes.sql
+++ plugins/metadata/db/mysql/schema/2024-07-09_15-19-45_add_constant_max_update_processes.sql
@@ -27,13 +27,17 @@ BEGIN;
   INSERT INTO config (`name`, `prefix`, `value`, `description`, `type`, `plugin`, `group`, `saved`, `editable`) VALUES (
     'METADATA_DATA_PATH',
     '',
-    '/var/www/data/datentool/',
+    '/var/www/data/',
     'Absoluter Pfad für das Verzeichnis in das die Daten der Ressourcen heruntergeladen, entpackt und die Datenpackete für den Download abgelegt werden.',
     'string',
     'metadata',
     'Plugins/metadata',
     0,
     2
+  );
+
+  INSERT INTO u_funktionen (`bezeichnung`) VALUES (
+    'metadata_update_outdated'
   );
 
 COMMIT;
