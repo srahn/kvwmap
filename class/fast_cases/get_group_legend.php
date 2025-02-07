640092b99d8878115c32d592421ea6fcd6d1dbb2
Stefan Rahn <stefan.rahn@gdi-service.de>
2025-02-07 11:18:30 +0100

    Bugfix 3.9.44 Nachtrag

--- class/fast_cases/get_group_legend.php
+++ class/fast_cases/get_group_legend.php
@@ -1319,8 +1319,8 @@ class GUI {
 				$label->size = $dbLabel['size'];
 				$label->minsize = $dbLabel['minsize'];
 				$label->maxsize = $dbLabel['maxsize'];
-				$label->minfeaturesize = $dbLabel['minfeaturesize'];
-				$label->maxfeaturesize = $dbLabel['maxfeaturesize'];
+				// $label->minfeaturesize = $dbLabel['minfeaturesize'];
+				// $label->maxfeaturesize = $dbLabel['maxfeaturesize'];
 				if ($dbLabel['maxscale'] != '') {
 					$label->maxscaledenom = $dbLabel['maxscale'];
 				}
