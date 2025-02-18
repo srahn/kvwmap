b29f26aa308bf397b62a3e7304dbb07e85152c9d
Georg Kämmert <georg.kaemmert@gdi-service.de>
2025-02-18 12:14:35 +0100

    Split part mit original_name für missing documents search.

--- class/DataTypeAttribute.php
+++ class/DataTypeAttribute.php
@@ -13,7 +13,7 @@ class DataTypeAttribute extends MyObject {
 	}
 
 	function get_document_select($layer, $attribute) {
-		$document_select = "SPLIT_PART((" . ($attribute->is_array_type() ? "UNNEST(" . $attribute->get('name') . ")" : $attribute->get('name')) . ")." . $this->get('name') . ", '&', 1)";
+		$document_select = "SPLIT_PART((" . ($attribute->is_array_type() ? "UNNEST(" . $attribute->get('name') . ")" : $attribute->get('name')) . ")." . $this->get('name') . ", '&original_name=', 1)";
 		if ($layer->get('document_url') != '') {
 			$document_select = "REPLACE(" . $document_select . ", '" . $layer->get('document_url') . "', '" . $layer->get('document_path') . "')";
 		}
