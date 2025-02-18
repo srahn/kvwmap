f4f142456efe20ed8f9c4ac2c44acc727094434b
Georg Kämmert <georg.kaemmert@gdi-service.de>
2025-02-18 09:11:47 +0100

    show missing files also for datatypes.

--- layouts/snippets/layer_missing_documents.php
+++ layouts/snippets/layer_missing_documents.php
@@ -1,48 +1,118 @@
 <?
 	include_once(CLASSPATH . 'PgObject.php');
 	include_once(CLASSPATH . 'LayerAttribute.php');
+	/**
+	 * Prüfen ob es ein 
+	 */
 ?>
-<h2 style="margin-top: 20px">Fehlende Dokumente im Layer <?php echo $this->formvars['selected_layer_id']; ?></h2><?
+<h2 style="margin-top: 20px">Fehlende Dokumente im Layer <?php echo $this->layer->get('alias'); ?> [<?php echo $this->layer->get('Name'); ?>] id: <? echo $this->formvars['selected_layer_id']; ?></h2><?
 // echo '<br>Layer-Attribute: ' . print_r($this->layer->data, true); ?>
 <div style="text-align: left; margin-left: 25px; width: 1000px; padding: 20px;"><?
 	echo '<b>Dokument-Verzeichnis:</b> ' . $this->layer->get('document_path');
-	$this->layer->document_attributes = LayerAttribute::find($this, "layer_id = " . $this->formvars['selected_layer_id'] . " AND form_element_type = 'Dokument'");
-	foreach ($this->layer->document_attributes AS $document_attribute) {
-		$pg_obj = new PgObject($this, $this->layer->get('schema'), $this->layer->get('maintable'));
-		$features = $pg_obj->find_where(
-			$document_attribute->get('name') . " IS NOT NULL", // where
-			$document_attribute->get('name'), // order
-			$this->layer->get('oid') . " AS oid, SPLIT_PART(" . (strpos($document_attribute->get('type'), '_') === 0 ? "UNNEST(" . $document_attribute->get('name') . ")" : $document_attribute->get('name')) . ",'&', 1) AS document" // select
-		);
-		// echo '<p>Dokument-Einträge in Datenbank:<br>';
-		// foreach ($features AS $feature) {
-		// 	echo '<br>' . $feature->get('document');
-		// }
-		$files = glob($this->layer->get('document_path') . '*[!thumb].*');
-		// foreach ($files AS $file) {
-		// 	echo '<br>' . $file;
-		// }
-		$missing_documents = array_diff(
-			array_map(function($feature) { return $feature->get('document'); }, $features), // documents of features in database
-			$files // files in document_path
-		); ?>
-		<p>
-		<table border="0" cellspacing="0" cellpadding="0">
-			<tr>
-				<th colspan="2" style="text-align: left;"><? echo '<p>Dokument-Attribut: ' . $document_attribute->get('name'); ?></th>
-			</tr>
-			</tr>
-			<tr>
-				<th><? echo $this->layer->get('oid'); ?></th>
-				<th>Datei</th>
-			</tr><?
-			foreach($missing_documents AS $key => $missing_document) { ?>
-			<tr>
-				<td><? echo $features[$key]->get('oid'); ?></td>
-				<td style="padding-left: 20px"><? echo str_replace($this->layer->get('document_path'), '', $missing_document); ?></td>
-			</tr><?
-		} ?>
-		</table><?
+	$pg_obj = new PgObject($this, $this->layer->get('schema'), $this->layer->get('maintable'));
+	$this->layer->attributes = LayerAttribute::find($this, "layer_id = " . $this->formvars['selected_layer_id']);
+	$attributes_with_missing_docs = array();
+	foreach ($this->layer->attributes AS $attribute) {
+		$missing_documents = array();
+		if ($attribute->is_document_attribute()) {
+			$features = $pg_obj->find_where(
+				$attribute->get('name') . " IS NOT NULL", // where
+				$attribute->get('name'), // order
+				$this->layer->get('oid') . " AS oid, SPLIT_PART(" . ($attribute->is_array_type() ? "UNNEST(" . $attribute->get('name') . ")" : $attribute->get('name')) . ",'&', 1) AS document" // select
+			);
+			if (count($features) > 0) {
+				// echo '<p>Dokument-Einträge in Datenbank:<br>';
+				// foreach ($features AS $feature) {
+				// 	echo '<br>' . $feature->get('document');
+				// }
+				// ToDo: Es muss geprüft werden ob in Options eine Regel zur Bildung des document_path gibt und in diesem Verzeichnis gesucht werden.
+				$files = glob($this->layer->get('document_path') . '*[!thumb].*');
+				// foreach ($files AS $file) {
+				// 	echo '<br>' . $file;
+				// }
+				$missing_documents = array_diff(
+					array_map(function($feature) { return $feature->get('document'); }, $features), // documents of features in database
+					$files // files in document_path
+				);
+	
+				if (count($missing_documents) > 0) {
+					$attributes_with_missing_docs[] = array(
+						'attribute' => $attribute,
+						'features' => $features,
+						'missing_documents' => $missing_documents
+					);
+				}
+			}
+		}
+
+		if ($attribute->is_datatype_attribute()) {
+			$datatype_attributes = $attribute->get_datatype_attributes();
+			foreach ($datatype_attributes AS $datatype_attribute) {
+				if ($datatype_attribute->is_document_attribute()) {
+					$datatype = $attribute->get_datatype();
+					echo '<br>Attribut: ' . $attribute->get('name') . ' Datentyp: ' . $datatype->get('name') . ' Datentypattribut: ' . $datatype_attribute->get('name');
+					// Das extrahieren der Dokumentnamen der Feature Datentyp und Array berücksichtigen.
+					$features = $pg_obj->find_where(
+						$datatype_attribute->get_document_where($this->layer, $attribute), // where
+						'document', // order
+					  $this->layer->get('oid') . " AS oid," .
+						$datatype_attribute->get_document_select($this->layer, $attribute) . " AS document" // select
+					);
+					$features = array_filter(
+						$features,
+						function($feature) {
+							return !is_null($feature->get('document'));
+						}
+					);
+					// echo '<p>Dokument-Einträge in Datenbank:<br>';
+					// foreach ($features AS $feature) {
+					// 	echo '<br>' . $feature->get('document');
+					// }
+
+					if (count($features) > 0) {
+						$files = glob($this->layer->get('document_path') . '*[!thumb].*');
+						// foreach ($files AS $file) {
+						// 	echo '<br>' . $file;
+						// }
+						$missing_documents = array_diff(
+							array_map(function($feature) { return $feature->get('document'); }, $features), // documents of features in database
+							$files // files in document_path
+						);
+						if (count($missing_documents) > 0) {
+							$attributes_with_missing_docs[] = array(
+								'attribute' => $attribute,
+								'features' => $features,
+								'missing_documents' => $missing_documents
+							);
+						}
+					}	
+				}
+			}
+		}
 	}
-?>
+
+	if (count($attributes_with_missing_docs) > 0) {
+		foreach ($attributes_with_missing_docs AS $attribute_with_missing_docs) { ?>
+			<p>
+			<table border="0" cellspacing="0" cellpadding="0">
+				<tr>
+					<th colspan="2" style="text-align: left;"><? echo '<p>Dokument-Attribut: ' . $attribute_with_missing_docs['attribute']->get('name'); ?></th>
+				</tr>
+				</tr>
+				<tr>
+					<th><? echo $this->layer->get('oid'); ?></th>
+					<th>Datei</th>
+				</tr><?
+				foreach($attribute_with_missing_docs['missing_documents'] AS $key => $missing_document) { ?>
+					<tr>
+						<td><? echo $attribute_with_missing_docs['features'][$key]->get('oid'); ?></td>
+						<td style="padding-left: 20px"><? echo str_replace($this->layer->get('document_path'), '', $missing_document); ?></td>
+					</tr><?
+				} ?>
+			</table><?php
+		}
+	}
+	else {
+		?><br>Es wurden keine fehlenden Dokumente gefunden.<?
+	} ?>
 </div>
