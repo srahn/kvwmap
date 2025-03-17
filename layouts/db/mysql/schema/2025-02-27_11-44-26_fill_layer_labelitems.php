ce4662553590b5dc2d3b8b620e709e18ccd2d04d
Stefan Rahn <stefan.rahn@gdi-service.de>
2025-03-17 14:19:06 +0100

    labelitems migration

--- layouts/db/mysql/schema/2025-02-27_11-44-26_fill_layer_labelitems.php
+++ layouts/db/mysql/schema/2025-02-27_11-44-26_fill_layer_labelitems.php
@@ -11,32 +11,34 @@ if (isset($this) AND is_object($this) AND get_class($this) == 'administration') 
     try {
       echo '<br>Layer-ID: ' . $layer->get('Layer_ID') . ':';
       $layerdb = $mapDB->getlayerdatabase($layer->get('Layer_ID'), $GUI->Stelle->pgdbhost);
-      $query_attributes = $mapDB->read_layer_attributes($layer->get('Layer_ID'), $layerdb, NULL, false, false, false);
-      try {
-        $attributes = $mapDB->getDataAttributes($layerdb, $layer->get('Layer_ID'));
-        $order = 2;
-        foreach($attributes as $attribute){
-          if(is_array($attribute) AND $attributes['the_geom'] != $attribute['name']){
-            $index = $query_attributes['indizes'][$attribute['name']];
-            $sql .= "
-              INSERT INTO 
-                layer_labelitems
-              VALUES (
-                " . $layer->get('Layer_ID') . ",
-                '" . $attribute['name'] . "',
-                '" . $query_attributes['alias'][$index] . "',
-                " . (($layer->get('labelitem') == $attribute['name']) ? '1' : $order) . ");";
-            if ($layer->get('labelitem') != $attribute['name']) {
-              $order++;
+      if ($layerdb) {
+        $query_attributes = $mapDB->read_layer_attributes($layer->get('Layer_ID'), $layerdb, NULL, false, false, false);
+        try {
+          $attributes = $mapDB->getDataAttributes($layerdb, $layer->get('Layer_ID'));
+          $order = 2;
+          foreach($attributes as $attribute){
+            if(is_array($attribute) AND $attributes['the_geom'] != $attribute['name']){
+              $index = $query_attributes['indizes'][$attribute['name']];
+              $sql .= "
+                INSERT INTO 
+                  layer_labelitems
+                VALUES (
+                  " . $layer->get('Layer_ID') . ",
+                  '" . $attribute['name'] . "',
+                  '" . $query_attributes['alias'][$index] . "',
+                  " . (($layer->get('labelitem') == $attribute['name']) ? '1' : $order) . ");";
+              if ($layer->get('labelitem') != $attribute['name']) {
+                $order++;
+              }
             }
           }
+          if (is_array($attributes)) {
+            echo ' Labelitems eingetragen.';
+          }
         }
-        if (is_array($attributes)) {
-          echo ' Labelitems eingetragen.';
+        catch (Exception $ex) {
+          echo ' Labelitems nicht aus Data ermittelbar.';exit;
         }
-      }
-      catch (Exception $ex) {
-        echo ' Labelitems nicht aus Data ermittelbar.';exit;
       }
     }
     catch (Exception $ex) {
