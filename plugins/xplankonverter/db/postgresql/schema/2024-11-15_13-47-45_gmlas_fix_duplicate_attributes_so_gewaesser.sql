4f993ae68d7920929a66fbec5496affab568ca62
rkraetschmer <robert.kraetschmer@gdi-service.de>
2025-04-01 14:33:07 +0000

    Fix migration errors.

--- plugins/xplankonverter/db/postgresql/schema/2024-11-15_13-47-45_gmlas_fix_duplicate_attributes_so_gewaesser.sql
+++ plugins/xplankonverter/db/postgresql/schema/2024-11-15_13-47-45_gmlas_fix_duplicate_attributes_so_gewaesser.sql
@@ -1,8 +1,6 @@
 BEGIN;
-
-DELETE FROM xplankonverter.mappingtable_gmlas_to_gml
-WHERE id IN (4496,4503)
-AND t_table = 'so_gewaesser'
-AND t_column IN ('aufschrift','nummer');
-
+  DELETE FROM xplankonverter.mappingtable_gmlas_to_gml
+  WHERE id IN (4496,4503)
+  AND t_table = 'so_gewaesser'
+  AND t_column IN ('aufschrift','nummer');
 COMMIT;
--- plugins/xplankonverter/db/postgresql/schema/2025-03-03_16-50-15_xplan_fix_typo_validation_plan.sql
+++ plugins/xplankonverter/db/postgresql/schema/2025-03-03_16-50-15_xplan_fix_typo_validation_plan.sql
@@ -1,5 +1,5 @@
 BEGIN;
-UPDATE xplankonverter.validierungen
-SET msg_correcture = "Vergeben Sie einen räumlichen Geltungsbereich für den Plan."
-WHERE msg_correcture = "Vergeben Sie einen räumlichen Geltungsbereich für den Plan an.";
+  UPDATE xplankonverter.validierungen
+  SET msg_correcture = 'Vergeben Sie einen räumlichen Geltungsbereich für den Plan.'
+  WHERE msg_correcture = 'Vergeben Sie einen räumlichen Geltungsbereich für den Plan an.';
 COMMIT;
