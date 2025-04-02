f7e057f6b198c407b792e291ec2ae4a50ecc425d
Stefan Rahn <stefan.rahn@gdi-service.de>
2025-04-02 09:37:51 +0200

    doppelter Code

--- class/kvwmap.php
+++ class/kvwmap.php
@@ -4527,8 +4527,6 @@ echo '			</table>
     else {
 			$attributes = $mapDB->read_layer_attributes($this->formvars['layer_id'], $layerdb, $attributenames);
 		}
-		// $attributes['options'][$this->formvars['attribute']] = str_replace('$userid', $this->user->id, $attributes['options'][$this->formvars['attribute']]);
-		// $attributes['options'][$this->formvars['attribute']] = str_replace('$stelleid', $this->Stelle->id, $attributes['options'][$this->formvars['attribute']]);
 		$options = array_shift(explode(';', $attributes['options'][$this->formvars['attribute']]));
     $reqby_start = strpos(strtolower($options), "<required by>");
     if ($reqby_start > 0) {
