2aa033c5f88b088936a61f3f0647f8e85b6c737d
Stefan Rahn <stefan.rahn@gdi-service.de>
2025-04-10 10:04:15 +0200

    Layout GLE nebeneinander

--- layouts/snippets/generic_layer_editor_2.php
+++ layouts/snippets/generic_layer_editor_2.php
@@ -110,7 +110,8 @@ if ($doit == true) {
 			<div style="width: 80px;"></div> <?
 			echo value_of($layer, 'paging'); ?>
 			<div class="gle-view">	<?
-				for ($g = 0; $g < 3; $g++) {
+				$s = ($layer['template'] == ''? 0 : 1);
+				for ($g = $s; $g < 3; $g++) {
 					echo '<img onclick="checkForUnsavedChanges(event);switch_gle_view1(' . $layer['Layer_ID'] . ', ' . $layer['gle_view'] . ', ' . $g . ', this);" title="' . ${'strSwitchGLEView' . $g} . '" class="hover-border pointer gle-view-button ' . ($layer['gle_view'] == $g? 'active':'') . '" src="' . GRAPHICSPATH . 'gle' . $g . '.png">';
 				}	?>
 			</div>
