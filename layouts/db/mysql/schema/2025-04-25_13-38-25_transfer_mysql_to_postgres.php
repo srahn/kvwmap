178aec9c040b01efcad74631f0960160dea8ce29
Stefan Rahn <stefan.rahn@gdi-service.de>
2025-04-28 15:34:33 +0200

    transfer_mysql_to_postgres

--- layouts/db/mysql/schema/2025-04-25_13-38-25_transfer_mysql_to_postgres.php
+++ layouts/db/mysql/schema/2025-04-25_13-38-25_transfer_mysql_to_postgres.php
@@ -29,34 +29,39 @@
   ";
   $this->pgdatabase->execSQL($sql, 0, 0);
 
-  $sql = "
-    with sequences as (
-      select *
-      from (
-        select table_schema,
-              table_name,
-              column_name,
-              pg_get_serial_sequence(format('%I.%I', table_schema, table_name), column_name) as col_sequence
-        from information_schema.columns
-        where table_schema = 'kvwmap'
-      ) t
-      where col_sequence is not null
-    ), maxvals as (
-      select table_schema, table_name, column_name, col_sequence,
-              (xpath('/row/max/text()',
-                query_to_xml(format('select max(%I) from %I.%I', column_name, table_schema, table_name), true, true, ''))
-              )[1]::text::bigint as max_val
-      from sequences
-    ) 
-    select table_schema, 
-          table_name, 
-          column_name, 
-          col_sequence,
-          coalesce(max_val, 0) as max_val,
-          setval(col_sequence, coalesce(max_val, 1)) --<< this will change the sequence
-    from maxvals;";
-  #$this->pgdatabase->execSQL($sql, 0, 0);
+  if ($this->pgdatabase->success) {
+    $sql = "
+      with sequences as (
+        select *
+        from (
+          select table_schema,
+                table_name,
+                column_name,
+                pg_get_serial_sequence(format('%I.%I', table_schema, table_name), column_name) as col_sequence
+          from information_schema.columns
+          where table_schema = 'kvwmap'
+        ) t
+        where col_sequence is not null
+      ), maxvals as (
+        select table_schema, table_name, column_name, col_sequence,
+                (xpath('/row/max/text()',
+                  query_to_xml(format('select max(%I) from %I.%I', column_name, table_schema, table_name), true, true, ''))
+                )[1]::text::bigint as max_val
+        from sequences
+      ) 
+      select table_schema, 
+            table_name, 
+            column_name, 
+            col_sequence,
+            coalesce(max_val, 0) as max_val,
+            setval(col_sequence, coalesce(max_val, 1)) --<< this will change the sequence
+      from maxvals;";
+    $this->pgdatabase->execSQL($sql, 0, 0);
 
-  $result[0] = true; # MIgration nicht bestätigen
+    $result[0] = false; # Migration bestätigen
+  }
+  else {
+    $result[0] = true; # Migration nicht bestätigen
+  }
 
 ?>
