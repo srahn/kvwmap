6415d0ea19925246020af97201ad7dddcefa53f7
Peter Korduan <peter.korduan@gdi-service.de>
2025-05-16 13:04:43 +0200

    Add Multipolygon Option to import ogr2ogr_gml.

--- plugins/metadata/model/Ressource.php
+++ plugins/metadata/model/Ressource.php
@@ -1534,7 +1534,7 @@ class Ressource extends PgObject {
 				$this->database,
 				$this->get('import_layer'),
 				NULL,
-				($first ? '-overwrite' : '-append'),
+				($first ? '-overwrite' : '-append') . ' -nlt MULTIPOLYGON',
 				'UTF-8',
 				false,
 				$this->unlogged,
