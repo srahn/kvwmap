1ee9516ec8d10fb29fa457ad1e62aaf5c2445f72
Stefan Rahn <stefan.rahn@gdi-service.de>
2025-06-03 09:14:22 +0200

    Branchwechsel

--- class/kvwmap.php
+++ class/kvwmap.php
@@ -5546,6 +5546,7 @@ echo '			</table>
 			}	# break fehlt mit Absicht
 			case "switch_branch" : case "update_code_and_databases" : {
 				$result = $this->administration->update_code();
+				$this->showAdminFunctions();
 			} break;
 			case "update_code_and_databases" : case "update_databases" : {
 				$this->administration->get_database_status();
