b36dda44ae3752aaca75d337fd42769ecdfea008
Georg Kämmert <georg.kaemmert@gdi-service.de>
2025-07-04 14:07:51 +0200

    Read konverterierung config from set_config.

--- plugins/xplankonverter/control/index.php
+++ plugins/xplankonverter/control/index.php
@@ -323,16 +323,16 @@ if (stripos($GUI->go, 'xplankonverter_') === 0) {
 	$GUI->plan_oid_name = $GUI->konvertierung->config['plan_oid_name'];
 	
 	//TEMP
-	$GUI->title = 'Flächennutzungsplan';
-	$GUI->plan_short_title = 'F-Plan';
-	$GUI->plan_class = 'FP_Plan';
-	$GUI->plan_abk = 'fplan';
-	$GUI->plan_layer_id = XPLANKONVERTER_FP_PLAENE_LAYER_ID;
-	$GUI->plan_attribut_aktualitaet = 'wirksamkeitsdatum';
-	$GUI->plan_table_name = 'fp_plan';
+	// $GUI->title = 'Flächennutzungsplan';
+	// $GUI->plan_short_title = 'F-Plan';
+	// $GUI->plan_class = 'FP_Plan';
+	// $GUI->plan_abk = 'fplan';
+	// $GUI->plan_layer_id = XPLANKONVERTER_FP_PLAENE_LAYER_ID;
+	// $GUI->plan_attribut_aktualitaet = 'wirksamkeitsdatum';
+	// $GUI->plan_table_name = 'fp_plan';
 	
-	$GUI->plan_table_name = strtolower($GUI->plan_class);
-	$GUI->plan_oid_name = $GUI->plan_table_name . '_oid';
+	// $GUI->plan_table_name = strtolower($GUI->plan_class);
+	// $GUI->plan_oid_name = $GUI->plan_table_name . '_oid';
 	// TEMP END
 }
 
