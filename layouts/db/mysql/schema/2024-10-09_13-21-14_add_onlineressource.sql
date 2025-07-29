bbefa47e678cf31b1603bd7002fae95fda4868c5
Peter Korduan <peter.korduan@gdi-service.de>
2025-07-29 14:50:50 +0200

    if not exists in migration

--- layouts/db/mysql/schema/2024-10-09_13-21-14_add_onlineressource.sql
+++ layouts/db/mysql/schema/2024-10-09_13-21-14_add_onlineressource.sql
@@ -1,5 +1,5 @@
 BEGIN;
-	ALTER TABLE `stelle` ADD COLUMN `ows_contenturl` text COMMENT 'Home-Page des Verantwortlichen für Metadaten.' AFTER `ows_contentorganization`;
-	ALTER TABLE `stelle` ADD COLUMN `ows_contacturl` text COMMENT 'Home-Page des Verantwortlichen für die Geodaten.' AFTER `ows_contactorganization`;
-	ALTER TABLE `stelle` ADD COLUMN `ows_distributionurl` text COMMENT 'Home-Page des Verantwortlichen für den Vertrieb.' AFTER `ows_distributionorganization`;
+	ALTER TABLE `stelle` ADD COLUMN IF NOT EXISTS `ows_contenturl` text COMMENT 'Home-Page des Verantwortlichen für Metadaten.' AFTER `ows_contentorganization`;
+	ALTER TABLE `stelle` ADD COLUMN IF NOT EXISTS `ows_contacturl` text COMMENT 'Home-Page des Verantwortlichen für die Geodaten.' AFTER `ows_contactorganization`;
+	ALTER TABLE `stelle` ADD COLUMN IF NOT EXISTS `ows_distributionurl` text COMMENT 'Home-Page des Verantwortlichen für den Vertrieb.' AFTER `ows_distributionorganization`;
 COMMIT;
