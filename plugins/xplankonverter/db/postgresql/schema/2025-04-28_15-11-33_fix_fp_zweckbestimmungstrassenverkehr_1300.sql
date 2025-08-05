4b63c5d1a3c8b648478323cf3502c7bd938c43c8
Georg Kämmert <georg.kaemmert@gdi-service.de>
2025-08-05 08:21:12 +0200

    Fix fp_zweckbestimmung.

--- plugins/xplankonverter/db/postgresql/schema/2025-04-28_15-11-33_fix_fp_zweckbestimmungstrassenverkehr_1300.sql
+++ plugins/xplankonverter/db/postgresql/schema/2025-04-28_15-11-33_fix_fp_zweckbestimmungstrassenverkehr_1300.sql
@@ -1,16 +1,15 @@
 
 BEGIN;
-INSERT INTO xplan_gml.enum_fp_zweckbestimmungstrassenverkehr (wert,abkuerzung,beschreibung)
-VALUES
-(
-	1300,
-	'Ortsdurchfahrt',
-	'Ortsdurchfahrt'
-)
-ON CONFLICT DO NOTHING;
+	INSERT INTO xplan_gml.enum_fp_zweckbestimmungstrassenverkehr (wert,abkuerzung,beschreibung)
+	VALUES
+	(
+		1300,
+		'Ortsdurchfahrt',
+		'Ortsdurchfahrt'
+	)
+	ON CONFLICT DO NOTHING;
 
-
-INSERT INTO pg_enum (enumtypid, enumlabel, enumsortorder)
+	INSERT INTO pg_enum (enumtypid, enumlabel, enumsortorder)
 	SELECT
 		'xplan_gml.fp_zweckbestimmungstrassenverkehr'::regtype::oid, '1300', 
 	CASE
@@ -19,5 +18,5 @@ INSERT INTO pg_enum (enumtypid, enumlabel, enumsortorder)
 		ELSE 1 END
 	WHERE NOT EXISTS(SELECT 1 FROM pg_enum WHERE enumtypid = 'xplan_gml.fp_zweckbestimmungstrassenverkehr'::regtype AND enumlabel = '1300' );	
 
-
+  --ALTER TYPE xplan_gml.fp_zweckbestimmungstrassenverkehr ADD VALUE '1300' AFTER '1200';
 COMMIT;
