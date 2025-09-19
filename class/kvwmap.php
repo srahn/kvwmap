a21a41efd596df723cbbfe610e2cf2a9363818d3
Stefan Rahn <stefan.rahn@gdi-service.de>
2025-09-19 11:46:19 +0200

    transfer_mysql_to_postgres

--- class/kvwmap.php
+++ class/kvwmap.php
@@ -5547,7 +5547,7 @@ echo '			</table>
 			case "switch_branch" : case "update_code_and_databases" : {
 				$result = $this->administration->update_code();
 			} # break fehlt mit Absicht
-			case "switch_branch" : case "update_code_and_databases" : case "update_databases" : {
+			case "update_code_and_databases" : case "update_databases" : {
 				$this->administration->get_database_status();
 				$err_msgs = $this->administration->update_databases();
 				if (count($err_msgs) > 0) {
