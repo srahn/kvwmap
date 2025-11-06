cac5054d1158d8b25c65a8c48974e84b5e1f3955
Stefan Rahn <stefan.rahn@gdi-service.de>
2025-11-06 15:51:52 +0100

    Bugfix 4.0.9

--- plugins/nachweisverwaltung/view/dokumenteneingabeformular.php
+++ plugins/nachweisverwaltung/view/dokumenteneingabeformular.php
@@ -481,7 +481,7 @@ $legendheight = $this->map->height + 20;
 							<select size="1" name="selected_group_id" onchange="ahah('index.php', 'go=getqueryableVectorLayers&group_id=' + this.value, [document.GUI.geom_from_layer], ['sethtml']);" <?php if(count_or_0($this->layergruppen['ID'])==0){ echo 'disabled';}?>>
 								<option value="">  -- <?php echo $this->strGroup; ?> --  </option>
 								<?
-								for($i = 0; $i < count($this->layergruppen['ID']); $i++){         
+								for($i = 0; $i < count_or_0($this->layergruppen['ID']); $i++){         
 									echo '<option';
 									if($this->layergruppen['ID'][$i] == $this->formvars['selected_group_id']){
 										echo ' selected';
