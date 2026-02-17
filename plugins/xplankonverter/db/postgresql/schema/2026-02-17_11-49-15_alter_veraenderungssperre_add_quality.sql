c3972ee62bccaf4419b1fd76c069e95256735331
Georg Kämmert <georg.kaemmert@gdi-service.de>
2026-02-17 12:01:40 +0100

    Fix qualitaet_document migration.

--- plugins/xplankonverter/db/postgresql/schema/2026-02-17_11-49-15_alter_veraenderungssperre_add_quality.sql
+++ plugins/xplankonverter/db/postgresql/schema/2026-02-17_11-49-15_alter_veraenderungssperre_add_quality.sql
@@ -1,4 +1,4 @@
 BEGIN;
   UPDATE xplan_gml.enum_xp_verlaengerungveraenderungssperre SET abkuerzung = 'Keine Verlängerung' WHERE wert = 1000;
-  ALTER TABLE xplankonverter.konvertierung ADD COLUMN IF NOT EXISTS qualitaet_document character varying(100);
+  ALTER TABLE xplankonverter.konvertierungen ADD COLUMN IF NOT EXISTS qualitaet_document character varying(100);
 COMMIT;
