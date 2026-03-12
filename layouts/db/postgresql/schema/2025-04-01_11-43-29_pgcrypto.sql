046e26aa0526b266087f33e91cdf340660afa874
Stefan Rahn <stefan.rahn@gdi-service.de>
2026-03-12 16:01:20 +0100

    install test

--- layouts/db/postgresql/schema/2025-04-01_11-43-29_pgcrypto.sql
+++ layouts/db/postgresql/schema/2025-04-01_11-43-29_pgcrypto.sql
@@ -2,7 +2,11 @@ BEGIN;
 
 SET search_path TO public;
 
-
+DO $$
+BEGIN
+   execute 'ALTER DATABASE '||current_database()||' SET search_path TO public;';
+END
+$$;
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
