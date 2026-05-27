cfdc0dd625dd19478bb8d5f3637cb98b7ab767ec
rkraetschmer <robert.kraetschmer@gdi-service.de>
2026-05-27 15:19:34 +0000

    fix fp_landwirtschaftsflaeche

--- plugins/xplankonverter/db/postgresql/schema/2026-05-27_16-38-00_fix_map_landwirtschaftsflaeche.sql
+++ plugins/xplankonverter/db/postgresql/schema/2026-05-27_16-38-00_fix_map_landwirtschaftsflaeche.sql
@@ -2,6 +2,6 @@ BEGIN;
 -- remove mapping fp_landwirtschaftsflaeche (marked as deprecated in xplan 5.4 and disappearing in xplan 6) onto fp_landwirtschaft
 -- instead mapping will happen in extract_gml.php
 UPDATE xplankonverter.mappingtable_gmlas_to_gml
-SET t_table = 'fp_landwirtschaft'
+SET t_table = 'fp_landwirtschaftsflaeche'
 WHERE o_table = 'fp_landwirtschaftsflaeche';
 COMMIT;
