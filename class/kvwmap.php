fbec020ddd3d3c6fa06a83aa1943f035229b1eb6
Stefan Rahn <stefan.rahn@gdi-service.de>
2026-07-07 14:20:31 +0200

    Bugfix 4.1.67

--- class/kvwmap.php
+++ class/kvwmap.php
@@ -13062,7 +13062,7 @@ MS_MAPFILE="' . WMS_MAPFILE_PATH . $mapfile . '" exec ${MAPSERV}');
 		$this->layergruppen = $this->mapDB->get_Groups($this->layergruppen); # Gruppen mit Pfaden versehen
 		# wenn Gruppe ausgewählt, Einschränkung auf Layer dieser Gruppe
 		if (value_of($this->formvars, 'selected_group_id') AND $this->formvars['selected_layer_id'] == '') {
-			$this->layerdaten = $this->Stelle->getqueryableVectorLayers(NULL, $this->user->id, $this->formvars['selected_group_id']);
+			$this->layerdaten = $this->Stelle->getqueryableVectorLayers(NULL, $this->user->id, $this->formvars['selected_group_id'], NULL, NULL, NULL, false, true);
 		}
 
 		# Export-Einstellungen speichern
