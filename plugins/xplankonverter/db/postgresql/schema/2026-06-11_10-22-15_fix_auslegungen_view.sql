716a58fe6b502ccd485051b079ab1c7ce069b220
Peter Korduan <peter.korduan@gdi-service.de>
2026-07-17 14:51:09 +0200

    xplan fix auslegung view

--- plugins/xplankonverter/db/postgresql/schema/2026-06-11_10-22-15_fix_auslegungen_view.sql
+++ plugins/xplankonverter/db/postgresql/schema/2026-06-11_10-22-15_fix_auslegungen_view.sql
@@ -52,7 +52,7 @@ BEGIN;
     WHERE v.id = d.protokoll_id
   );
   ALTER TABLE xplankonverter.veroeffentlichungsprotokoll_dokumente DROP CONSTRAINT IF EXISTS protokoll_id_fk;
-  ALTER TABLE xplankonverter.veroeffentlichungsprotokoll_dokumente ADD CONSTRAINT protokoll_id_fk FOREIGN KEY (protokoll_id) REFERENCES xplankonverter.veroeffentlichungsprotokolle(id) ON UPDATE CASCADE ON DELETE CASCADE
+  ALTER TABLE xplankonverter.veroeffentlichungsprotokoll_dokumente ADD CONSTRAINT protokoll_id_fk FOREIGN KEY (protokoll_id) REFERENCES xplankonverter.veroeffentlichungsprotokolle(id) ON UPDATE CASCADE ON DELETE CASCADE;
 
   ALTER TABLE xplankonverter.veroeffentlichungsprotokolle DROP COLUMN pruefungen_seit_observationstart;
 COMMIT;
